3e8260770b9d501eba55413c2c77591a1e8c3331
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-06 17:57:26 +0200

    setup 5

---
+++ src/main/java/org/shvetsov/_5.java
@@ -0,0 +1,19 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 5,
+        name = "Longest Palindromic Substring",
+        level = Level.MEDIUM,
+        tags = {"String", "Dynamic Programming"}
+)
+public class _5 {
+
+    public String longestPalindrome(String s) {
+
+        return null;
+    }
+
+}
