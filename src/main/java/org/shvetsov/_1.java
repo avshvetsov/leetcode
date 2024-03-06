a2f114a0d6da8696ae73407e032fadf2a3ba180b
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-06 12:27:35 +0200

    setup 1

---
+++ src/main/java/org/shvetsov/_1.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1,
+        name = "Two Sum",
+        level = Level.EASY,
+        tags = {"Array", "Hash Table"}
+)
+public class _1 {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        return null;
+    }
+}
