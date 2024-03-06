ca35af333b66f701f21a192cf6920e6c62a8b8d9
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-06 17:47:06 +0200

    solve 4

--- src/main/java/org/shvetsov/_4.java
+++ src/main/java/org/shvetsov/_4.java
@@ -12,7 +12,32 @@ import org.shvetsov.core.Level;
 public class _4 {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-        return -1;
+        int[] array = new int[nums1.length + nums2.length];
+        int i = 0;
+        int j1 = 0;
+        int j2 = 0;
+        while (j1 < nums1.length && j2 < nums2.length) {
+            if (nums1[j1] < nums2[j2]) {
+                array[i] = nums1[j1];
+                j1++;
+            } else {
+                array[i] = nums2[j2];
+                j2++;
+            }
+            i++;
+        }
+        while (j1 < nums1.length) {
+            array[i] = nums1[j1];
+            j1++;
+            i++;
+        }
+        while (j2 < nums2.length) {
+            array[i] = nums2[j2];
+            j2++;
+            i++;
+        }
+        return array.length % 2 != 0 ?
+                array[array.length / 2] :
+                ((double) array[array.length / 2] + (double) array[(array.length / 2) - 1]) / 2;
     }
 }
