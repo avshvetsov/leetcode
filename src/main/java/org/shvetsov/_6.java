3860afb4ce53572ffaa4efe0df619dd5fa67cb84
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-06 19:09:11 +0200

    solve 6

--- src/main/java/org/shvetsov/_6.java
+++ src/main/java/org/shvetsov/_6.java
@@ -11,7 +11,20 @@ import org.shvetsov.core.Level;
 )
 public class _6 {
     public String convert(String s, int numRows) {
-
-        return null;
+        if (numRows == 1) {
+            return s;
+        }
+        StringBuilder res = new StringBuilder();
+        int length = s.length();
+        int symbolsInLoop = numRows * 2 - 2;
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < length; j += symbolsInLoop) {
+                res.append(s.charAt(i + j));
+                if (i != 0 && i != numRows - 1 && j + 2 * numRows - i - 2 < length) {
+                    res.append(s.charAt(j + 2 * numRows - i - 2));
+                }
+            }
+        }
+        return res.toString();
     }
 }
