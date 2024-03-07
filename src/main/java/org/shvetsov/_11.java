4eb3f53f996ef31bc3bcd7544e8f32e2400efc64
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-07 18:42:43 +0200

    setup 11

---
+++ src/main/java/org/shvetsov/_11.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 11,
+        name = "Container With Most Water",
+        level = Level.MEDIUM,
+        tags = {"Array", "Two Pointers", "Greedy"}
+)
+public class _11 {
+
+    public int maxArea(int[] height) {
+
+        return 0;
+    }
+}
