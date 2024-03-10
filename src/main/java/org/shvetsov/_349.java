d4ccb443d69a45adccfb3188a305d5a72cff619d
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-10 10:40:22 +0200

    solve 349

--- src/main/java/org/shvetsov/_349.java
+++ src/main/java/org/shvetsov/_349.java
@@ -3,6 +3,11 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @LeetCode(
         number = 349,
         name = "Intersection of Two Arrays",
@@ -11,7 +16,31 @@ import org.shvetsov.core.Level;
 )
 public class _349 {
     public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
 
-        return null;
+        int f = 0;
+        int s = 0;
+
+        Set<Integer> res = new HashSet<>();
+
+        while (f < nums1.length && s < nums2.length) {
+            if (nums1[f] == nums2[s]) {
+                res.add(nums1[f]);
+                f++;
+                s++;
+            } else if (nums1[f] < nums2[s]) f++;
+            else s++;
+        }
+
+        return res.stream().mapToInt(Integer::intValue).toArray();
     }
+
+
+    public int[] intersection2(int[] nums1, int[] nums2) {
+        Set<Integer> list1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        Set<Integer> list2 = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+        return list1.stream().distinct().filter(list2::contains).mapToInt(Integer::intValue).toArray();
+    }
+
 }
--- src/test/java/org/shvetsov/_349Test.java
+++ src/test/java/org/shvetsov/_349Test.java
@@ -16,7 +16,13 @@ class _349Test {
 
     @Test
     void intersection() {
-        assertThat(task.intersection(new int[]{1, 2, 2, 1}, new int[]{1, 2, 2, 1})).containsExactly(2);
+        assertThat(task.intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2})).containsExactly(2);
         assertThat(task.intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})).containsExactlyInAnyOrder(4, 9);
+    }
+
+    @Test
+    void intersection2() {
+        assertThat(task.intersection2(new int[]{1, 2, 2, 1}, new int[]{2, 2})).containsExactly(2);
+        assertThat(task.intersection2(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})).containsExactlyInAnyOrder(4, 9);
     }
 }
