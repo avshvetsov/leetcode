7c3dd9fe8f98ce52de8e33563c2a2eba69e09f87
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-13 14:20:06 +0200

    solve 1171

--- src/main/java/org/shvetsov/_1171.java
+++ src/main/java/org/shvetsov/_1171.java
@@ -4,6 +4,8 @@ import org.shvetsov.classes.ListNode;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.*;
+
 @LeetCode(
         number = 1171,
         name = "Remove Zero Sum Consecutive Nodes from Linked List",
@@ -13,8 +15,45 @@ import org.shvetsov.core.Level;
 public class _1171 {
 
     public ListNode removeZeroSumSublists(ListNode head) {
-
-        return null;
+        if (head == null) {
+            return null;
+        }
+        ListNode curr = head;
+        int sum = 0;
+        while (curr != null) {
+            sum += curr.val;
+            if (sum == 0) {
+                return removeZeroSumSublists(curr.next);
+            }
+            curr = curr.next;
+        }
+        head.next = removeZeroSumSublists(head.next);
+        return head;
     }
+
+
+    public ListNode removeZeroSumSublistsInefficient(ListNode head) {
+        Map<Integer, ListNode> map = new HashMap<>();
+        ListNode curr = head;
+        int sum = 0;
+        while (curr != null) {
+            sum += curr.val;
+            if (sum == 0) {
+                map.clear();
+                head = curr.next;
+            } else {
+                if (map.containsKey(sum)) {
+                    map.get(sum).next = curr.next;
+                    map.clear();
+                    curr = head;
+                    sum = 0;
+                    continue;
+                } else  map.put(sum, curr);
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+
 
 }
--- src/test/java/org/shvetsov/_1171Test.java
+++ src/test/java/org/shvetsov/_1171Test.java
@@ -24,4 +24,36 @@ class _1171Test {
         assertThat(task.removeZeroSumSublists(constructListNode(1, 2, 3, -3, 4))).isEqualTo(constructListNode(1, 2, 4));
         assertThat(task.removeZeroSumSublists(constructListNode(1, 2, 3, -3, -2))).isEqualTo(constructListNode(1));
     }
+
+    @Test
+    void removeZeroSumSublists2() {
+        assertThat(task.removeZeroSumSublists(constructListNode(1, -1))).isEqualTo(null);
+    }
+
+    @Test
+    void removeZeroSumSublists3() {
+        assertThat(task.removeZeroSumSublists(constructListNode(5, -3, -4, 1, 6, -2, -5))).satisfiesAnyOf(
+                listNode -> assertThat(listNode).isEqualTo(constructListNode(5, -3, -4)),
+                listNode -> assertThat(listNode).isEqualTo(constructListNode(5, -2, -5))
+        );
+    }
+
+    @Test
+    void removeZeroSumSublists4() {
+        assertThat(task.removeZeroSumSublists(constructListNode(1, 2, 3, -4, 1, 5))).isEqualTo(constructListNode(1, 2, 5));
+    }
+
+    @Test
+    void removeZeroSumSublists5() {
+        assertThat(task.removeZeroSumSublists(constructListNode(2, 2, -2, 1, -1, -1))).isEqualTo(constructListNode(2, -1));
+    }
+
+    @Test
+    void removeZeroSumSublists6() {
+        assertThat(task.removeZeroSumSublists(constructListNode(1, 3, 2, -3, -2, 5, 5, -5, 1))).isEqualTo(constructListNode(1, 5, 1));
+    }
+    @Test
+    void removeZeroSumSublists7() {
+        assertThat(task.removeZeroSumSublists(constructListNode(1,0,2,-3))).isEqualTo(null);
+    }
 }
