08ab398a84bc8393bc9346cafa256f9102099f02
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-19 13:03:10 +0200

    solve 525

--- src/main/java/org/shvetsov/_525.java
+++ src/main/java/org/shvetsov/_525.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @LeetCode(
         number = 525,
         name = "Contiguous Array",
@@ -12,7 +15,19 @@ import org.shvetsov.core.Level;
 public class _525 {
 
     public int findMaxLength(int[] nums) {
-
-        return -1;
+        Map<Integer, Integer> sumIndex = new HashMap<>() {{
+            put(0, -1);
+        }};
+        int sum = 0;
+        int maxLength = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i] == 0 ? -1 : 1;
+            if (sumIndex.containsKey(sum)) {
+                maxLength = Math.max(maxLength, i - sumIndex.get(sum));
+            } else {
+                sumIndex.put(sum, i);
+            }
+        }
+        return maxLength;
     }
 }
--- src/test/java/org/shvetsov/_525Test.java
+++ src/test/java/org/shvetsov/_525Test.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class _525Test {
 
@@ -19,5 +18,11 @@ class _525Test {
     void findMaxLength() {
         assertThat(task.findMaxLength(new int[]{0, 1})).isEqualTo(2);
         assertThat(task.findMaxLength(new int[]{0, 1, 0})).isEqualTo(2);
+    }
+
+    @Test
+    void findMaxLength2() {
+        assertThat(task.findMaxLength(new int[]{1, 0, 0, 0, 1})).isEqualTo(2);
+        assertThat(task.findMaxLength(new int[]{1, 0, 0, 1, 0, 0, 1})).isEqualTo(4);
     }
 }
