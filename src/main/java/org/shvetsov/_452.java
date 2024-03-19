56298bdc42cc0aa467c2c237f11d92fd31391d0c
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-19 13:09:27 +0200

    setup 452

---
+++ src/main/java/org/shvetsov/_452.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 452,
+        name = "Minimum Number of Arrows to Burst Balloons",
+        level = Level.MEDIUM,
+        tags = {"Array", "Greedy", "Sorting"}
+)
+public class _452 {
+
+    public int findMinArrowShots(int[][] points) {
+
+        return -1;
+    }
+}
