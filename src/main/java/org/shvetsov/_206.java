fcdfcbb5d694d514f341ad32a20c0b16fb1b821b
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-21 10:06:39 +0200

    setup 206

---
+++ src/main/java/org/shvetsov/_206.java
@@ -0,0 +1,20 @@
+package org.shvetsov;
+
+import org.shvetsov.classes.ListNode;
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 206,
+        name = "Reverse Linked List",
+        level = Level.EASY,
+        tags = {"Linked List", "Recursion"}
+)
+public class _206 {
+
+    public ListNode reverseList(ListNode head) {
+
+        return null;
+    }
+
+}
