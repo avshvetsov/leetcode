7b4f330f3d5750caadc789df6f765f35176d578d
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-25 12:37:30 +0200

    solve 442

--- src/main/java/org/shvetsov/_442.java
+++ src/main/java/org/shvetsov/_442.java
@@ -15,6 +15,25 @@ public class _442 {
 
     //1 <= nums[i] <= n
     public List<Integer> findDuplicates(int[] nums) {
-        return null;
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int pos = Math.abs(nums[i]);
+            if (nums[pos - 1] < 0) {
+                res.add(pos);
+            }
+            nums[pos - 1] *= -1;
+        }
+        return res;
+    }
+
+    public List<Integer> findDuplicates2(int[] nums) {
+        Set<Integer> cache = new HashSet<>();
+        List<Integer> res = new ArrayList<>();
+        for (int num : nums) {
+            if (!cache.add(num)) {
+                res.add(num);
+            }
+        }
+        return res;
     }
 }
