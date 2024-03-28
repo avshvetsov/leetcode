878db682f8d471946569a14a0e4001ca3872982f
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-28 11:48:05 +0200

    solve 713

--- src/main/java/org/shvetsov/_713.java
+++ src/main/java/org/shvetsov/_713.java
@@ -10,8 +10,60 @@ import org.shvetsov.core.Level;
         tags = {"Array", "Sliding Window"}
 )
 public class _713 {
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
 
-        return 0;
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
+        int l = 0;
+        int r = 0;
+        int prod = 1;
+        int res = 0;
+        while (r < nums.length) {
+            prod *= nums[r];
+            while (prod >= k) {
+                prod /= nums[l];
+                l++;
+            }
+            res = res + r - l + 1;
+            r++;
+        }
+        return res;
+    }
+
+
+    public int numSubarrayProductLessThanKInefficient(int[] nums, int k) {
+        int l = 0;
+        int r = 0;
+        int prod = nums[l];
+        int res = 0;
+        while (l < nums.length - 1) {
+            if (prod < k) {
+                res++;
+                r++;
+                if (r < nums.length) {
+                    prod *= nums[r];
+                } else {
+                    l++;
+                    r = l;
+                    prod = nums[l];
+                }
+            } else {
+                if (l < r) {
+                    l++;
+                    r = l;
+                    prod = nums[l];
+                } else {
+                    r++;
+                    l++;
+                    prod = nums[l];
+                }
+            }
+
+        }
+        if (prod < k) {
+            res++;
+        }
+        return res;
     }
 }
--- src/test/java/org/shvetsov/_713Test.java
+++ src/test/java/org/shvetsov/_713Test.java
@@ -13,9 +13,10 @@ class _713Test {
     public void setup() {
         task = new _713();
     }
+
     @Test
     void numSubarrayProductLessThanK() {
         assertThat(task.numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 100)).isEqualTo(8);
-        assertThat(task.numSubarrayProductLessThanK(new int[]{1,2,3}, 0)).isEqualTo(0);
+        assertThat(task.numSubarrayProductLessThanK(new int[]{1, 2, 3}, 0)).isEqualTo(0);
     }
 }
