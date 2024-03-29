fb36bad2eae69e27d90840f06e0fa522c1790f52
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-03-29 10:00:07 +0200

    setup 2962

---
+++ src/main/java/org/shvetsov/_2962.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2962,
+        name = "Count Subarrays Where Max Element Appears at Least K Times",
+        level = Level.MEDIUM,
+        tags = {"Array", "Sliding Window"}
+)
+public class _2962 {
+    public long countSubarrays(int[] nums, int k) {
+
+        return 0;
+    }
+}
