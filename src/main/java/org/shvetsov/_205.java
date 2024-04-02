c3bb36c906d80a93f7e4bd4c9182efa59fc3613f
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-02 10:12:20 +0300

    setup 205

---
+++ src/main/java/org/shvetsov/_205.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 205,
+        name = "Isomorphic Strings",
+        level = Level.EASY,
+        tags = {"Hash Table", "String"}
+)
+public class _205 {
+    public boolean isIsomorphic(String s, String t) {
+
+        return false;
+    }
+}
