72c714f11f468f483c65a9ad59cfc18552b8aec4
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-07 12:16:12 +0300

    solve 678

--- src/main/java/org/shvetsov/_678.java
+++ src/main/java/org/shvetsov/_678.java
@@ -11,7 +11,50 @@ import org.shvetsov.core.Level;
 )
 public class _678 {
     public boolean checkValidString(String s) {
+        char[] chars = s.toCharArray();
+        int countL = 0;
+        int countS = 0;
+        int countR = 0;
+        for (int i = 0; i < chars.length; i++) {
+            switch (chars[i]) {
+                case '(' -> countL++;
+                case '*' -> countS++;
+                case ')' -> {
+                    if (countL + countS == 0) {
+                        return false;
+                    } else {
+                        if (countL > 0) {
+                            countL--;
+                        } else {
+                            countS--;
+                        }
+                    }
+                }
+                default -> throw new IllegalArgumentException();
+            }
+        }
 
-        return false;
+        countL = 0;
+        countS = 0;
+        countR = 0;
+        for (int i = chars.length - 1; i >= 0; i--) {
+            switch (chars[i]) {
+                case '(' -> {
+                    if (countR + countS == 0) {
+                        return false;
+                    } else {
+                        if (countR > 0) {
+                            countR--;
+                        } else {
+                            countS--;
+                        }
+                    }
+                }
+                case '*' -> countS++;
+                case ')' -> countR++;
+                default -> throw new IllegalArgumentException();
+            }
+        }
+        return true;
     }
 }
