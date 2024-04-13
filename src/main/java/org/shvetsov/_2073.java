67ac4fc5037d9a1ec3c40d1c550f53ad0a0c6d99
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-13 10:20:22 +0300

    setup 2073

---
+++ src/main/java/org/shvetsov/_2073.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2073,
+        name = "Time Needed to Buy Tickets",
+        level = Level.EASY,
+        tags = {"Array", "Queue", "Simulation"}
+)
+public class _2073 {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+
+        return 0;
+    }
+}
