6236c6aa07988731fabf32267d0905d5ac5807fa
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-15 13:05:09 +0300

    TreeNode fix

--- src/main/java/org/shvetsov/classes/TreeNode.java
+++ src/main/java/org/shvetsov/classes/TreeNode.java
@@ -1,18 +1,18 @@
 package org.shvetsov.classes;
 
 public class TreeNode {
-    public Integer val;
+    public int val;
     public TreeNode left;
     public TreeNode right;
 
     public TreeNode() {
     }
 
-    public TreeNode(Integer val) {
+    public TreeNode(int val) {
         this.val = val;
     }
 
-    public TreeNode(Integer val, TreeNode left, TreeNode right) {
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
