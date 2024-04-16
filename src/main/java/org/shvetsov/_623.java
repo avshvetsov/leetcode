e9348a6bdf9d2312be7ff23e90f792b72bcbdddd
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-16 09:18:11 +0300

    setup 623

---
+++ src/main/java/org/shvetsov/_623.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.classes.TreeNode;
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 623,
+        name = "Add One Row to Tree",
+        level = Level.MEDIUM,
+        tags = {"Tree", "Depth-First Search", "Breadth-First Search", "Binary Tree"}
+)
+public class _623 {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+
+        return new TreeNode();
+    }
+}
