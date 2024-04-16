bcaca321b57ad33d00377782a2b9be401351ce10
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-16 10:35:03 +0300

    solve 623

--- src/main/java/org/shvetsov/_623.java
+++ src/main/java/org/shvetsov/_623.java
@@ -12,7 +12,25 @@ import org.shvetsov.core.Level;
 )
 public class _623 {
     public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        depthSearchHelper(root, val, depth);
+        return root;
+    }
 
-        return new TreeNode();
+    private void depthSearchHelper(TreeNode root, int val, int depth) {
+        if(root == null) return;
+        if (depth == 2) {
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+
+            root.left = new TreeNode(val, left, null);
+            root.right = new TreeNode(val, null, right);
+            return;
+        }
+        depth--;
+        depthSearchHelper(root.left, val, depth);
+        depthSearchHelper(root.right, val, depth);
     }
 }
--- src/test/java/org/shvetsov/_623Test.java
+++ src/test/java/org/shvetsov/_623Test.java
@@ -2,9 +2,11 @@ package org.shvetsov;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.shvetsov.classes.TreeNode;
 import org.shvetsov.utils.TreeUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.shvetsov.utils.TreeUtils.*;
 
 class _623Test {
 
@@ -17,9 +19,9 @@ class _623Test {
 
     @Test
     void addOneRow() {
-        assertThat(task.addOneRow(TreeUtils.constructTreeNode(4, 2, 6, 3, 1, 5), 1, 2))
-                .isEqualTo(TreeUtils.constructTreeNode(4, 1, 1, 2, null, null, 6, 3, 1, 5));
-        assertThat(task.addOneRow(TreeUtils.constructTreeNode(4, 2, null, 3, 1), 1, 3))
-                .isEqualTo(TreeUtils.constructTreeNode(4, 2, null, 1, 1, 3, null, null, 1));
+        assertThat(task.addOneRow(constructTreeNode(4, 2, 6, 3, 1, 5), 1, 2))
+                .isEqualTo(constructTreeNode(4, 1, 1, 2, null, null, 6, 3, 1, 5));
+        assertThat(task.addOneRow(constructTreeNode(4, 2, null, 3, 1), 1, 3))
+                .isEqualTo(constructTreeNode(4, 2, null, 1, 1, 3, null, null, 1));
     }
 }
