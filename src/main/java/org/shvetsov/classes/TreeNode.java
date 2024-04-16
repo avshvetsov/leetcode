40f7a810f1680801289a832468a1eeafa4eb3d71
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-16 10:34:23 +0300

    TreeNodeUtils fix

--- src/main/java/org/shvetsov/classes/TreeNode.java
+++ src/main/java/org/shvetsov/classes/TreeNode.java
@@ -1,5 +1,7 @@
 package org.shvetsov.classes;
 
+import java.util.Objects;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -16,6 +18,22 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TreeNode treeNode)) return false;
+
+        return val == treeNode.val && Objects.equals(left, treeNode.left) && Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = val;
+        result = 31 * result + Objects.hashCode(left);
+        result = 31 * result + Objects.hashCode(right);
+        return result;
     }
 }
 
--- src/main/java/org/shvetsov/utils/TreeUtils.java
+++ src/main/java/org/shvetsov/utils/TreeUtils.java
@@ -3,24 +3,45 @@ package org.shvetsov.utils;
 
 import org.shvetsov.classes.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeUtils {
 
     private TreeUtils() {
     }
 
-    public static TreeNode constructTreeNode(Integer... array) {
-        return constructTreeHelper(array, 0);
-    }
+//    public static TreeNode constructTreeNode(Integer... array) {
+//        return constructTreeHelper(array, 0);
+//    }
 
-    private static TreeNode constructTreeHelper(Integer[] arr, int index) {
-        if (index >= arr.length || arr[index] == null) {
-            return null;
+//    private static TreeNode constructTreeHelper(Integer[] arr, int index) {
+//        if (index >= arr.length || arr[index] == null) {
+//            return null;
+//        }
+//
+//        TreeNode root = new TreeNode(arr[index]);
+//        root.left = constructTreeHelper(arr, 2 * index + 1);
+//        root.right = constructTreeHelper(arr, 2 * index + 2);
+//
+//        return root;
+//    }
+
+    public static TreeNode constructTreeNode(Integer... treeValues) {
+        TreeNode root = new TreeNode(treeValues[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        for (int i = 1; i < treeValues.length; i++) {
+            TreeNode curr = queue.poll();
+            if (treeValues[i] != null) {
+                curr.left = new TreeNode(treeValues[i]);
+                queue.offer(curr.left);
+            }
+            if (++i < treeValues.length && treeValues[i] != null) {
+                curr.right = new TreeNode(treeValues[i]);
+                queue.offer(curr.right);
+            }
         }
-
-        TreeNode root = new TreeNode(arr[index]);
-        root.left = constructTreeHelper(arr, 2 * index + 1);
-        root.right = constructTreeHelper(arr, 2 * index + 2);
-
         return root;
     }
 }
