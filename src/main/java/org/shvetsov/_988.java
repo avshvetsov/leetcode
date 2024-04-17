76da57ffc4164169a68f936533d5bd7766ab1d7f
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-17 09:29:21 +0300

    setup 988

---
+++ src/main/java/org/shvetsov/_988.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.classes.TreeNode;
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 988,
+        name = "Smallest String Starting From Leaf",
+        level = Level.MEDIUM,
+        tags = {"String", "Tree", "Depth-First Search", "Binary Tree"}
+)
+public class _988 {
+    public String smallestFromLeaf(TreeNode root) {
+
+        return "";
+    }
+}
---
+++ src/test/java/org/shvetsov/_988Test.java
@@ -0,0 +1,25 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.shvetsov.utils.TreeUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _988Test {
+
+    private static _988 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _988();
+    }
+
+
+    @Test
+    void smallestFromLeaf() {
+        assertThat(task.smallestFromLeaf(TreeUtils.constructTreeNode(0, 1, 2, 3, 4, 3, 4))).isEqualTo("dba");
+        assertThat(task.smallestFromLeaf(TreeUtils.constructTreeNode(25, 1, 3, 1, 3, 0, 2))).isEqualTo("adz");
+        assertThat(task.smallestFromLeaf(TreeUtils.constructTreeNode(2, 2, 1, null, 1, 0, null, 0))).isEqualTo("abc");
+    }
+}
