632bb4a90860cd95dcef2cab5031396421896a3b
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-18 10:18:50 +0300

    solve 463

--- src/main/java/org/shvetsov/_463.java
+++ src/main/java/org/shvetsov/_463.java
@@ -13,7 +13,16 @@ public class _463 {
 
     public int islandPerimeter(int[][] grid) {
         int result = 0;
-
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    if (i - 1 < 0 || grid[i - 1][j] == 0) result++;
+                    if (i + 1 >= grid.length || grid[i + 1][j] == 0) result++;
+                    if (j - 1 < 0 || grid[i][j - 1] == 0) result++;
+                    if (j + 1 >= grid[0].length || grid[i][j + 1] == 0) result++;
+                }
+            }
+        }
         return result;
     }
 }
