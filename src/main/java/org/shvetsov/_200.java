4dafdabafbf2ec997aac77548a49da3df49c54ec
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-19 17:35:51 +0300

    solve 200

--- src/main/java/org/shvetsov/_200.java
+++ src/main/java/org/shvetsov/_200.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @LeetCode(
         number = 200,
         name = "Number of Islands",
@@ -10,8 +13,63 @@ import org.shvetsov.core.Level;
         tags = {"Array", "Depth-First Search", "Breadth-First Search", "Union Find", "Matrix"}
 )
 public class _200 {
-    public int numIslands(char[][] grid) {
 
-        return 0;
+    public int numIslands(char[][] grid) {
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    findFullIsland(grid, i, j);
+                    result++;
+                }
+            }
+        }
+        return result;
     }
+
+    private void findFullIsland(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
+            return;
+        } else {
+            grid[i][j] = '0';
+            findFullIsland(grid, i - 1, j);
+            findFullIsland(grid, i + 1, j);
+            findFullIsland(grid, i, j - 1);
+            findFullIsland(grid, i, j + 1);
+        }
+    }
+
+
+    public static class WithSet {
+        private final Set<Point> used = new HashSet<>();
+
+        public int numIslands(char[][] grid) {
+            int result = 0;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (grid[i][j] == '1' && !used.contains(new Point(i, j))) {
+                        findFullIsland(grid, i, j);
+                        result++;
+                    }
+                }
+            }
+            used.clear();
+            return result;
+        }
+
+        private void findFullIsland(char[][] grid, int i, int j) {
+            if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1' || used.contains(new Point(i, j))) {
+                return;
+            } else {
+                used.add(new Point(i, j));
+                findFullIsland(grid, i - 1, j);
+                findFullIsland(grid, i + 1, j);
+                findFullIsland(grid, i, j - 1);
+                findFullIsland(grid, i, j + 1);
+            }
+        }
+
+        record Point(int x, int y) {}
+    }
+
 }
