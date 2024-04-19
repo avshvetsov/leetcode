240aa845c73aeae9e3a9d4813bdaa3b8233cee1b
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-19 10:01:56 +0300

    setup 200

---
+++ src/main/java/org/shvetsov/_200.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 200,
+        name = "Number of Islands",
+        level = Level.MEDIUM,
+        tags = {"Array", "Depth-First Search", "Breadth-First Search", "Union Find", "Matrix"}
+)
+public class _200 {
+    public int numIslands(char[][] grid) {
+
+        return 0;
+    }
+}
