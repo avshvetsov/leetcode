77ffbccf2e961dbed1f576f65f8c4cd18bf2b9fe
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-19 17:42:34 +0300

    setup 12

---
+++ src/main/java/org/shvetsov/_12.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 12,
+        name = "Integer to Roman",
+        level = Level.MEDIUM,
+        tags = {"Hash Table", "Math", "String"}
+)
+public class _12 {
+    public String intToRoman(int num) {
+
+        return "";
+    }
+}
