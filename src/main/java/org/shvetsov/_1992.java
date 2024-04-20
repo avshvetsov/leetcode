340a7774a5f427a047688f0074d1d64887a07d0b
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-20 10:42:18 +0300

    solve 1992

--- src/main/java/org/shvetsov/_1992.java
+++ src/main/java/org/shvetsov/_1992.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @LeetCode(
         number = 1992,
         name = "Find All Groups of Farmland",
@@ -11,7 +14,26 @@ import org.shvetsov.core.Level;
 )
 public class _1992 {
     public int[][] findFarmland(int[][] land) {
+        List<int[]> result = new ArrayList<>();
+        for (int i = 0; i < land.length; i++) {
+            for (int j = 0; j < land[0].length; j++) {
+                if (land[i][j] == 1) {
+                    result.add(findFullLand(land, i, j));
+                }
+            }
+        }
+        return result.toArray(new int[][]{});
+    }
 
-        return new int[][]{};
+    private int[] findFullLand(int[][] land, int i, int j) {
+        int[] res = new int[]{i, j, -1, -1};
+        for (int i1 = i; i1 < land.length && land[i1][j] == 1; i1++) {
+            for (int j1 = j; j1 < land[i1].length && land[i1][j1] == 1; j1++) {
+                land[i1][j1] = 0;
+                res[3] = j1;
+            }
+            res[2] = i1;
+        }
+        return res;
     }
 }
