02c4bfa5f3621fcdb8cb07c25b496d80e329e45a
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-21 15:59:04 +0300

    solve 1971

--- src/main/java/org/shvetsov/_1971.java
+++ src/main/java/org/shvetsov/_1971.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.*;
+
 @LeetCode(
         number = 1971,
         name = "Find if Path Exists in Graph",
@@ -10,8 +12,150 @@ import org.shvetsov.core.Level;
         tags = {"Depth-First Search", "Breadth-First Search", "Union Find", "Graph"}
 )
 public class _1971 {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
 
-        return false;
+    //graph visualizer: https://csacademy.com/app/graph_editor/
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if (edges.length == 0) return true;
+        boolean result = false;
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < edges.length; i++) {
+            if (graph.containsKey(edges[i][0])) {
+                graph.get(edges[i][0]).add(edges[i][1]);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(edges[i][1]);
+                graph.put(edges[i][0], list);
+            }
+
+            if (graph.containsKey(edges[i][1])) {
+                graph.get(edges[i][1]).add(edges[i][0]);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(edges[i][0]);
+                graph.put(edges[i][1], list);
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(source);
+        Set<Integer> visited = new HashSet<>();
+        visited.add(source);
+        while (!queue.isEmpty()) {
+            Integer curr = queue.poll();
+            List<Integer> next = graph.get(curr);
+            if (next != null) {
+                if (next.contains(destination)) {
+                    result = true;
+                    break;
+                }
+                next.removeIf(visited::contains);
+                //можем помечать их как visited, потому что они и так добавлены в queue и соответственно через них будет проход
+                visited.addAll(next);
+                queue.addAll(next);
+            }
+        }
+        return result;
+    }
+
+
+    public static class BFS {
+        public boolean validPath(int n, int[][] edges, int source, int destination) {
+            Map<Integer, List<Integer>> graph = new HashMap<>();
+            for (int[] edge : edges) {
+                int f = edge[0];
+                int t = edge[1];
+                graph.computeIfAbsent(f, k -> new ArrayList<>()).add(t);
+                graph.computeIfAbsent(t, k -> new ArrayList<>()).add(f);
+            }
+            Queue<Integer> queue = new LinkedList<>();
+            boolean[] visited = new boolean[n];
+
+            queue.offer(source);
+            visited[source] = true;
+
+            while (!queue.isEmpty()) {
+                int node = queue.poll();
+                if (node == destination) {
+                    return true;
+                }
+                List<Integer> neighbors = graph.getOrDefault(node, new ArrayList<>());
+                for (int neighbor : neighbors) {
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        queue.offer(neighbor);
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public static class DFS{
+        public boolean validPath(int n, int[][] edges, int source, int destination) {
+            Map<Integer, List<Integer>> graph = new HashMap<>();
+            for (int[] edge : edges) {
+                int f = edge[0];
+                int t = edge[1];
+                graph.computeIfAbsent(f, k -> new ArrayList<>()).add(t);
+                graph.computeIfAbsent(t, k -> new ArrayList<>()).add(f);
+            }
+            boolean[] visited = new boolean[n];
+
+            return validPathHelper(graph, source, destination, visited);
+        }
+
+        private boolean validPathHelper(Map<Integer, List<Integer>> graph, int current, int destination, boolean[] visited) {
+            if (current == destination) {
+                return true;
+            }
+            visited[current] = true;
+            for (int neighbor : graph.get(current)) {
+                if (!visited[neighbor]) {
+                    if (validPathHelper(graph, neighbor, destination, visited)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public static class UnionFind {
+        private int[] parent;
+        private int[] rank;
+
+        public boolean validPath(int n, int[][] edges, int source, int destination) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+            for (int[] edge : edges) {
+                union(edge[0], edge[1]);
+            }
+            return find(source) == find(destination);
+        }
+
+        private int find(int val) {
+            if (parent[val] != val) {
+                parent[val] = find(parent[val]);
+            }
+            return parent[val];
+        }
+
+        private void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                rank[rootX]++;
+                parent[rootY] = rootX;
+            }
+        }
     }
 }
--- src/test/java/org/shvetsov/_1971Test.java
+++ src/test/java/org/shvetsov/_1971Test.java
@@ -8,15 +8,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _1971Test {
 
     private static _1971 task;
+    private static _1971.BFS taskBFS;
+    private static _1971.DFS taskDFS;
+    private static _1971.UnionFind taskUnionFind;
 
     @BeforeEach
     public void setup() {
         task = new _1971();
+        taskBFS = new _1971.BFS();
+        taskDFS = new _1971.DFS();
+        taskUnionFind = new _1971.UnionFind();
     }
 
     @Test
     void validPath() {
-        assertThat(task.validPath(3, new int[][]{{0,1},{1,2},{2,0}}, 0, 2)).isTrue();
-        assertThat(task.validPath(6, new int[][]{{0,1},{0,2},{3,5},{5,4},{4,3}}, 0, 5)).isFalse();
+        assertThat(task.validPath(3, new int[][]{{0, 1}, {1, 2}, {2, 0}}, 0, 2)).isTrue();
+        assertThat(task.validPath(6, new int[][]{{0, 1}, {0, 2}, {3, 5}, {5, 4}, {4, 3}}, 0, 5)).isFalse();
+    }
+
+
+    @Test
+    void validPathBFS() {
+        assertThat(taskBFS.validPath(3, new int[][]{{0, 1}, {1, 2}, {2, 0}}, 0, 2)).isTrue();
+        assertThat(taskBFS.validPath(6, new int[][]{{0, 1}, {0, 2}, {3, 5}, {5, 4}, {4, 3}}, 0, 5)).isFalse();
+    }
+
+    @Test
+    void validPathDFS() {
+        assertThat(taskDFS.validPath(3, new int[][]{{0, 1}, {1, 2}, {2, 0}}, 0, 2)).isTrue();
+        assertThat(taskDFS.validPath(6, new int[][]{{0, 1}, {0, 2}, {3, 5}, {5, 4}, {4, 3}}, 0, 5)).isFalse();
+    }
+
+    @Test
+    void validPathUnionFind() {
+        assertThat(taskUnionFind.validPath(3, new int[][]{{0, 1}, {1, 2}, {2, 0}}, 0, 2)).isTrue();
+        assertThat(taskUnionFind.validPath(6, new int[][]{{0, 1}, {0, 2}, {3, 5}, {5, 4}, {4, 3}}, 0, 5)).isFalse();
+        assertThat(taskUnionFind.validPath(10, new int[][]{{2, 6}, {4, 7}, {1, 2}, {3, 5}, {7, 9}, {6, 4}, {9, 8}, {0, 1}, {3, 0}}, 3, 5)).isTrue();
     }
 }
