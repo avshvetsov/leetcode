bf5464e0a5ff7e0acdc00fc6d89d43129de3168f
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-24 10:27:54 +0300

    solve 1137

--- src/main/java/org/shvetsov/_1137.java
+++ src/main/java/org/shvetsov/_1137.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 1137,
         name = "N-th Tribonacci Number",
@@ -10,8 +12,47 @@ import org.shvetsov.core.Level;
         tags = {"Math", "Dynamic Programming", "Memoization"}
 )
 public class _1137 {
-    public int tribonacci(int n) {
 
-        return 0;
+    public int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int res = 0;
+        int a = 0, b = 1, c = 1;
+        for (int i = 3; i <= n; i++) {
+            res = a + b + c;
+            a = b;
+            b = c;
+            c = res;
+        }
+        return res;
     }
+
+
+    public static class MemoRecursion {
+        int[] cache = new int[38];
+        {
+            Arrays.fill(cache, -1);
+            cache[0] = 0;
+            cache[1] = 1;
+            cache[2] = 1;
+        }
+
+        public int tribonacci(int n) {
+            if (cache[n] != -1) {
+                return cache[n];
+            }
+            if (cache[n - 1] == -1) {
+                cache[n - 1] = tribonacci(n - 1);
+            }
+            if (cache[n - 2] == -1) {
+                cache[n - 2] = tribonacci(n - 2);
+            }
+            if (cache[n - 3] == -1) {
+                cache[n - 3] = tribonacci(n - 3);
+            }
+            return cache[n - 1] + cache[n - 2] + cache[n - 3];
+        }
+    }
+
 }
--- src/test/java/org/shvetsov/_1137Test.java
+++ src/test/java/org/shvetsov/_1137Test.java
@@ -8,15 +8,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _1137Test {
 
     private static _1137 task;
+    private static _1137.MemoRecursion taskMemoRecursion;
 
     @BeforeEach
     public void setup() {
         task = new _1137();
+        taskMemoRecursion = new _1137.MemoRecursion();
     }
 
     @Test
     void tribonacci() {
         assertThat(task.tribonacci(4)).isEqualTo(4);
         assertThat(task.tribonacci(25)).isEqualTo(1389537);
+    }
+
+    @Test
+    void tribonacciMemoRecursion() {
+        assertThat(taskMemoRecursion.tribonacci(4)).isEqualTo(4);
+        assertThat(taskMemoRecursion.tribonacci(25)).isEqualTo(1389537);
     }
 }
