cd522be67251bbfa635438c5cdd009934a2f937d
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-25 09:23:39 +0300

    setup 2370

---
+++ src/main/java/org/shvetsov/_2370.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2370,
+        name = "Longest Ideal Subsequence",
+        level = Level.MEDIUM,
+        tags = {"Hash Table", "String", "Dynamic Programming"}
+)
+public class _2370 {
+
+    public int longestIdealString(String s, int k) {
+
+        return 0;
+    }
+}
