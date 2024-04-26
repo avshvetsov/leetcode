7881ba0ae60aefb388afd8a3d6b58332d30e468d
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-26 09:22:09 +0300

    setup 1289

---
+++ src/main/java/org/shvetsov/_1289.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1289,
+        name = "Minimum Falling Path Sum II",
+        level = Level.HARD,
+        tags = {"Array", "Dynamic Programming", "Matrix"}
+)
+public class _1289 {
+    public int minFallingPathSum(int[][] grid) {
+
+        return 0;
+    }
+}
