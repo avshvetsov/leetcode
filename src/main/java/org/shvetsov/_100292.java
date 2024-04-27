f2cd129d7c78de2efa15961f14a4b9240c5da0e5
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-27 18:10:39 +0300

    setup 100292

---
+++ src/main/java/org/shvetsov/_100292.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 100292,
+        name = "Find All Possible Stable Binary Arrays I",
+        level = Level.MEDIUM,
+        tags = {}
+)
+public class _100292 {
+    public int numberOfStableArrays(int zero, int one, int limit) {
+
+        return 0;
+    }
+}
