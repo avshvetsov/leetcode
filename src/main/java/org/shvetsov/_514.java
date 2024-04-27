0632ad65c7b9b9dded6814de252d49818bdb0879
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-27 23:10:36 +0300

    solve 514

--- src/main/java/org/shvetsov/_514.java
+++ src/main/java/org/shvetsov/_514.java
@@ -1,7 +1,10 @@
 package org.shvetsov;
 
+import org.apache.commons.math3.util.Pair;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
+
+import java.util.*;
 
 @LeetCode(
         number = 514,
@@ -11,7 +14,74 @@ import org.shvetsov.core.Level;
 )
 public class _514 {
     public int findRotateSteps(String ring, String key) {
+        int[][] distances = new int[ring.length()][ring.length()];
+        int n = ring.length();
+        for (int i = 0; i < distances.length; i++) {
+            int ii = 1;
+            while (ii <= n - ii) {
+                distances[i][(i + ii) % n] = ii;
+                distances[i][(n + i - ii) % n] = ii;
+                ii++;
+            }
+        }
 
-        return 0;
+        List<Pair<Integer, Integer>> states = new ArrayList<>();
+        states.add(new Pair<>(0, 0));
+        for (char c : key.toCharArray()) {
+            List<Pair<Integer, Integer>> prevStates = new ArrayList<>(states);
+            states = new ArrayList<>();
+
+            int index = ring.indexOf(c);
+            while (index >= 0) {
+                int min = Integer.MAX_VALUE;
+                for (Pair<Integer, Integer> prevState : prevStates) {
+                    min = Math.min(min, prevState.getValue() + distances[prevState.getKey()][index]);
+                }
+                states.add(new Pair<>(index, min));
+                index = ring.indexOf(c, index + 1);
+            }
+        }
+
+        int res = states.stream()
+                .mapToInt(Pair::getValue)
+                .min()
+                .orElseThrow();
+        return res + key.length();
     }
+
+
+
+    public static class Improvements {
+        public int findRotateSteps(String ring, String key) {
+            char[] r = ring.toCharArray();
+            List<Integer>[] positions = new List[26];
+            for (int i = 0; i < r.length; i++) {
+                int c = r[i] - 'a';
+                if (positions[c] == null) positions[c] = new ArrayList<>();
+                positions[c].add(i);
+            }
+
+            List<int[]> states = new ArrayList<>();
+            states.add(new int[]{0, 0});
+            for (char c : key.toCharArray()) {
+                List<int[]> prevStates = new ArrayList<>(states);
+                states = new ArrayList<>();
+
+                for (Integer index : positions[c - 'a']) {
+                    int min = Integer.MAX_VALUE;
+                    for (int[] prevState : prevStates) {
+                        min = Math.min(min, prevState[1] + Math.min(Math.abs(index - prevState[0]), ring.length() - Math.abs(index - prevState[0])));
+                    }
+                    states.add(new int[]{index, min});
+                }
+            }
+
+            int res = Integer.MAX_VALUE;
+            for (int[] state : states) {
+                res = Math.min(res, state[1]);
+            }
+            return res + key.length();
+        }
+    }
+
 }
--- src/test/java/org/shvetsov/_514Test.java
+++ src/test/java/org/shvetsov/_514Test.java
@@ -8,15 +8,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _514Test {
 
     private static _514 task;
+    private static _514.Improvements taskImprovements;
 
     @BeforeEach
     public void setup() {
         task = new _514();
+        taskImprovements = new _514.Improvements();
     }
 
     @Test
     void findRotateSteps() {
         assertThat(task.findRotateSteps("godding", "gd")).isEqualTo(4);
         assertThat(task.findRotateSteps("godding", "godding")).isEqualTo(13);
+    }
+
+    @Test
+    void findRotateStepsImproovments() {
+        assertThat(taskImprovements.findRotateSteps("godding", "gd")).isEqualTo(4);
+        assertThat(taskImprovements.findRotateSteps("godding", "godding")).isEqualTo(13);
     }
 }
