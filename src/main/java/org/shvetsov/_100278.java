d62da4aa81722e152bbd17e974857dfa6e0185e5
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-27 17:53:50 +0300

    setup 100278

---
+++ src/main/java/org/shvetsov/_100278.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 100278,
+        name = "Right Triangles",
+        level = Level.MEDIUM,
+        tags = {}
+)
+public class _100278 {
+    public long numberOfRightTriangles(int[][] grid) {
+
+        return 0;
+    }
+}
