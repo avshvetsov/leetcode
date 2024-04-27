cdd032ed190d4abe2610e5deb9cc92ee786b2301
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-27 20:07:51 +0300

    solve 3129

--- src/main/java/org/shvetsov/_3129.java
+++ src/main/java/org/shvetsov/_3129.java
@@ -11,47 +11,51 @@ import org.shvetsov.core.Level;
 )
 public class _3129 {
     int lim = 0;
+    int[][][][] cache = new int[][][][]{};
+
     public int numberOfStableArrays(int zero, int one, int limit) {
         lim = limit;
-        long res = countHelper(zero, one, limit, 0);
-
+        long res = 0;
+        int[][][][] cache = new int[zero + 1][one + 1][limit + 1][2];
+        for (int i = 0; i <= zero; i++) {
+            for (int j = 0; j <= one; j++) {
+                for (int l = 0; l <= limit; l++) {
+                    cache[i][j][l][0] = -1;
+                    cache[i][j][l][1] = -1;
+                }
+            }
+        }
+        this.cache = cache;
+        res += countHelper(zero - 1, one, limit - 1, 0);
+        res += countHelper(zero, one - 1, limit - 1, 1);
         return (int) res % 1000000007;
     }
 
-    private long countHelper(int zero, int one, int limit, int last) {
+    private int countHelper(int zero, int one, int limit, int last) {
+        if (cache[zero][one][limit][last] != -1) {
+            return cache[zero][one][limit][last];
+        }
+        if (zero == 0 && one == 0) {
+            return 1;
+        }
         long zeroRes = 0;
         long oneRes = 0;
-        if (zero == 0) {
-            if (one <= limit) return 1;
-            else return 0;
-        } else if (zero > 0) {
+
+        if (zero > 0) {
             if (last == 0 && limit > 0) {
                 zeroRes = countHelper(zero - 1, one, limit - 1, 0);
             } else if (last == 1) {
                 zeroRes = countHelper(zero - 1, one, lim - 1, 0);
-            } else if (limit == 0) {
-                zeroRes = 0;
             }
         }
-        if (one == 0) {
-            if (zero <= limit) return 1;
-            else return 0;
-        } else if (one > 0) {
+        if (one > 0) {
             if (last == 1 && limit > 0) {
                 oneRes = countHelper(zero, one - 1, limit - 1, 1);
             } else if (last == 0) {
                 oneRes = countHelper(zero, one - 1, lim - 1, 1);
-            } else if (limit == 0) {
-                oneRes = 0;
             }
         }
-        if (zeroRes > 0 && oneRes > 0) {
-            return zeroRes + oneRes;
-        } else if (zero > 0) {
-            return zero;
-        } else if (one > 0) {
-            return oneRes;
-        }
-        return 0;
+        cache[zero][one][limit][last] = (int) (zeroRes + oneRes) % 1000000007;
+        return (int) (zeroRes + oneRes) % 1000000007;
     }
 }
