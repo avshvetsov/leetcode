5381d5fdc302d8e3ecc36536d4d53eb3dbc2e148
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-27 10:20:04 +0300

    setup 514

---
+++ src/main/java/org/shvetsov/_514.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 514,
+        name = "Freedom Trail",
+        level = Level.HARD,
+        tags = {"String", "Dynamic Programming", "Depth-First Search", "Breadth-First Search"}
+)
+public class _514 {
+    public int findRotateSteps(String ring, String key) {
+
+        return 0;
+    }
+}
