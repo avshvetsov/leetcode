b537d4a73dc345116d21a5ad9516e929e7e0464f
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-28 10:37:05 +0300

    setup 35

---
+++ src/test/java/org/shvetsov/_35Test.java
@@ -0,0 +1,23 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _35Test {
+
+    private static _35 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _35();
+    }
+
+    @Test
+    void searchInsert() {
+        assertThat(task.searchInsert(new int[]{1, 3, 5, 6}, 5)).isEqualTo(2);
+        assertThat(task.searchInsert(new int[]{1, 3, 5, 6}, 2)).isEqualTo(1);
+        assertThat(task.searchInsert(new int[]{1, 3, 5, 6}, 7)).isEqualTo(4);
+    }
+}
