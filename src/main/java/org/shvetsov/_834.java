981d9882f193807295f8ec6de6095956efeb6d6e
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-04-28 22:17:47 +0300

    solve 834 but with 'Memory Limit Exceeded'

--- src/main/java/org/shvetsov/_834.java
+++ src/main/java/org/shvetsov/_834.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.*;
+
 @LeetCode(
         number = 834,
         name = "Sum of Distances in Tree",
@@ -10,8 +12,94 @@ import org.shvetsov.core.Level;
         tags = {"Dynamic Programming", "Tree", "Depth-First Search", "Graph"}
 )
 public class _834 {
-    public int[] sumOfDistancesInTree(int n, int[][] edges) {
 
-        return new int[n];
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+        Set<Integer>[] graph = new Set[n];
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new HashSet<>();
+        }
+        for (int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+
+        int[][] dp = new int[n][n];
+
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i] += sumHelper(graph, dp, i, j, -1);
+            }
+        }
+        return res;
+    }
+
+    private int sumHelper(Set<Integer>[] graph, int[][] dp, int current, int target, int prev) {
+        if (dp[current][target] != 0) return dp[current][target];
+        Set<Integer> nexts = graph[current];
+        if (current == target) {
+            return 0;
+        }
+        for (int next : nexts) {
+            if (next != prev) {
+                int d = sumHelper(graph, dp, next, target, current);
+                if (d >= 0) {
+                    dp[current][target] = d + 1;
+                    dp[target][current] = d + 1;
+                    return d + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    //TODO: learn how to solve 'Memory Limit Exceeded' problem
+    public static class MemoryEfficientButNotMy {
+        private Map<Integer, List<Integer>> graph;
+        private int[] count;
+        private int[] res;
+
+        private void dfs(int node, int parent) {
+            for (int child : graph.get(node)) {
+                if (child != parent) {
+                    dfs(child, node);
+                    count[node] += count[child];
+                    res[node] += res[child] + count[child];
+                }
+            }
+        }
+
+        private void dfs2(int node, int parent) {
+            for (int child : graph.get(node)) {
+                if (child != parent) {
+                    res[child] = res[node] - count[child] + (count.length - count[child]);
+                    dfs2(child, node);
+                }
+            }
+        }
+
+        public int[] sumOfDistancesInTree(int n, int[][] edges) {
+            graph = new HashMap<>();
+            count = new int[n];
+            res = new int[n];
+            Arrays.fill(count, 1);
+
+            for (int i = 0; i < n; i++) {
+                graph.put(i, new ArrayList<>());
+            }
+
+            for (int[] edge : edges) {
+                int u = edge[0];
+                int v = edge[1];
+                graph.get(u).add(v);
+                graph.get(v).add(u);
+            }
+
+            dfs(0, -1);
+            dfs2(0, -1);
+
+            return res;
+        }
     }
 }
--- src/test/java/org/shvetsov/_834Test.java
+++ src/test/java/org/shvetsov/_834Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _834Test {
 
     private static _834 task;
+    private static _834.MemoryEfficientButNotMy taskMemoryEfficientButNotMy;
 
     @BeforeEach
     public void setup() {
         task = new _834();
+        taskMemoryEfficientButNotMy = new _834.MemoryEfficientButNotMy();
     }
 
     @Test
@@ -19,5 +21,12 @@ class _834Test {
         assertThat(task.sumOfDistancesInTree(6, new int[][]{{0, 1}, {0, 2}, {2, 3}, {2, 4}, {2, 5}})).isEqualTo(new int[]{8, 12, 6, 10, 10, 10});
         assertThat(task.sumOfDistancesInTree(1, new int[][]{})).isEqualTo(new int[]{0});
         assertThat(task.sumOfDistancesInTree(2, new int[][]{{1, 0}})).isEqualTo(new int[]{1, 1});
+    }
+
+    @Test
+    void sumOfDistancesInTreeMemoryEfficient() {
+        assertThat(taskMemoryEfficientButNotMy.sumOfDistancesInTree(6, new int[][]{{0, 1}, {0, 2}, {2, 3}, {2, 4}, {2, 5}})).isEqualTo(new int[]{8, 12, 6, 10, 10, 10});
+        assertThat(taskMemoryEfficientButNotMy.sumOfDistancesInTree(1, new int[][]{})).isEqualTo(new int[]{0});
+        assertThat(taskMemoryEfficientButNotMy.sumOfDistancesInTree(2, new int[][]{{1, 0}})).isEqualTo(new int[]{1, 1});
     }
 }
