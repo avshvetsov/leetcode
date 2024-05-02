ae12942488283f9f4047b6373c93481d05ee4364
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-02 09:38:13 +0300

    setup 2441

---
+++ src/main/java/org/shvetsov/_2441.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2441,
+        name = "Largest Positive Integer That Exists With Its Negative",
+        level = Level.EASY,
+        tags = {"Array, Hash Table, Two Pointers, Sorting"}
+)
+public class _2441 {
+    public int findMaxK(int[] nums) {
+
+        return 0;
+    }
+}
