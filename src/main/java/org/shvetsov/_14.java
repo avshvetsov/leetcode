e592c9a9c3445d96dd3c3a751afffc6d7719c179
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-07 10:19:18 +0300

    solve 14

--- src/main/java/org/shvetsov/_14.java
+++ src/main/java/org/shvetsov/_14.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 14,
         name = "Longest Common Prefix",
@@ -11,7 +13,21 @@ import org.shvetsov.core.Level;
 )
 public class _14 {
     public String longestCommonPrefix(String[] strs) {
-
-        return "";
+        String smallest = Arrays.stream(strs)
+                .reduce((s, s2) -> s.length() < s2.length() ? s : s2)
+                .orElse("");
+        StringBuilder res = new StringBuilder();
+        boolean isEqual = true;
+        for (int i = 0; i < smallest.toCharArray().length && isEqual; i++) {
+            char c = smallest.charAt(i);
+            for (String str : strs) {
+                if (str.charAt(i) != c) {
+                    isEqual = false;
+                    break;
+                }
+            }
+            if (isEqual) res.append(c);
+        }
+        return res.toString();
     }
 }
