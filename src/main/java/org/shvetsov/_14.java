a3d2c203a4ddf7550df1e2d1664a4a7eca4c41e1
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-07 09:57:05 +0300

    setup 14

---
+++ src/main/java/org/shvetsov/_14.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 14,
+        name = "Longest Common Prefix",
+        level = Level.EASY,
+        tags = {"String", "Trie"}
+)
+public class _14 {
+    public String longestCommonPrefix(String[] strs) {
+
+        return "";
+    }
+}
