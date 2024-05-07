0433d112998b7409fcfd65977f854924f2693c8c
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-07 09:54:39 +0300

    solve 2816

--- src/main/java/org/shvetsov/_2816.java
+++ src/main/java/org/shvetsov/_2816.java
@@ -12,7 +12,26 @@ import org.shvetsov.core.Level;
 )
 public class _2816 {
     public ListNode doubleIt(ListNode head) {
+        //reverse
+        ListNode prev = null, next;
+        while (head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
 
-        return head;
+        //construct
+        ListNode res = null;
+        int add = 0;
+        while (prev != null) {
+            res = new ListNode((prev.val * 2) % 10 + add, res);
+            add = (prev.val * 2) / 10;
+            prev = prev.next;
+        }
+        if (add > 0) {
+            res = new ListNode(add, res);
+        }
+        return res;
     }
 }
