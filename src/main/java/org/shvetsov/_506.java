c93432e55cdd552818850bafb47832e656b4fd5c
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-08 10:42:42 +0300

    solve 506

--- src/main/java/org/shvetsov/_506.java
+++ src/main/java/org/shvetsov/_506.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.*;
+
 @LeetCode(
         number = 506,
         name = "Relative Ranks",
@@ -11,7 +13,71 @@ import org.shvetsov.core.Level;
 )
 public class _506 {
     public String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+        String[] ranks = new String[n];
+        Map<Integer, String> map = new HashMap<>();
+        int[] sorted = Arrays.copyOf(score, n);
+        Arrays.sort(sorted);
+        for (int i = 0; i < n; i++) {
+            int pos = n - i;
+            switch (pos) {
+                case 1 -> map.put(sorted[i], "Gold Medal");
+                case 2 -> map.put(sorted[i], "Silver Medal");
+                case 3 -> map.put(sorted[i], "Bronze Medal");
+                default -> map.put(sorted[i], Integer.toString(pos));
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            ranks[i] = map.get(score[i]);
+        }
+        return ranks;
+    }
 
-        return new String[0];
+    public String[] findRelativeRanksStream(int[] score) {
+        int n = score.length;
+        String[] ranks = new String[n];
+        Map<Integer, String> map = new HashMap<>();
+        List<Integer> sorted = Arrays.stream(score).sorted().boxed().toList();
+        for (int i = 0; i < n; i++) {
+            switch (n - i) {
+                case 1 -> map.put(sorted.get(i), "Gold Medal");
+                case 2 -> map.put(sorted.get(i), "Silver Medal");
+                case 3 -> map.put(sorted.get(i), "Bronze Medal");
+                default -> map.put(sorted.get(i), Integer.toString(n - i));
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            ranks[i] = map.get(score[i]);
+        }
+        return ranks;
+    }
+
+    public static class SolutionWithLoops {
+        public String[] findRelativeRanks(int[] score) {
+            int n = score.length;
+            String[] ranks = new String[n];
+            int[] positions = new int[n];
+            for (int i = 0; i < score.length; i++) {
+                int value = score[i];
+                int pos = 1;
+                for (int j = 0; j <= i; j++) {
+                    if (i == j) {
+                        positions[j] = pos;
+                    } else if (value > score[j]) {
+                        positions[j]++;
+                    } else pos++;
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                switch (positions[i]) {
+                    case 1 -> ranks[i] = "Gold Medal";
+                    case 2 -> ranks[i] = "Silver Medal";
+                    case 3 -> ranks[i] = "Bronze Medal";
+                    default -> ranks[i] = String.valueOf(positions[i]);
+                }
+            }
+            return ranks;
+        }
     }
 }
--- src/test/java/org/shvetsov/_506Test.java
+++ src/test/java/org/shvetsov/_506Test.java
@@ -8,15 +8,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _506Test {
 
     private static _506 task;
+    private static _506.SolutionWithLoops taskSolutionWithLoops;
 
     @BeforeEach
     public void setup() {
         task = new _506();
+        taskSolutionWithLoops = new _506.SolutionWithLoops();
     }
 
     @Test
     void findRelativeRanks() {
         assertThat(task.findRelativeRanks(new int[]{5, 4, 3, 2, 1})).isEqualTo(new String[]{"Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"});
         assertThat(task.findRelativeRanks(new int[]{10, 3, 8, 9, 4})).isEqualTo(new String[]{"Gold Medal", "5", "Bronze Medal", "Silver Medal", "4"});
+    }
+
+    @Test
+    void findRelativeRanksSolutionWithLoops() {
+        assertThat(taskSolutionWithLoops.findRelativeRanks(new int[]{5, 4, 3, 2, 1})).isEqualTo(new String[]{"Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"});
+        assertThat(taskSolutionWithLoops.findRelativeRanks(new int[]{10, 3, 8, 9, 4})).isEqualTo(new String[]{"Gold Medal", "5", "Bronze Medal", "Silver Medal", "4"});
     }
 }
