da63eaa817ccb6228c76b99061e08c34ad0a207a
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-12 13:23:17 +0300

    solve 2373

--- src/main/java/org/shvetsov/_2373.java
+++ src/main/java/org/shvetsov/_2373.java
@@ -12,7 +12,18 @@ import org.shvetsov.core.Level;
 public class _2373 {
 
     public int[][] largestLocal(int[][] grid) {
-
-        return new int[0][0];
+        int n = grid.length;
+        int[][] offsets = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        int[][] res = new int[n - 2][n - 2];
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                int max = Integer.MIN_VALUE;
+                for (int[] offset : offsets) {
+                    max = Math.max(max, grid[i + offset[0]][j + offset[1]]);
+                }
+                res[i - 1][j - 1] = max;
+            }
+        }
+        return res;
     }
 }
