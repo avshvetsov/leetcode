3018843463b3649b3ad016dc3b7f5618a6f425c0
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-13 09:45:38 +0300

    setup 861

---
+++ src/main/java/org/shvetsov/_861.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 861,
+        name = "Score After Flipping Matrix",
+        level = Level.MEDIUM,
+        tags = {"Array", "Greedy", "Bit Manipulation", "Matrix"}
+)
+public class _861 {
+
+    public int matrixScore(int[][] grid) {
+
+        return 0;
+    }
+}
---
+++ src/test/java/org/shvetsov/_861Test.java
@@ -0,0 +1,22 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _861Test {
+
+    private static _861 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _861();
+    }
+
+    @Test
+    void matrixScore() {
+        assertThat(task.matrixScore(new int[][]{{0, 0, 1, 1}, {1, 0, 1, 0}, {1, 1, 0, 0}})).isEqualTo(39);
+        assertThat(task.matrixScore(new int[][]{{0}})).isEqualTo(1);
+    }
+}
