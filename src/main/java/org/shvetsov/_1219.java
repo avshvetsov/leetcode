32acf362d4577f9715e4b0cdcc9b59033ce89449
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-14 13:15:19 +0300

    solve 1219

--- src/main/java/org/shvetsov/_1219.java
+++ src/main/java/org/shvetsov/_1219.java
@@ -10,8 +10,32 @@ import org.shvetsov.core.Level;
         tags = {"Array", "Backtracking", "Matrix"}
 )
 public class _1219 {
-    public int getMaximumGold(int[][] grid) {
 
-        return 0;
+    static int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int r, c;
+
+    public int getMaximumGold(int[][] grid) {
+        r = grid.length;
+        c = grid[0].length;
+        int result = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                result = Math.max(result, pathFindHelper(grid, i, j));
+            }
+        }
+        return result;
+    }
+
+    private int pathFindHelper(int[][] grid, int row, int col) {
+        if (row < 0 || col < 0 || row == r || col == c || grid[row][col] == 0) return 0;
+
+        int res = 0;
+        int curr = grid[row][col];
+        grid[row][col] = 0;
+        for (int[] dir : dirs) {
+            res = Math.max(curr + pathFindHelper(grid, row + dir[0], col + dir[1]), res);
+        }
+        grid[row][col] = curr;
+        return res;
     }
 }
--- src/test/java/org/shvetsov/_1219Test.java
+++ src/test/java/org/shvetsov/_1219Test.java
@@ -19,4 +19,9 @@ class _1219Test {
         assertThat(task.getMaximumGold(new int[][]{{0, 6, 0}, {5, 8, 7}, {0, 9, 0}})).isEqualTo(24);
         assertThat(task.getMaximumGold(new int[][]{{1, 0, 7}, {2, 0, 6}, {3, 4, 5}, {0, 3, 0}, {9, 0, 20}})).isEqualTo(28);
     }
+
+    @Test
+    void getMaximumGold1() {
+        assertThat(task.getMaximumGold(new int[][]{{1, 0, 7, 0, 0, 0}, {2, 0, 6, 0, 1, 0}, {3, 5, 6, 7, 4, 2}, {4, 3, 1, 0, 2, 0}, {3, 0, 5, 0, 20, 0}})).isEqualTo(60);
+    }
 }
