26c36759dc9aceeac9a2c607495738ba320be4d7
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-15 11:15:05 +0300

    setup 2812

---
+++ src/main/java/org/shvetsov/_2812.java
@@ -0,0 +1,20 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+import java.util.List;
+
+@LeetCode(
+        number = 2812,
+        name = "Find the Safest Path in a Grid",
+        level = Level.MEDIUM,
+        tags = {"Array", "Binary Search", "Breadth-First Search", "Union Find", "Matrix"}
+)
+public class _2812 {
+
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+
+        return 0;
+    }
+}
