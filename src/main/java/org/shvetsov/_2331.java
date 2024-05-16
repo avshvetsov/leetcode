c392238f2eed9c2aefb8af6d7e203d7a46cf2970
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-16 10:06:57 +0300

    solve 2331

--- src/main/java/org/shvetsov/_2331.java
+++ src/main/java/org/shvetsov/_2331.java
@@ -12,7 +12,12 @@ import org.shvetsov.core.Level;
 )
 public class _2331 {
     public boolean evaluateTree(TreeNode root) {
-
-        return false;
+        return switch (root.val) {
+            case 0 -> false;
+            case 1 -> true;
+            case 2 -> evaluateTree(root.left) || evaluateTree(root.right);
+            case 3 -> evaluateTree(root.left) && evaluateTree(root.right);
+            default -> false;
+        };
     }
 }
