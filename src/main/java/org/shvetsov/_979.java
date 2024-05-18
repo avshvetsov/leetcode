3dbc09dcb3760f2dc9635516ea182c84e5059a02
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-18 13:54:12 +0300

    solve 979

--- src/main/java/org/shvetsov/_979.java
+++ src/main/java/org/shvetsov/_979.java
@@ -11,8 +11,51 @@ import org.shvetsov.core.Level;
         tags = {"Tree", "Depth-First Search", "Binary Tree"}
 )
 public class _979 {
-    public int distributeCoins(TreeNode root) {
 
-        return -1;
+    int res = 0;
+
+    public int distributeCoins(TreeNode root) {
+        int[] ans = distributeHelper(root);
+        res += ans[1];
+        return res;
+    }
+
+    //resp [0] - debt count, [1] - debt cost, [2] - coins
+    private int[] distributeHelper(TreeNode curr) {
+        if (curr.left == null && curr.right == null) {
+            if (curr.val == 0) {
+                return new int[]{1, 1, 0};
+            } else {
+                res += curr.val - 1;
+                return new int[]{0, 0, curr.val - 1};
+            }
+        }
+        int[] ans = new int[3];
+        int[] left = new int[]{0,0,0};
+        int[] right = new int[]{0,0,0};
+        if (curr.left != null) {
+            left = distributeHelper(curr.left);
+        }
+        if (curr.right != null) {
+            right = distributeHelper(curr.right);
+        }
+        int currCoins = curr.val + left[2] + right[2];
+        int currDebtCounts = left[0] + right[0];
+        int currDebtCosts = left[1] + right[1];
+        if (currCoins == 0) {
+            ans[0] = currDebtCounts + 1;
+            ans[1] = currDebtCosts + ans[0];
+        } else {
+            if (currCoins > currDebtCounts) {
+                ans[1] = currDebtCosts;
+                ans[2] = currCoins - currDebtCounts - 1;
+                res += ans[2];
+            } else {
+                ans[0] = currDebtCounts - currCoins + 1;
+                ans[1] = currDebtCosts + ans[0];
+            }
+        }
+
+        return ans;
     }
 }
--- src/test/java/org/shvetsov/_979Test.java
+++ src/test/java/org/shvetsov/_979Test.java
@@ -2,9 +2,11 @@ package org.shvetsov;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.shvetsov.classes.TreeNode;
 import org.shvetsov.utils.TreeUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.shvetsov.utils.TreeUtils.*;
 
 class _979Test {
 
@@ -17,11 +19,25 @@ class _979Test {
 
     @Test
     void distributeCoinsExample1() {
-        assertThat(task.distributeCoins(TreeUtils.constructTreeNode(3, 0, 0))).isEqualTo(2);
+        assertThat(task.distributeCoins(constructTreeNode(3, 0, 0))).isEqualTo(2);
     }
 
     @Test
     void distributeCoinsExample2() {
-        assertThat(task.distributeCoins(TreeUtils.constructTreeNode(0, 3, 0))).isEqualTo(3);
+        assertThat(task.distributeCoins(constructTreeNode(0, 3, 0))).isEqualTo(3);
+    }
+
+    @Test
+    void distributeCoinsTest18() {
+        TreeNode root = constructTreeNode(4, 0, null, null, 0, null, 0);
+        System.out.println(root);
+        assertThat(task.distributeCoins(root)).isEqualTo(6);
+    }
+
+    @Test
+    void distributeCoinsTest69() {
+        TreeNode root = constructTreeNode(0,1,1,null,null,1,0,null,1,null,4,null,0);
+        System.out.println(root);
+        assertThat(task.distributeCoins(root)).isEqualTo(9);
     }
 }
