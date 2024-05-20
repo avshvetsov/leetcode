b8adc9b935c51843c90a96b8661b14794b579f57
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-20 10:55:27 +0300

    setup 1863

---
+++ src/main/java/org/shvetsov/_1863.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1863,
+        name = "Sum of All Subset XOR Totals",
+        level = Level.EASY,
+        tags = {"Array", "Math", "Backtracking", "Bit Manipulation", "Combinatorics", "Enumeration"}
+)
+public class _1863 {
+
+    public int subsetXORSum(int[] nums) {
+
+        return 0;
+    }
+}
