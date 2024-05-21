6788bffe431cc91edb19d4c27489abedee5ed984
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-21 09:33:02 +0300

    solve 78

--- src/main/java/org/shvetsov/_78.java
+++ src/main/java/org/shvetsov/_78.java
@@ -4,6 +4,7 @@ import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @LeetCode(
@@ -13,8 +14,37 @@ import java.util.List;
         tags = {"Array", "Backtracking", "Bit Manipulation"}
 )
 public class _78 {
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> result = new LinkedList<>();
 
-        return new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        result.add(List.of());
+        subsetHelper(nums, 0, new LinkedList<>());
+        return result;
+    }
+
+    private void subsetHelper(int[] nums, int l, List<Integer> acc) {
+        for (int i = l; i < nums.length; i++) {
+            List<Integer> subset = new LinkedList<>(acc);
+            subset.add(nums[i]);
+            result.add(subset);
+            subsetHelper(nums, i + 1, subset);
+        }
+    }
+
+    public static class BitManipulation {
+        public List<List<Integer>> subsets(int[] nums) {
+            List<List<Integer>> result = new ArrayList<>();
+            int n = nums.length;
+            for (int i = 0; i < (1 << n); i++) {
+                List<Integer> subset = new ArrayList<>();
+                for (int j = 0; j < n; j++) {
+                    if ((i & (1 << j)) > 0) {
+                        subset.add(nums[j]);
+                    }
+                }
+                result.add(subset);
+            }
+            return result;
+        }
     }
 }
--- src/test/java/org/shvetsov/_78Test.java
+++ src/test/java/org/shvetsov/_78Test.java
@@ -2,7 +2,6 @@ package org.shvetsov;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.shvetsov.utils.LinkedListUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +11,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _78Test {
 
     private static _78 task;
+    private static _78.BitManipulation taskBitManipulation;
 
     @BeforeEach
     public void setup() {
         task = new _78();
+        taskBitManipulation = new _78.BitManipulation();
     }
 
     @Test
@@ -28,6 +29,7 @@ class _78Test {
             add(List.of(2));
             add(List.of(2, 3));
             add(List.of(1, 2, 3));
+            add(List.of(3));
         }};
         assertThat(task.subsets(new int[]{1, 2, 3})).containsExactlyInAnyOrderElementsOf(expected);
     }
@@ -39,5 +41,29 @@ class _78Test {
             add(List.of(0));
         }};
         assertThat(task.subsets(new int[]{0})).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    void subsetsBitManipulationExample1() {
+        List<List<Integer>> expected = new ArrayList<>() {{
+            add(List.of());
+            add(List.of(1));
+            add(List.of(1, 2));
+            add(List.of(1, 3));
+            add(List.of(2));
+            add(List.of(2, 3));
+            add(List.of(1, 2, 3));
+            add(List.of(3));
+        }};
+        assertThat(taskBitManipulation.subsets(new int[]{1, 2, 3})).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    void subsetsBitManipulationExample2() {
+        List<List<Integer>> expected = new ArrayList<>() {{
+            add(List.of());
+            add(List.of(0));
+        }};
+        assertThat(taskBitManipulation.subsets(new int[]{0})).containsExactlyInAnyOrderElementsOf(expected);
     }
 }
