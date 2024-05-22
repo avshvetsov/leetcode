326082f7537283161e1f2260c7625f7b1394140e
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-22 10:35:00 +0300

    solve 131

--- src/main/java/org/shvetsov/_131.java
+++ src/main/java/org/shvetsov/_131.java
@@ -4,7 +4,9 @@ import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @LeetCode(
         number = 131,
@@ -14,9 +16,54 @@ import java.util.List;
 )
 public class _131 {
 
-    public List<List<String>> partition(String s) {
+    private final Map<Integer, List<String>> palindromes = new HashMap<>();
 
-        return new ArrayList<>();
+    public List<List<String>> partition(String s) {
+        int n = s.length();
+        //find all palindromes
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                String curr = s.substring(i, j + 1);
+                if (checkIsPalindrome(curr)) {
+                    palindromes.computeIfAbsent(i, k -> new ArrayList<>()).add(curr);
+                }
+            }
+        }
+        //build partitions
+        List<List<String>> result = new ArrayList<>();
+        buildPartitionsHelper(0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void buildPartitionsHelper(int start, List<String> partition, List<List<String>> result) {
+        if (this.palindromes.containsKey(start)) {
+            List<String> palindromes = this.palindromes.get(start);
+            for (String palindrome : palindromes) {
+                //backtracking approach
+                partition.add(palindrome);
+                buildPartitionsHelper(start + palindrome.length(), partition, result);
+                partition.removeLast();
+
+                //immutable approach
+/*
+                List<String> newPartition = new ArrayList<>(partition) {{
+                    add(palindrome);
+                }};
+                buildPartitionsHelper(start + palindrome.length(), newPartition, result);
+*/
+            }
+        } else result.add(new ArrayList<>(partition));
+
+    }
+
+    private boolean checkIsPalindrome(String str) {
+        int l = 0, r = str.length() - 1;
+        while (r - l > 0) {
+            if (str.charAt(l) != str.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
     }
 
 }
