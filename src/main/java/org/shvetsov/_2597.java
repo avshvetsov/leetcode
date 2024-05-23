1978f2709e7e1e821a3820520c27e3cce69ebb8f
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-23 15:38:12 +0300

    solve 2597

--- src/main/java/org/shvetsov/_2597.java
+++ src/main/java/org/shvetsov/_2597.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @LeetCode(
         number = 2597,
         name = "The Number of Beautiful Subsets",
@@ -11,9 +14,98 @@ import org.shvetsov.core.Level;
 )
 public class _2597 {
 
-    public int beautifulSubsets(int[] nums, int k) {
+    private int result = 0;
+    private Map<Integer, Integer> valueDp = new HashMap<>();
 
-        return 0;
+    public int beautifulSubsets(int[] nums, int k) {
+        int n = 0;
+        for (int num : nums) {
+            int i = num + k;
+            if (!valueDp.containsKey(i)) {
+                valueDp.put(i, n);
+                n++;
+            }
+            int j = num - k;
+            if (!valueDp.containsKey(j)) {
+                valueDp.put(j, n);
+                n++;
+            }
+        }
+        int[] dp = new int[valueDp.size()];
+        subsetHelper(nums, k, 0, dp);
+        return result;
+    }
+
+    private void subsetHelper(int[] nums, int k, int start, int[] dp) {
+        for (int i = start; i < nums.length; i++) {
+            if (!valueDp.containsKey(nums[i]) || dp[valueDp.get(nums[i])] == 0) {
+                result++;
+                dp[valueDp.get(nums[i] - k)]++;
+                dp[valueDp.get(nums[i] + k)]++;
+                subsetHelper(nums, k, i + 1, dp);
+                dp[valueDp.get(nums[i] - k)]--;
+                dp[valueDp.get(nums[i] + k)]--;
+            }
+        }
+    }
+
+    public static class BruteforceWithDp {
+        private int result = 0;
+        private Map<Integer, Integer> valueDp = new HashMap<>();
+
+        public int beautifulSubsets(int[] nums, int k) {
+            int n = 0;
+            for (int num : nums) {
+                int i = num + k;
+                if (!valueDp.containsKey(i)) {
+                    valueDp.put(i, n);
+                    n++;
+                }
+                int j = num - k;
+                if (!valueDp.containsKey(j)) {
+                    valueDp.put(j, n);
+                    n++;
+                }
+            }
+            int[] dp = new int[valueDp.size()];
+            subsetHelper(nums, k, 0, dp);
+            return result;
+        }
+
+        private void subsetHelper(int[] nums, int k, int start, int[] dp) {
+            for (int i = start; i < nums.length; i++) {
+                if (!valueDp.containsKey(nums[i]) || dp[valueDp.get(nums[i])] == 0) {
+                    result++;
+                    dp[valueDp.get(nums[i] - k)]++;
+                    dp[valueDp.get(nums[i] + k)]++;
+                    subsetHelper(nums, k, i + 1, dp);
+                    dp[valueDp.get(nums[i] - k)]--;
+                    dp[valueDp.get(nums[i] + k)]--;
+                }
+            }
+        }
+    }
+
+    public static class Bruteforce {
+        private int result = 0;
+
+        public int beautifulSubsets(int[] nums, int k) {
+            subsetHelper(nums, k, 0, new HashMap<>());
+            return result;
+        }
+
+        private void subsetHelper(int[] nums, int k, int start, HashMap<Integer, Integer> blocked) {
+            for (int i = start; i < nums.length; i++) {
+                if (blocked.getOrDefault(nums[i], 0) == 0) {
+                    result++;
+                    blocked.put(nums[i] - k, blocked.getOrDefault(nums[i] - k, 0) + 1);
+                    blocked.put(nums[i] + k, blocked.getOrDefault(nums[i] + k, 0) + 1);
+                    subsetHelper(nums, k, i + 1, blocked);
+                    blocked.put(nums[i] - k, blocked.get(nums[i] - k) - 1);
+                    blocked.put(nums[i] + k, blocked.get(nums[i] + k) - 1);
+                }
+            }
+        }
     }
 
 }
--- src/test/java/org/shvetsov/_2597Test.java
+++ src/test/java/org/shvetsov/_2597Test.java
@@ -8,18 +8,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _2597Test {
 
     private static _2597 task;
+    private static _2597.Bruteforce taskBruteforce;
 
     @BeforeEach
     public void setup() {
         task = new _2597();
+        taskBruteforce = new _2597.Bruteforce();
     }
 
     @Test
     void beautifulSubsetsExample1() {
         assertThat(task.beautifulSubsets(new int[]{2, 4, 6}, 2)).isEqualTo(4);
     }
+
     @Test
     void beautifulSubsetsExample2() {
         assertThat(task.beautifulSubsets(new int[]{1}, 1)).isEqualTo(1);
+    }
+
+    @Test
+    void beautifulSubsetsExample3() {
+        assertThat(task.beautifulSubsets(new int[]{4, 2, 5, 9, 10, 3}, 1)).isEqualTo(23);
+    }
+
+    @Test
+    void beautifulSubsetsExample4() {
+        assertThat(task.beautifulSubsets(new int[]{9, 5, 7, 10, 6, 2}, 9)).isEqualTo(63);
+    }
+
+    @Test
+    void beautifulSubsetsExample5() {
+        assertThat(task.beautifulSubsets(new int[]{1, 2, 3, 4}, 1)).isEqualTo(7);
+    }
+
+    @Test
+    void taskNotEfficientSubsetsExample1() {
+        assertThat(taskBruteforce.beautifulSubsets(new int[]{2, 4, 6}, 2)).isEqualTo(4);
+    }
+
+    @Test
+    void taskNotEfficientSubsetsExample2() {
+        assertThat(taskBruteforce.beautifulSubsets(new int[]{1}, 1)).isEqualTo(1);
+    }
+
+    @Test
+    void taskNotEfficientSubsetsExample3() {
+        assertThat(taskBruteforce.beautifulSubsets(new int[]{4, 2, 5, 9, 10, 3}, 1)).isEqualTo(23);
     }
 }
