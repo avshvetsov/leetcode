1510cf87f152037b29de4a1209cb259ec43360ae
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-26 18:40:50 +0300

    setup 552

---
+++ src/main/java/org/shvetsov/_552.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 552,
+        name = "Student Attendance Record II",
+        level = Level.HARD,
+        tags = {"Dynamic Programming"}
+)
+public class _552 {
+    public int checkRecord(int n) {
+
+        return 0;
+    }
+}
