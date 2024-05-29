c52eb8acc7bb2e4aeb609b8a9da4ff853f874b62
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-29 09:43:24 +0300

    setup 1404

---
+++ src/main/java/org/shvetsov/_1404.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1404,
+        name = "Number of Steps to Reduce a Number in Binary Representation to One",
+        level = Level.MEDIUM,
+        tags = {"String", "Bit Manipulation"}
+)
+public class _1404 {
+    public int numSteps(String s) {
+
+        return 0;
+    }
+}
