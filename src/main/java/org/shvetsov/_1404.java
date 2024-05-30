f2063834b6c8e30b0b4c4b3723cd6129742a3bb0
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-30 09:41:58 +0300

    solve 1404

--- src/main/java/org/shvetsov/_1404.java
+++ src/main/java/org/shvetsov/_1404.java
@@ -3,6 +3,11 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 @LeetCode(
         number = 1404,
         name = "Number of Steps to Reduce a Number in Binary Representation to One",
@@ -11,7 +16,26 @@ import org.shvetsov.core.Level;
 )
 public class _1404 {
     public int numSteps(String s) {
-
-        return 0;
+        int res = 0;
+        List<Character> chars = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            chars.add(c);
+        }
+        while (chars.size() > 1) {
+            if (chars.getLast() == '1') {
+                int i = chars.size() - 1;
+                while (i >= 0 && chars.get(i) == '1') {
+                    chars.set(i, '0');
+                    i--;
+                }
+                if (i >= 0) {
+                    chars.set(i, '1');
+                } else {
+                    chars.addFirst('1');
+                }
+            } else chars.removeLast();
+            res++;
+        }
+        return res;
     }
 }
