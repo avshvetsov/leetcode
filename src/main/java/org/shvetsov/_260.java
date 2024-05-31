448bf45ae423c0f25b56e58ec27e54ca904047e9
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-05-31 09:43:15 +0300

    setup 260

---
+++ src/main/java/org/shvetsov/_260.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 260,
+        name = "Single Number III",
+        level = Level.MEDIUM,
+        tags = {"Array", "Bit Manipulation"}
+)
+public class _260 {
+
+    public int[] singleNumber(int[] nums) {
+
+        return new int[]{};
+    }
+}
