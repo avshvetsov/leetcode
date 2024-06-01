f18a3adc57a625093ce477804efde5fb20a48b79
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-01 09:48:19 +0300

    setup 3110

---
+++ src/main/java/org/shvetsov/_3110.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 3110,
+        name = "Score of a String",
+        level = Level.EASY,
+        tags = {"String"}
+)
+public class _3110 {
+    public int scoreOfString(String s) {
+
+        return 0;
+    }
+}
