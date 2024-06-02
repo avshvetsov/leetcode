e95bb1142faeac322343300e37213c063c24a8ed
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-02 10:30:36 +0300

    solve 344

--- src/main/java/org/shvetsov/_344.java
+++ src/main/java/org/shvetsov/_344.java
@@ -11,6 +11,13 @@ import org.shvetsov.core.Level;
 )
 public class _344 {
     public void reverseString(char[] s) {
-
+        int l = 0, r = s.length - 1;
+        while (l < r) {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
     }
 }
