5c6c0583bd991d4d3c67050a02b1b5483221bea7
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-04 09:34:12 +0300

    solve 409

---
+++ src/main/java/org/shvetsov/_409.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 409,
+        name = "Longest Palindrome",
+        level = Level.EASY,
+        tags = {"Hash Table", "String", "Greedy"}
+)
+public class _409 {
+    public int longestPalindrome(String s) {
+
+        return 0;
+    }
+}
