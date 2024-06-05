eeae19bf2cf7700ba3a94470c4d864d703a6a288
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-05 11:46:03 +0300

    solve 1002

--- src/main/java/org/shvetsov/_1002.java
+++ src/main/java/org/shvetsov/_1002.java
@@ -3,6 +3,7 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @LeetCode(
@@ -13,7 +14,26 @@ import java.util.List;
 )
 public class _1002 {
     public List<String> commonChars(String[] words) {
-
-        return null;
+        int[] counts = new int[26];
+        for (char c : words[0].toCharArray()) {
+            counts[c - 'a']++;
+        }
+        for (String word : words) {
+            int[] curr = new int[26];
+            for (char c : word.toCharArray()) {
+                curr[c - 'a']++;
+            }
+            for (int i = 0; i < curr.length; i++) {
+                counts[i] = Math.min(curr[i], counts[i]);
+            }
+        }
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            while (counts[i] > 0) {
+                result.add(String.valueOf((char) ('a' + i)));
+                counts[i]--;
+            }
+        }
+        return result;
     }
 }
