2bc1de538266a34a8d0b84c1c7f199eb2a657239
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-10 10:04:57 +0300

    solve 1051

--- src/main/java/org/shvetsov/_1051.java
+++ src/main/java/org/shvetsov/_1051.java
@@ -3,6 +3,10 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 @LeetCode(
         number = 1051,
         name = "Height Checker",
@@ -10,8 +14,47 @@ import org.shvetsov.core.Level;
         tags = {"Array", "Sorting", "Counting Sort"}
 )
 public class _1051 {
-    public int heightChecker(int[] heights) {
 
-        return -1;
+    static final int MIN = 1;
+    static final int MAX = 100;
+
+    public int heightChecker(int[] heights) {
+        int result = 0;
+        int[] expected = Arrays.copyOf(heights, heights.length);
+//        Arrays.sort(expected);
+        countingSort(expected);
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public void countingSort(int[] arr) {
+        int[] map = new int[MAX - MIN + 1];
+        for (int i : arr) {
+            map[i - MIN]++;
+        }
+        int j = 0;
+        for (int i = 0; i < map.length; i++) {
+            while (map[i] > 0) {
+                arr[j++] = i + MIN;
+                map[i]--;
+            }
+        }
+    }
+
+    public void countingSortMap(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        int j = 0;
+        for (int i = MIN; i <= MAX; i++) {
+            for (int k = 0; k < map.getOrDefault(i, 0); k++) {
+                arr[j++] = i;
+            }
+        }
     }
 }
