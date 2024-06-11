d067b756ad77f50dc1566d5c4310d60bcc16e78c
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-11 09:37:01 +0300

    setup 1122

---
+++ src/main/java/org/shvetsov/_1122.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1122,
+        name = "Relative Sort Array",
+        level = Level.EASY,
+        tags = {"Array", "Hash Table", "Sorting", "Counting Sort"}
+)
+public class _1122 {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+
+        return null;
+    }
+}
