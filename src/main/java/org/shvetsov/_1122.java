d045d149416172d6286dcf3a8a2ce9ecbbf4bc86
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-11 09:46:10 +0300

    solve 1122

--- src/main/java/org/shvetsov/_1122.java
+++ src/main/java/org/shvetsov/_1122.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 1122,
         name = "Relative Sort Array",
@@ -10,8 +12,28 @@ import org.shvetsov.core.Level;
         tags = {"Array", "Hash Table", "Sorting", "Counting Sort"}
 )
 public class _1122 {
-    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+    private static final int MIN = 1;
+    private static final int MAX = 1000;
 
-        return null;
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] freq = new int[MAX + 1];
+        for (int i : arr1) {
+            freq[i]++;
+        }
+        int j = 0;
+        for (int i : arr2) {
+            int end = j + freq[i];
+            freq[i] = 0;
+            Arrays.fill(arr1, j, end, i);
+            j = end;
+        }
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] > 0) {
+                int end = j + freq[i];
+                Arrays.fill(arr1, j, end, i);
+                j = end;
+            }
+        }
+        return arr1;
     }
 }
