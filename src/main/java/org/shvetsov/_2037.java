a4989bcd7498cf6b76eb37a9b278b9269481d235
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-13 09:46:51 +0300

    setup 2037

---
+++ src/main/java/org/shvetsov/_2037.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2037,
+        name = "Minimum Number of Moves to Seat Everyone",
+        level = Level.EASY,
+        tags = {"Array", "Greedy", "Sorting"}
+)
+public class _2037 {
+    public int minMovesToSeat(int[] seats, int[] students) {
+
+        return 0;
+    }
+}
