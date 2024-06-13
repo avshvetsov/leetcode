b2563feeb73309f1d4a0c70e837a43a135d16a57
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-13 09:43:14 +0300

    solve 75

--- src/main/java/org/shvetsov/_75.java
+++ src/main/java/org/shvetsov/_75.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 75,
         name = "Sort Colors",
@@ -11,6 +13,38 @@ import org.shvetsov.core.Level;
 )
 public class _75 {
     public void sortColors(int[] nums) {
+        int l = 0, m = 0, r = nums.length - 1;
+        while (m <= r) {
+            if (nums[m] == 0) {
+                swap(nums, m, l);
+                l++;
+                m++;
+            } else if (nums[m] == 1) {
+                m++;
+            } else if (nums[m] == 2) {
+                swap(nums, m, r);
+                r--;
+            }
+        }
+    }
 
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static class CountingSort {
+        public void sortColors(int[] nums) {
+            int[] count = new int[3];
+            for (int num : nums) {
+                count[num]++;
+            }
+            int j = 0;
+            for (int i = 0; i < count.length; i++) {
+                Arrays.fill(nums, j, j + count[i], i);
+                j += count[i];
+            }
+        }
     }
 }
--- src/test/java/org/shvetsov/_75Test.java
+++ src/test/java/org/shvetsov/_75Test.java
@@ -8,12 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _75Test {
 
     private static _75 task;
+    private static _75.CountingSort taskCountingSort;
 
     @BeforeEach
     public void setup() {
         task = new _75();
+        taskCountingSort = new _75.CountingSort();
     }
-
 
     @Test
     void sortColorsExample1() {
@@ -26,6 +27,20 @@ class _75Test {
     void sortColorsExample2() {
         int[] input = {2, 0, 1};
         task.sortColors(input);
+        assertThat(input).isEqualTo(new int[]{0, 1, 2});
+    }
+
+    @Test
+    void sortColorsCountingSortExample1() {
+        int[] input = {2, 0, 2, 1, 1, 0};
+        taskCountingSort.sortColors(input);
+        assertThat(input).isEqualTo(new int[]{0, 0, 1, 1, 2, 2});
+    }
+
+    @Test
+    void sortColorsCountingSortExample2() {
+        int[] input = {2, 0, 1};
+        taskCountingSort.sortColors(input);
         assertThat(input).isEqualTo(new int[]{0, 1, 2});
     }
 }
