8e704ace4b8aaf0821c9cf0ea6b452e91aae93ff
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-14 09:55:56 +0300

    setup 945

---
+++ src/main/java/org/shvetsov/_945.java
@@ -0,0 +1,19 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 945,
+        name = "Minimum Increment to Make Array Unique",
+        level = Level.MEDIUM,
+        tags = {"Array", "Greedy", "Sorting", "Counting"}
+)
+public class _945 {
+
+    public int minIncrementForUnique(int[] nums) {
+
+        return 0;
+    }
+
+}
