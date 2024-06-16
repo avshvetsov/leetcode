15b1752d4fdc82d0d19f1f15ab674489701b4a9f
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-16 14:59:02 +0300

    setup 330

---
+++ src/test/java/org/shvetsov/_330Test.java
@@ -0,0 +1,31 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _330Test {
+
+    private static _330 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _330();
+    }
+
+    @Test
+    void minPatchesExample1() {
+        assertThat(task.minPatches(new int[]{1, 3}, 6)).isEqualTo(1);
+    }
+
+    @Test
+    void minPatchesExample2() {
+        assertThat(task.minPatches(new int[]{1, 5, 10}, 20)).isEqualTo(2);
+    }
+
+    @Test
+    void minPatchesExample3() {
+        assertThat(task.minPatches(new int[]{1, 2, 2}, 5)).isEqualTo(0);
+    }
+}
