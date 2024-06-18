a3c9ed38dfd9087e7acb42133494532ce7ef65ca
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-18 10:05:11 +0300

    solve 826

--- src/main/java/org/shvetsov/_826.java
+++ src/main/java/org/shvetsov/_826.java
@@ -3,6 +3,11 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
 @LeetCode(
         number = 826,
         name = "Most Profit Assigning Work",
@@ -11,7 +16,45 @@ import org.shvetsov.core.Level;
 )
 public class _826 {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int result = 0;
+        NavigableMap<Integer, Integer> mapDiffProf = new TreeMap<>();
+        mapDiffProf.put(0, 0);
+        for (int i = 0; i < difficulty.length; i++) {
+            int diff = difficulty[i];
+            int prof = profit[i];
+            if (mapDiffProf.floorEntry(diff).getValue() < prof) {
+                mapDiffProf.put(diff, prof);
+                Map.Entry<Integer, Integer> higherEntry = mapDiffProf.higherEntry(diff);
+                while (higherEntry != null && higherEntry.getValue() < prof) {
+                    diff = higherEntry.getKey();
+                    mapDiffProf.remove(diff);
+                    higherEntry = mapDiffProf.higherEntry(diff);
+                }
+            }
+        }
+        for (int w : worker) {
+            result += mapDiffProf.floorEntry(w).getValue();
+        }
+        return result;
+    }
 
-        return 0;
+    public static class Memorization {
+        public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+            int result = 0;
+            int maxW = Arrays.stream(worker).max().getAsInt();
+            int[] dp = new int[maxW + 1];
+            for (int i = 0; i < difficulty.length; i++) {
+                if (difficulty[i] <= maxW && profit[i] > dp[difficulty[i]]) {
+                    dp[difficulty[i]] = profit[i];
+                }
+            }
+            for (int i = 1; i <= maxW; i++) {
+                dp[i] = Math.max(dp[i], dp[i - 1]);
+            }
+            for (int w : worker) {
+                result += dp[w];
+            }
+            return result;
+        }
     }
 }
--- src/test/java/org/shvetsov/_826Test.java
+++ src/test/java/org/shvetsov/_826Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _826Test {
 
     private static _826 task;
+    private static _826.Memorization taskMemorization;
 
     @BeforeEach
     public void setup() {
         task = new _826();
+        taskMemorization = new _826.Memorization();
     }
 
     @Test
@@ -24,5 +26,24 @@ class _826Test {
     void maxProfitAssignmentExample2() {
         assertThat(task.maxProfitAssignment(new int[]{85, 47, 57}, new int[]{24, 66, 99}, new int[]{40, 25, 25}))
                 .isEqualTo(0);
+    }
+    @Test
+    void maxProfitAssignmentMemoExample1() {
+        assertThat(taskMemorization.maxProfitAssignment(new int[]{2, 4, 6, 8, 10}, new int[]{10, 20, 30, 40, 50}, new int[]{4, 5, 6, 7}))
+                .isEqualTo(100);
+    }
+
+    @Test
+    void maxProfitAssignmentMemoExample2() {
+        assertThat(taskMemorization.maxProfitAssignment(new int[]{85, 47, 57}, new int[]{24, 66, 99}, new int[]{40, 25, 25}))
+                .isEqualTo(0);
+    }
+
+    @Test
+    void maxProfitAssignmentMemoTC56() {
+        assertThat(taskMemorization.maxProfitAssignment(new int[]{66,1,28,73,53,35,45,60,100,44,59,94,27,88,7,18,83,18,72,63},
+                new int[]{66,20,84,81,56,40,37,82,53,45,43,96,67,27,12,54,98,19,47,77},
+                new int[]{61,33,68,38,63,45,1,10,53,23,66,70,14,51,94,18,28,78,100,16}))
+                .isEqualTo(1392);
     }
 }
