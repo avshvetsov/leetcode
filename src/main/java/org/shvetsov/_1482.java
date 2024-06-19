c83122bbf6f19399d4755f8fd22646903d284bfc
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-19 10:23:58 +0300

    solve 1482

--- src/main/java/org/shvetsov/_1482.java
+++ src/main/java/org/shvetsov/_1482.java
@@ -11,7 +11,37 @@ import org.shvetsov.core.Level;
 )
 public class _1482 {
     public int minDays(int[] bloomDay, int m, int k) {
-
-        return 0;
+        if (m * k > bloomDay.length) return -1;
+        //find min, max days of bloomDay array
+        int min = bloomDay[0], max = bloomDay[0];
+        for (int day : bloomDay) {
+            min = Math.min(min, day);
+            max = Math.max(max, day);
+        }
+        int result = -1;
+        while (min <= max) {
+            int mid = (max + min) / 2;
+            int strick = 0;
+            int coverBouquets = 0;
+            //check if mid day enough to make m bouquets
+            for (int day : bloomDay) {
+                if (day <= mid) {
+                    strick++;
+                } else {
+                    coverBouquets += strick / k;
+                    strick = 0;
+                }
+            }
+            coverBouquets += strick / k;
+            //if mid day enough -> max = mid, else min = mid + 1
+            if (coverBouquets >= m) {
+                result = mid;
+                max = mid;
+                if (max == min) break;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return result;
     }
 }
