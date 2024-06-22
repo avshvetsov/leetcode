42dd89f6346de28b5f0842ca68fb3994967aa682
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-22 08:19:42 +0300

    setup 1248

---
+++ src/main/java/org/shvetsov/_1248.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1248,
+        name = "Count Number of Nice Subarrays",
+        level = Level.MEDIUM,
+        tags = {"Array", "Hash Table", "Math", "Sliding Window"}
+)
+public class _1248 {
+    public int numberOfSubarrays(int[] nums, int k) {
+
+        return 0;
+    }
+}
