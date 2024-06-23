e17698e8c2dd4f3ffd6d1a4a595f79701bbf0465
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-23 14:21:53 +0300

    solve 1248

--- src/main/java/org/shvetsov/_1248.java
+++ src/main/java/org/shvetsov/_1248.java
@@ -3,6 +3,11 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 @LeetCode(
         number = 1248,
         name = "Count Number of Nice Subarrays",
@@ -11,7 +16,47 @@ import org.shvetsov.core.Level;
 )
 public class _1248 {
     public int numberOfSubarrays(int[] nums, int k) {
+        List<int[]> countBA = new LinkedList<>();
+        int prevLastOdd = -1;
+        int lastOdd = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 1) {
+                if (lastOdd != -1) {
+                    int[] ba = new int[]{lastOdd - prevLastOdd - 1, i - lastOdd - 1};
+                    countBA.add(ba);
+                    prevLastOdd = lastOdd;
+                    lastOdd = i;
+                } else {
+                    lastOdd = i;
+                }
+            }
+        }
+        int[] ba = new int[]{lastOdd - prevLastOdd - 1, nums.length - lastOdd - 1};
+        countBA.add(ba);
 
-        return 0;
+        int l = 0, r = k - 1, res = 0;
+        while (r < countBA.size()) {
+            res += (countBA.get(l)[0] + 1) * (countBA.get(r)[1] + 1);
+            l++;
+            r++;
+        }
+        return res;
+    }
+
+    public static class PrefixSum {
+        public int numberOfSubarrays(int[] nums, int k) {
+            int result = 0;
+            int currSum = 0;
+            Map<Integer, Integer> prefixSum = new HashMap<>();
+            prefixSum.put(currSum, 1);
+            for (int num : nums) {
+                currSum += num % 2;
+                if (prefixSum.containsKey(currSum - k)) {
+                    result += prefixSum.get(currSum - k);
+                }
+                prefixSum.put(currSum, prefixSum.getOrDefault(currSum, 0) + 1);
+            }
+            return result;
+        }
     }
 }
--- src/test/java/org/shvetsov/_1248Test.java
+++ src/test/java/org/shvetsov/_1248Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _1248Test {
 
     private static _1248 task;
+    private static _1248.PrefixSum taskPrefixSum;
 
     @BeforeEach
     public void setup() {
         task = new _1248();
+        taskPrefixSum = new _1248.PrefixSum();
     }
 
     @Test
@@ -27,5 +29,28 @@ class _1248Test {
     @Test
     void numberOfSubarraysExample3() {
         assertThat(task.numberOfSubarrays(new int[]{2, 2, 2, 1, 2, 2, 1, 2, 2, 2}, 2)).isEqualTo(16);
+    }
+    @Test
+    void numberOfSubarraysTC4() {
+        assertThat(task.numberOfSubarrays(new int[]{1,1,1,1,1}, 1)).isEqualTo(5);
+    }
+    @Test
+    void numberOfSubarraysTC6() {
+        assertThat(task.numberOfSubarrays(new int[]{45627,50891,94884,11286,35337,46414,62029,20247,72789,89158,54203,79628,25920,16832,47469,80909}, 1)).isEqualTo(28);
+    }
+
+    @Test
+    void numberOfSubarraysPrefixSumExample1() {
+        assertThat(taskPrefixSum.numberOfSubarrays(new int[]{1, 1, 2, 1, 1}, 3)).isEqualTo(2);
+    }
+
+    @Test
+    void numberOfSubarraysPrefixSumExample2() {
+        assertThat(taskPrefixSum.numberOfSubarrays(new int[]{2, 4, 6}, 1)).isEqualTo(0);
+    }
+
+    @Test
+    void numberOfSubarraysPrefixSumExample3() {
+        assertThat(taskPrefixSum.numberOfSubarrays(new int[]{2, 2, 2, 1, 2, 2, 1, 2, 2, 2}, 2)).isEqualTo(16);
     }
 }
