71941cbc1f54f1ca005adcb682ef0878065d5a28
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-26 09:49:52 +0300

    fix 1382 test

--- src/test/java/org/shvetsov/_1382Test.java
+++ src/test/java/org/shvetsov/_1382Test.java
@@ -2,10 +2,9 @@ package org.shvetsov;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.shvetsov.utils.TreeUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.shvetsov.utils.TreeUtils.*;
+import static org.shvetsov.utils.TreeUtils.constructTreeNode;
 
 class _1382Test {
 
@@ -19,7 +18,9 @@ class _1382Test {
     @Test
     void balanceBSTExample1() {
         assertThat(task.balanceBST(constructTreeNode(1, null, 2, null, 3, null, 4, null, null)))
-                .isIn(constructTreeNode(2, 1, 3, null, null, null, 4), constructTreeNode(3, 1, 4, null, 2));
+                .isIn(constructTreeNode(2, 1, 3, null, null, null, 4),
+                        constructTreeNode(3, 1, 4, null, 2),
+                        constructTreeNode(3, 2, 4, 1));
     }
 
     @Test
