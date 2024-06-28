edb969763f6e8d274e3d015d9e85741ea2d33812
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-06-28 10:57:15 +0300

    solve 2285

--- src/main/java/org/shvetsov/_2285.java
+++ src/main/java/org/shvetsov/_2285.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.*;
+
 @LeetCode(
         number = 2285,
         name = "Maximum Total Importance of Roads",
@@ -11,7 +13,18 @@ import org.shvetsov.core.Level;
 )
 public class _2285 {
     public long maximumImportance(int n, int[][] roads) {
-
-        return 0;
+        int[] cnts = new int[n];
+        for (int[] road : roads) {
+            for (int city : road) {
+                cnts[city]++;
+            }
+        }
+        long res = 0;
+        Arrays.sort(cnts);
+        while (n > 0 && cnts[n - 1] > 0) {
+            res += (long) cnts[n - 1] * n;
+            n--;
+        }
+        return res;
     }
 }
