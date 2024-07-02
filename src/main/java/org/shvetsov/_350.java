f750c403664c85cb25c639ec8c695116ec2456a1
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-02 09:55:46 +0300

    setup 350

---
+++ src/main/java/org/shvetsov/_350.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 350,
+        name = "Intersection of Two Arrays II",
+        level = Level.EASY,
+        tags = {"Array", "Hash Table", "Two Pointers", "Binary Search", "Sorting"}
+)
+public class _350 {
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        return null;
+    }
+}
