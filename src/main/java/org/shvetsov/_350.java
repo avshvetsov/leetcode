7fda77b2ee7d6bab537db73e7a0d4e956d9e4233
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-02 10:23:20 +0300

    solve 350

--- src/main/java/org/shvetsov/_350.java
+++ src/main/java/org/shvetsov/_350.java
@@ -3,6 +3,11 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 @LeetCode(
         number = 350,
         name = "Intersection of Two Arrays II",
@@ -11,7 +16,45 @@ import org.shvetsov.core.Level;
 )
 public class _350 {
     public int[] intersect(int[] nums1, int[] nums2) {
+        int[] cnt1 = new int[1001];
+        int[] cnt2 = new int[1001];
+        for (int i : nums1) {
+            cnt1[i]++;
+        }
+        for (int i : nums2) {
+            cnt2[i]++;
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < cnt1.length; i++) {
+            int cnt = Math.min(cnt1[i], cnt2[i]);
+            while (cnt > 0) {
+                res.add(i);
+                cnt--;
+            }
+        }
+        return res.stream().mapToInt(i -> i).toArray();
+    }
 
-        return null;
+    public static class WithSortedList {
+        public int[] intersect(int[] nums1, int[] nums2) {
+            int l1 = nums1.length;
+            int l2 = nums2.length;
+            int i = 0, j = 0, k = 0;
+            Arrays.sort(nums1);
+            Arrays.sort(nums2);
+            while (i < l1 && j < l2) {
+                if (nums1[i] < nums2[j]) {
+                    i++;
+                } else if (nums1[i] > nums2[j]) {
+                    j++;
+                } else {
+                    nums1[k] = nums1[i];
+                    i++;
+                    j++;
+                    k++;
+                }
+            }
+            return Arrays.copyOfRange(nums1, 0, k);
+        }
     }
 }
--- src/test/java/org/shvetsov/_350Test.java
+++ src/test/java/org/shvetsov/_350Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _350Test {
 
     private static _350 task;
+    private static _350.WithSortedList taskWithSortedList;
 
     @BeforeEach
     public void setup() {
         task = new _350();
+        taskWithSortedList = new _350.WithSortedList();
     }
 
     @Test
@@ -22,5 +24,14 @@ class _350Test {
     @Test
     void intersectExample2() {
         assertThat(task.intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})).containsExactlyInAnyOrder(4, 9);
+    }
+    @Test
+    void intersectWithSortedListExample1() {
+        assertThat(taskWithSortedList.intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2})).containsExactlyInAnyOrder(2, 2);
+    }
+
+    @Test
+    void intersectWithSortedListExample2() {
+        assertThat(taskWithSortedList.intersect(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})).containsExactlyInAnyOrder(4, 9);
     }
 }
