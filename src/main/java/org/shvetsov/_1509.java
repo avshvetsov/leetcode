6dc3b43f0fc536d7751057aa4305ab91e7da85ad
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-03 10:24:55 +0300

    solve 1509

--- src/main/java/org/shvetsov/_1509.java
+++ src/main/java/org/shvetsov/_1509.java
@@ -3,6 +3,10 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 @LeetCode(
         number = 1509,
         name = "Minimum Difference Between Largest and Smallest Value in Three Moves",
@@ -11,7 +15,40 @@ import org.shvetsov.core.Level;
 )
 public class _1509 {
     public int minDifference(int[] nums) {
+        int n = nums.length;
+        if (n < 5) return 0;
+        int[][] options = new int[][]{{3, 0}, {2, 1}, {1, 2}, {0, 3}};
+        Arrays.sort(nums);
+        int diff = nums[n - 1] - nums[0];
+        for (int[] option : options) {
+            diff = Math.min(diff, nums[n - option[0] - 1] - nums[option[1]]);
+        }
+        return diff;
+    }
 
-        return 0;
+    //time complexity - O(n)
+    public static class LinearTimeComplexity {
+        public int minDifference(int[] nums) {
+            int n = nums.length;
+            if (n < 5) return 0;
+            PriorityQueue<Integer> bottomQueue = new PriorityQueue<>(4, Comparator.naturalOrder());
+            PriorityQueue<Integer> topQueue = new PriorityQueue<>(4, Comparator.reverseOrder());
+            for (int i : nums) {
+                bottomQueue.add(i);
+                topQueue.add(i);
+            }
+            int[] bottom = new int[4];
+            int[] top = new int[4];
+            for (int i = 0; i < 4; i++) {
+                bottom[i] = bottomQueue.poll();
+                top[i] = topQueue.poll();
+            }
+            int[][] options = new int[][]{{3, 0}, {2, 1}, {1, 2}, {0, 3}};
+            int minDiff = Integer.MAX_VALUE;
+            for (int[] option : options) {
+                minDiff = Math.min(minDiff, top[option[0]] - bottom[option[1]]);
+            }
+            return minDiff;
+        }
     }
 }
--- src/test/java/org/shvetsov/_1509Test.java
+++ src/test/java/org/shvetsov/_1509Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _1509Test {
 
     private static _1509 task;
+    private static _1509.LinearTimeComplexity taskLinearTimeComplexity;
 
     @BeforeEach
     public void setup() {
         task = new _1509();
+        taskLinearTimeComplexity = new _1509.LinearTimeComplexity();
     }
 
     @Test
@@ -27,5 +29,20 @@ class _1509Test {
     @Test
     void minDifferenceExample3() {
         assertThat(task.minDifference(new int[]{3, 100, 20})).isEqualTo(0);
+    }
+
+    @Test
+    void minDifferenceLinearTimeComplexityExample1() {
+        assertThat(taskLinearTimeComplexity.minDifference(new int[]{5, 3, 2, 4})).isEqualTo(0);
+    }
+
+    @Test
+    void minDifferenceLinearTimeComplexityExample2() {
+        assertThat(taskLinearTimeComplexity.minDifference(new int[]{1, 5, 0, 10, 14})).isEqualTo(1);
+    }
+
+    @Test
+    void minDifferenceLinearTimeComplexityExample3() {
+        assertThat(taskLinearTimeComplexity.minDifference(new int[]{3, 100, 20})).isEqualTo(0);
     }
 }
