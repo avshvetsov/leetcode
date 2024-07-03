545af25eebd9a6b4c9afea735b184b3158a7df72
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-03 09:42:02 +0300

    setup 1509

---
+++ src/main/java/org/shvetsov/_1509.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1509,
+        name = "Minimum Difference Between Largest and Smallest Value in Three Moves",
+        level = Level.MEDIUM,
+        tags = {"Array", "Greedy", "Sorting"}
+)
+public class _1509 {
+    public int minDifference(int[] nums) {
+
+        return 0;
+    }
+}
