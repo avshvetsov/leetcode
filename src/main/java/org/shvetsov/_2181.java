b3413c3a23b4585d1cb8c0c49c7c1fd721ac07af
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-04 13:04:16 +0300

    solve 2181

--- src/main/java/org/shvetsov/_2181.java
+++ src/main/java/org/shvetsov/_2181.java
@@ -12,7 +12,23 @@ import org.shvetsov.core.Level;
 )
 public class _2181 {
     public ListNode mergeNodes(ListNode head) {
-
-        return head;
+        ListNode res = new ListNode();
+        ListNode resCurr = res;
+        ListNode curr = head;
+        int counter = 0;
+        do {
+            if (curr.val != 0) {
+                counter += curr.val;
+            } else {
+                if (counter > 0) {
+                    resCurr.next = new ListNode();
+                    resCurr = resCurr.next;
+                    resCurr.val = counter;
+                    counter = 0;
+                }
+            }
+            curr = curr.next;
+        } while (curr != null);
+        return res.next;
     }
 }
