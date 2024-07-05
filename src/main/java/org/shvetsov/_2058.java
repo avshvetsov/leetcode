aa81f387cf03d43f58eb1d1a4ed2179ab3d7eb6b
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-05 09:55:37 +0300

    setup 2058

---
+++ src/main/java/org/shvetsov/_2058.java
@@ -0,0 +1,19 @@
+package org.shvetsov;
+
+import org.shvetsov.classes.ListNode;
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2058,
+        name = "Find the Minimum and Maximum Number of Nodes Between Critical Points",
+        level = Level.MEDIUM,
+        tags = {"Linked List"}
+)
+public class _2058 {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+
+        return null;
+    }
+
+}
