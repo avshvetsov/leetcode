fef0430c1b1cd83cbf21c126019161466e26d8e8
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-05 10:31:42 +0300

    solve 2058

--- src/main/java/org/shvetsov/_2058.java
+++ src/main/java/org/shvetsov/_2058.java
@@ -4,6 +4,9 @@ import org.shvetsov.classes.ListNode;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @LeetCode(
         number = 2058,
         name = "Find the Minimum and Maximum Number of Nodes Between Critical Points",
@@ -12,8 +15,57 @@ import org.shvetsov.core.Level;
 )
 public class _2058 {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
+        ListNode prev = head;
+        ListNode cur = head.next;
+        ListNode next;
+        int pos = 1;
+        int firstCP = -1;
+        int lastCP = -1;
+        int min = Integer.MAX_VALUE;
+        while (cur != null) {
+            next = cur.next;
+            if (next != null && ((cur.val > prev.val && cur.val > next.val) || (cur.val < prev.val && cur.val < next.val))) {
+                if (firstCP == -1) firstCP = pos;
+                if (lastCP != -1) {
+                    min = Math.min(min, pos - lastCP);
+                }
+                lastCP = pos;
+            }
+            prev = cur;
+            cur = cur.next;
+            pos++;
+        }
+        if (firstCP == -1 || firstCP == lastCP) return new int[]{-1, -1};
+        return new int[]{min, lastCP - firstCP};
+    }
 
-        return null;
+    public static class withListOfCP {
+        public int[] nodesBetweenCriticalPoints(ListNode head) {
+            List<Integer> cp = new ArrayList<>();
+            ListNode prev = head;
+            ListNode cur = head.next;
+            ListNode next;
+            int pos = 1;
+            while (cur != null) {
+                next = cur.next;
+                if (next != null && ((cur.val > prev.val && cur.val > next.val) || (cur.val < prev.val && cur.val < next.val))) {
+                    cp.add(pos);
+                }
+                prev = cur;
+                cur = cur.next;
+                pos++;
+            }
+
+            if (cp.size() < 2) return new int[]{-1, -1};
+            int min = Integer.MAX_VALUE;
+            int max = cp.getLast() - cp.getFirst();
+            for (int i = 1; i < cp.size(); i++) {
+                min = Math.min(min, cp.get(i) - cp.get(i - 1));
+            }
+            return new int[]{min, max};
+        }
+
+
     }
 
 }
--- src/test/java/org/shvetsov/_2058Test.java
+++ src/test/java/org/shvetsov/_2058Test.java
@@ -9,10 +9,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _2058Test {
 
     private static _2058 task;
+    private static _2058.withListOfCP taskwithListOfCP;
 
     @BeforeEach
     public void setup() {
         task = new _2058();
+        taskwithListOfCP = new _2058.withListOfCP();
     }
 
     @Test
@@ -28,5 +30,20 @@ class _2058Test {
     @Test
     void nodesBetweenCriticalPointsExample3() {
         assertThat(task.nodesBetweenCriticalPoints(LinkedListUtils.constructListNode(1, 3, 2, 2, 3, 2, 2, 2, 7))).isEqualTo(new int[]{3, 3});
+    }
+
+    @Test
+    void nodesBetweenCriticalPointsWithListOfCPExample1() {
+        assertThat(taskwithListOfCP.nodesBetweenCriticalPoints(LinkedListUtils.constructListNode(3, 1))).isEqualTo(new int[]{-1, -1});
+    }
+
+    @Test
+    void nodesBetweenCriticalPointsWithListOfCPExample2() {
+        assertThat(taskwithListOfCP.nodesBetweenCriticalPoints(LinkedListUtils.constructListNode(5, 3, 1, 2, 5, 1, 2))).isEqualTo(new int[]{1, 3});
+    }
+
+    @Test
+    void nodesBetweenCriticalPointsWithListOfCPExample3() {
+        assertThat(taskwithListOfCP.nodesBetweenCriticalPoints(LinkedListUtils.constructListNode(1, 3, 2, 2, 3, 2, 2, 2, 7))).isEqualTo(new int[]{3, 3});
     }
 }
