62e86a4bc06d1955905dd979f827e6952c0f0f4d
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-08 09:38:40 +0300

    setup 1823

---
+++ src/main/java/org/shvetsov/_1823.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1823,
+        name = "Find the Winner of the Circular Game",
+        level = Level.MEDIUM,
+        tags = {"Array", "Math", "Recursion", "Queue", "Simulation"}
+)
+public class _1823 {
+    public int findTheWinner(int n, int k) {
+
+        return -1;
+    }
+}
