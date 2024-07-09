86fc42c68208f2073f77b22494ce343b48019376
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-09 09:56:39 +0300

    solve 1701

--- src/main/java/org/shvetsov/_1701.java
+++ src/main/java/org/shvetsov/_1701.java
@@ -11,7 +11,15 @@ import org.shvetsov.core.Level;
 )
 public class _1701 {
     public double averageWaitingTime(int[][] customers) {
-
-        return 0;
+        long sumOfTime = 0;
+        long endCookTime = 0;
+        for (int[] customer : customers) {
+            int arrival = customer[0];
+            int time = customer[1];
+            long startCookTime = Math.max(arrival, endCookTime);
+            endCookTime = startCookTime + time;
+            sumOfTime += endCookTime - arrival;
+        }
+        return (double) sumOfTime / customers.length;
     }
 }
