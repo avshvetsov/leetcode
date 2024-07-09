21120db74834a89fedbe283d38e8781d343a352c
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-09 09:25:39 +0300

    setup 1701

---
+++ src/main/java/org/shvetsov/_1701.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1701,
+        name = "Average Waiting Time",
+        level = Level.MEDIUM,
+        tags = {"Array", "Simulation"}
+)
+public class _1701 {
+    public double averageWaitingTime(int[][] customers) {
+
+        return 0;
+    }
+}
