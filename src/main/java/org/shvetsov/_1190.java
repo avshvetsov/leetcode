33c8d71b9a9f832e8cb65e7a102a94c38460a9f8
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-11 10:38:18 +0300

    solve 1190

--- src/main/java/org/shvetsov/_1190.java
+++ src/main/java/org/shvetsov/_1190.java
@@ -3,6 +3,10 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
 @LeetCode(
         number = 1190,
         name = "Reverse Substrings Between Each Pair of Parentheses",
@@ -11,6 +15,25 @@ import org.shvetsov.core.Level;
 )
 public class _1190 {
     public String reverseParentheses(String s) {
-        return "";
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != ')') {
+                stack.push(chars[i]);
+            } else {
+                Deque<Character> reversed = new LinkedList<>();
+                Character popped = stack.pop();
+                while (popped != '(') {
+                    reversed.addLast(popped);
+                    popped = stack.pop();
+                }
+                stack.addAll(reversed);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.insert(0, stack.pop());
+        }
+        return sb.toString();
     }
 }
