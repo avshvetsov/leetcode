b4586101d6acfd3a168e9677d02f9d61fff6bc0e
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-13 12:17:45 +0300

    solve 2751

--- src/main/java/org/shvetsov/_2751.java
+++ src/main/java/org/shvetsov/_2751.java
@@ -3,7 +3,7 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
-import java.util.List;
+import java.util.*;
 
 @LeetCode(
         number = 2751,
@@ -13,7 +13,91 @@ import java.util.List;
 )
 public class _2751 {
     public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        //build Robots
+        char[] dirs = directions.toCharArray();
+        Robot[] robots = new Robot[positions.length];
+        for (int i = 0; i < positions.length; i++) {
+            robots[i] = new Robot(positions[i], healths[i], dirs[i], i);
+        }
+        Arrays.sort(robots, Comparator.comparingInt(value -> value.pos));
+        //simulate
+        Stack<Robot> stack = new Stack<>();
+        for (Robot robot : robots) {
+            robotCombat(stack, robot);
+        }
 
-        return null;
+        return stack.stream()
+                .sorted(Comparator.comparingInt(r -> r.originalOrderNum))
+                .map(robot -> robot.health)
+                .toList();
+    }
+
+    private void robotCombat(Stack<Robot> stack, Robot robot) {
+        if (!stack.isEmpty() && stack.peek().direction == 'R' && robot.direction == 'L') {
+            Robot prev = stack.peek();
+            if (prev.health == robot.health) {
+                stack.pop();
+            } else if (prev.health > robot.health) {
+                prev.health--;
+            } else if (prev.health < robot.health) {
+                stack.pop();
+                robot.health--;
+                robotCombat(stack, robot);
+            }
+        } else {
+            stack.push(robot);
+        }
+    }
+
+    public static class Robot {
+        int pos;
+        int health;
+        char direction;
+        int originalOrderNum;
+
+        public Robot(int pos, int health, char direction, int originalOrderNum) {
+            this.pos = pos;
+            this.health = health;
+            this.direction = direction;
+            this.originalOrderNum = originalOrderNum;
+        }
+    }
+
+    public static class NoInnerClass {
+        public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+            //build Robots
+            char[] dirs = directions.toCharArray();
+            int[][] robots = new int[positions.length][4];
+            for (int i = 0; i < positions.length; i++) {
+                robots[i] = new int[]{positions[i], healths[i], dirs[i], i};
+            }
+            Arrays.sort(robots, Comparator.comparingInt(value -> value[0]));
+            //simulate
+            Stack<int[]> stack = new Stack<>();
+            for (int[] robot : robots) {
+                robotCombat(stack, robot);
+            }
+            return stack.stream()
+                    .sorted(Comparator.comparingInt(r -> r[3]))
+                    .map(robot -> robot[1])
+                    .toList();
+        }
+
+        private void robotCombat(Stack<int[]> stack, int[] robot) {
+            if (!stack.isEmpty() && stack.peek()[2] == 'R' && robot[2] == 'L') {
+                int[] prev = stack.peek();
+                if (prev[1] == robot[1]) {
+                    stack.pop();
+                } else if (prev[1] > robot[1]) {
+                    prev[1]--;
+                } else if (prev[1] < robot[1]) {
+                    stack.pop();
+                    robot[1]--;
+                    robotCombat(stack, robot);
+                }
+            } else {
+                stack.push(robot);
+            }
+        }
     }
 }
--- src/test/java/org/shvetsov/_2751Test.java
+++ src/test/java/org/shvetsov/_2751Test.java
@@ -10,10 +10,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _2751Test {
 
     private static _2751 task;
+    private static _2751.NoInnerClass taskNoInnerClass;
 
     @BeforeEach
     public void setup() {
         task = new _2751();
+        taskNoInnerClass = new _2751.NoInnerClass();
     }
 
     @Test
@@ -31,6 +33,23 @@ class _2751Test {
     @Test
     void survivedRobotsHealthsExample3() {
         assertThat(task.survivedRobotsHealths(new int[]{1, 2, 5, 6}, new int[]{10, 10, 11, 11}, "RLRL"))
+                .isEqualTo(List.of());
+    }
+    @Test
+    void survivedRobotsHealthsNoInnerClassExample1() {
+        assertThat(taskNoInnerClass.survivedRobotsHealths(new int[]{5, 4, 3, 2, 1}, new int[]{2, 17, 9, 15, 10}, "RRRRR"))
+                .isEqualTo(List.of(2, 17, 9, 15, 10));
+    }
+
+    @Test
+    void survivedRobotsHealthsNoInnerClassExample2() {
+        assertThat(taskNoInnerClass.survivedRobotsHealths(new int[]{3, 5, 2, 6}, new int[]{10, 10, 15, 12}, "RLRL"))
+                .isEqualTo(List.of(14));
+    }
+
+    @Test
+    void survivedRobotsHealthsNoInnerClassExample3() {
+        assertThat(taskNoInnerClass.survivedRobotsHealths(new int[]{1, 2, 5, 6}, new int[]{10, 10, 11, 11}, "RLRL"))
                 .isEqualTo(List.of());
     }
 }
