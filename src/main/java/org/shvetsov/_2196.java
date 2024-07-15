e4d4e33bfd53826079517a1e76d392c5943448ab
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-15 15:58:37 +0300

    solve 2196

--- src/main/java/org/shvetsov/_2196.java
+++ src/main/java/org/shvetsov/_2196.java
@@ -4,6 +4,11 @@ import org.shvetsov.classes.TreeNode;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 @LeetCode(
         number = 2196,
         name = "Create Binary Tree From Descriptions",
@@ -12,7 +17,58 @@ import org.shvetsov.core.Level;
 )
 public class _2196 {
     public TreeNode createBinaryTree(int[][] descriptions) {
+        TreeNode[] nodes = new TreeNode[10000];
+        Set<Integer> parents = new HashSet<>();
+        Set<Integer> children = new HashSet<>();
+        for (int[] description : descriptions) {
+            //construct
+            TreeNode parent = null;
+            TreeNode child = null;
+            if (nodes[description[0]] == null) {
+                parent = new TreeNode(description[0]);
+                nodes[description[0]] = parent;
+            } else parent = nodes[description[0]];
 
-        return null;
+            if (nodes[description[1]] == null) {
+                child = new TreeNode(description[1]);
+                nodes[description[1]] = child;
+            } else child = nodes[description[1]];
+
+            if (description[2] == 1) {
+                parent.left = child;
+            } else parent.right = child;
+
+            //find head
+            parents.add(parent.val);
+            children.add(child.val);
+        }
+        parents.removeAll(children);
+        return nodes[parents.toArray(new Integer[0])[0]];
+    }
+
+    public static class WithMap {
+        public TreeNode createBinaryTree(int[][] descriptions) {
+            Map<Integer, TreeNode> nodes = new HashMap<>();
+            Set<Integer> parents = new HashSet<>();
+            Set<Integer> children = new HashSet<>();
+            for (int[] description : descriptions) {
+                //construct
+                TreeNode parent = nodes.computeIfAbsent(description[0], k -> new TreeNode(description[0]));
+                TreeNode child = nodes.computeIfAbsent(description[1], k -> new TreeNode(description[1]));
+                if (description[2] == 1) {
+                    parent.left = child;
+                } else parent.right = child;
+
+                //find head
+                parents.add(parent.val);
+                children.add(child.val);
+            }
+            for (Integer parent : parents) {
+                if (!children.contains(parent)) {
+                    return nodes.get(parent);
+                }
+            }
+            return null;
+        }
     }
 }
--- src/test/java/org/shvetsov/_2196Test.java
+++ src/test/java/org/shvetsov/_2196Test.java
@@ -9,10 +9,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _2196Test {
 
     private static _2196 task;
+    private static _2196.WithMap taskWithMap;
 
     @BeforeEach
     public void setup() {
         task = new _2196();
+        taskWithMap = new _2196.WithMap();
     }
 
     @Test
@@ -24,6 +26,18 @@ class _2196Test {
     @Test
     void createBinaryTreeExample2() {
         assertThat(task.createBinaryTree(new int[][]{{1, 2, 1}, {2, 3, 0}, {3, 4, 1}}))
+                .isEqualTo(TreeUtils.constructTreeNode(1, 2, null, null, 3, 4));
+    }
+
+    @Test
+    void createBinaryTreeWithMapExample1() {
+        assertThat(taskWithMap.createBinaryTree(new int[][]{{20, 15, 1}, {20, 17, 0}, {50, 20, 1}, {50, 80, 0}, {80, 19, 1}}))
+                .isEqualTo(TreeUtils.constructTreeNode(50, 20, 80, 15, 17, 19));
+    }
+
+    @Test
+    void createBinaryTreeWithMapExample2() {
+        assertThat(taskWithMap.createBinaryTree(new int[][]{{1, 2, 1}, {2, 3, 0}, {3, 4, 1}}))
                 .isEqualTo(TreeUtils.constructTreeNode(1, 2, null, null, 3, 4));
     }
 }
