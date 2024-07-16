1f6fb89eb7aa6bce84f00a3babd228bc3559eb54
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-16 11:27:08 +0300

    setup 2096

---
+++ src/main/java/org/shvetsov/_2096.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.classes.TreeNode;
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2096,
+        name = "Step-By-Step Directions From a Binary Tree Node to Another",
+        level = Level.MEDIUM,
+        tags = {"String", "Tree", "Depth-First Search", "Binary Tree"}
+)
+public class _2096 {
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+
+        return "";
+    }
+}
