a3d75de925be8167fa9221ac00c5ebb717730d3a
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-17 10:57:42 +0300

    solve 1110

--- src/main/java/org/shvetsov/_1110.java
+++ src/main/java/org/shvetsov/_1110.java
@@ -4,7 +4,8 @@ import org.shvetsov.classes.TreeNode;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @LeetCode(
         number = 1110,
@@ -14,7 +15,36 @@ import java.util.List;
 )
 public class _1110 {
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        Set<Integer> toDeleteSet = Arrays.stream(to_delete).boxed().collect(Collectors.toSet());
+        boolean isDeleted = toDeleteSet.contains(root.val);
+        List<TreeNode> result = new ArrayList<>();
+        if (!isDeleted) {
+            result.add(root);
+        }
+        return dfs(result, root, toDeleteSet, isDeleted);
+    }
 
-        return null;
+    private List<TreeNode> dfs(List<TreeNode> result, TreeNode root, Set<Integer> toDeleteSet, boolean isDeleted) {
+        if (root.left != null) {
+            boolean isLeftDeleted = toDeleteSet.contains(root.left.val);
+            dfs(result, root.left, toDeleteSet, isLeftDeleted);
+            if (isDeleted && !isLeftDeleted) {
+                result.add(root.left);
+            }
+            if (isLeftDeleted) {
+                root.left = null;
+            }
+        }
+        if (root.right != null) {
+            boolean isRightDeleted = toDeleteSet.contains(root.right.val);
+            dfs(result, root.right, toDeleteSet, isRightDeleted);
+            if (isDeleted && !isRightDeleted) {
+                result.add(root.right);
+            }
+            if (isRightDeleted) {
+                root.right = null;
+            }
+        }
+        return result;
     }
 }
