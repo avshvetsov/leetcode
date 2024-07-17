582b5264edef260557bec0bd7795be5ca9302a69
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-17 09:45:53 +0300

    setup 1110

---
+++ src/main/java/org/shvetsov/_1110.java
@@ -0,0 +1,20 @@
+package org.shvetsov;
+
+import org.shvetsov.classes.TreeNode;
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+import java.util.List;
+
+@LeetCode(
+        number = 1110,
+        name = "Delete Nodes And Return Forest",
+        level = Level.MEDIUM,
+        tags = {"Array", "Hash Table", "Tree", "Depth-First Search", "Binary Tree"}
+)
+public class _1110 {
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+
+        return null;
+    }
+}
