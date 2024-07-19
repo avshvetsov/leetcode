2eaf6c267730e21197392a2c36f543a120701634
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-19 10:06:21 +0300

    solve 1380

--- src/main/java/org/shvetsov/_1380.java
+++ src/main/java/org/shvetsov/_1380.java
@@ -3,7 +3,10 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @LeetCode(
         number = 1380,
@@ -13,7 +16,23 @@ import java.util.List;
 )
 public class _1380 {
     public List<Integer> luckyNumbers (int[][] matrix) {
-
-        return null;
+        Set<Integer> minRow = new HashSet<>();
+        Set<Integer> maxCol = new HashSet<>();
+        for (int[] row : matrix) {
+            int min = Integer.MAX_VALUE;
+            for (int i : row) {
+                min = Math.min(min, i);
+            }
+            minRow.add(min);
+        }
+        for (int i = 0; i < matrix[0].length; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = 0; j < matrix.length; j++) {
+                max = Math.max(max, matrix[j][i]);
+            }
+            maxCol.add(max);
+        }
+        minRow.retainAll(maxCol);
+        return new ArrayList<>(minRow);
     }
 }
