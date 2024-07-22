7fb4715b39028f5459699a5008b61a42b5ba9ef8
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-22 12:07:58 +0300

    solve 2418

--- src/main/java/org/shvetsov/_2418.java
+++ src/main/java/org/shvetsov/_2418.java
@@ -3,6 +3,11 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 @LeetCode(
         number = 2418,
         name = "Sort the People",
@@ -11,7 +16,16 @@ import org.shvetsov.core.Level;
 )
 public class _2418 {
     public String[] sortPeople(String[] names, int[] heights) {
-
-        return null;
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] sortedH = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(sortedH);
+        for (int i = sortedH.length - 1; i >= 0; i--) {
+            map.put(sortedH[i], sortedH.length - 1 - i);
+        }
+        String[] res = new String[names.length];
+        for (int i = 0; i < res.length; i++) {
+            res[map.get(heights[i])] = names[i];
+        }
+        return res;
     }
 }
