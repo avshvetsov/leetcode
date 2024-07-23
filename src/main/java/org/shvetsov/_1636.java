c3cbd9ae5e8217c61b5c0b94c23d30c9d71a82df
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-23 09:36:31 +0300

    setup 1636

---
+++ src/main/java/org/shvetsov/_1636.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1636,
+        name = "Sort Array by Increasing Frequency",
+        level = Level.EASY,
+        tags = {"Array", "Hash Table", "Sorting"}
+)
+public class _1636 {
+
+    public int[] frequencySort(int[] nums) {
+
+        return null;
+    }
+}
