c31492164033a7742110cc287dbd126e81ed12c5
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-23 11:04:26 +0300

    solve 1636

--- src/main/java/org/shvetsov/_1636.java
+++ src/main/java/org/shvetsov/_1636.java
@@ -3,6 +3,11 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @LeetCode(
         number = 1636,
         name = "Sort Array by Increasing Frequency",
@@ -12,7 +17,18 @@ import org.shvetsov.core.Level;
 public class _1636 {
 
     public int[] frequencySort(int[] nums) {
-
-        return null;
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream().sorted((e1, e2) -> {
+                    if (!e1.getValue().equals(e2.getValue())) {
+                        return e1.getValue().compareTo(e2.getValue());
+                    } else {
+                        return e2.getKey().compareTo(e1.getKey());
+                    }
+                })
+                .flatMapToInt(entry -> IntStream.generate(entry::getKey).limit(entry.getValue()))
+                .toArray();
     }
 }
