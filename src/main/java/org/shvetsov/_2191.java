2bd5655b5e8fcd5b47d3eeb20d813fbb14333100
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-24 11:04:39 +0300

    solve 2191

--- src/main/java/org/shvetsov/_2191.java
+++ src/main/java/org/shvetsov/_2191.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
 @LeetCode(
         number = 2191,
         name = "Sort the Jumbled Numbers",
@@ -11,7 +14,25 @@ import org.shvetsov.core.Level;
 )
 public class _2191 {
     public int[] sortJumbled(int[] mapping, int[] nums) {
+        return IntStream.range(0, nums.length)
+                .mapToObj(i -> new int[]{nums[i], i, transform(mapping, nums[i])})
+                .sorted(Comparator
+                        .comparing((int[] ints) -> ints[2])
+                        .thenComparingInt((int[] ints) -> ints[1]))
+                .mapToInt(ints -> ints[0])
+                .toArray();
+    }
 
-        return null;
+    private int transform(int[] mapping, int num) {
+        int result = 0;
+        int p = 1;
+        int left;
+        do {
+            left = num % 10;
+            result += mapping[left] * p;
+            num /= 10;
+            p *= 10;
+        } while (num > 0);
+        return result;
     }
 }
