58e7c2ace788334bc199a62d9c4df69a505fde62
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-25 21:14:05 +0300

    solve 912

--- src/main/java/org/shvetsov/_912.java
+++ src/main/java/org/shvetsov/_912.java
@@ -10,8 +10,56 @@ import org.shvetsov.core.Level;
         tags = {"Array", "Divide and Conquer", "Sorting", "Heap (Priority Queue)", "Merge Sort", "Bucket Sort", "Radix Sort", "Counting Sort"}
 )
 public class _912 {
-    public int[] sortArray(int[] nums) {
 
-        return null;
+    public int[] sortArray(int[] nums) {
+        int[] freq = new int[2 * 50000 + 1];
+        for (int num : nums) {
+            freq[num + 50000]++;
+        }
+        int i = 0;
+        for (int j = 0; j < freq.length; j++) {
+            while (freq[j] > 0) {
+                nums[i] = j - 50000;
+                i++;
+                freq[j]--;
+            }
+        }
+        return nums;
     }
+
+
+    public static class QuickSort {
+        public int[] sortArray(int[] nums) {
+            quickSort(nums, 0, nums.length - 1);
+            return nums;
+        }
+
+        private void quickSort(int[] nums, int s, int e) {
+            if (e > s) {
+                int pivot = partition(nums, s, e);
+
+                quickSort(nums, s, pivot - 1);
+                quickSort(nums, pivot + 1, e);
+            }
+        }
+
+        private int partition(int[] nums, int s, int e) {
+            int pivot = nums[e];
+            int i = s - 1;
+            for (int j = s; j < e; j++) {
+                if (nums[j] < pivot) {
+                    i++;
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+            i++;
+            int temp = nums[i];
+            nums[i] = nums[e];
+            nums[e] = temp;
+            return i;
+        }
+    }
+
 }
--- src/test/java/org/shvetsov/_912Test.java
+++ src/test/java/org/shvetsov/_912Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _912Test {
 
     private static _912 task;
+    private static _912.QuickSort taskQuickSort;
 
     @BeforeEach
     public void setup() {
         task = new _912();
+        taskQuickSort = new _912.QuickSort();
     }
 
     @Test
@@ -22,5 +24,15 @@ class _912Test {
     @Test
     void sortArrayExample2() {
         assertThat(task.sortArray(new int[]{5, 1, 1, 2, 0, 0})).isEqualTo(new int[]{0, 0, 1, 1, 2, 5});
+    }
+
+    @Test
+    void sortArrayQuickSortExample1() {
+        assertThat(taskQuickSort.sortArray(new int[]{5, 2, 3, 1})).isEqualTo(new int[]{1, 2, 3, 5});
+    }
+
+    @Test
+    void sortArrayQuickSortExample2() {
+        assertThat(taskQuickSort.sortArray(new int[]{5, 1, 1, 2, 0, 0})).isEqualTo(new int[]{0, 0, 1, 1, 2, 5});
     }
 }
