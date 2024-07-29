29659da15de6c399d3be235312f940a2615c66fd
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-29 10:32:31 +0300

    solve 1395

--- src/main/java/org/shvetsov/_1395.java
+++ src/main/java/org/shvetsov/_1395.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @LeetCode(
         number = 1395,
         name = "Count Number of Teams",
@@ -11,7 +14,23 @@ import org.shvetsov.core.Level;
 )
 public class _1395 {
     public int numTeams(int[] rating) {
-
-        return 0;
+        int result = 0;
+        List<int[]> dp = new ArrayList<>();
+        for (int i = 0; i < rating.length; i++) {
+            int curr = rating[i];
+            int more = 0;
+            int less = 0;
+            for (int[] ints : dp) {
+                if (curr > ints[0]) {
+                    result += ints[1];
+                    more++;
+                } else if (curr < ints[0]) {
+                    result += ints[2];
+                    less++;
+                }
+            }
+            dp.add(new int[]{curr, more, less});
+        }
+        return result;
     }
 }
