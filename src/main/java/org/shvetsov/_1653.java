46b3efbac7e9cf0cb1786dab0c4ccd1162e4b75a
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-30 10:24:37 +0300

    solve 1653

--- src/main/java/org/shvetsov/_1653.java
+++ src/main/java/org/shvetsov/_1653.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Stack;
+
 @LeetCode(
         number = 1653,
         name = "Minimum Deletions to Make String Balanced",
@@ -11,7 +13,39 @@ import org.shvetsov.core.Level;
 )
 public class _1653 {
     public int minimumDeletions(String s) {
+        int res = Integer.MAX_VALUE;
+        char[] chars = s.toCharArray();
+        int[] dpl = new int[s.length()];
+        int[] dpr = new int[s.length()];
+        int l = 0, r = 0;
+        for (int i = 0, j = s.length() - 1; i <= s.length() && j >= 0; i++, j--) {
+            if (chars[i] == 'b') {
+                l++;
+            }
+            if (chars[j] == 'a') {
+                r++;
+            }
+            dpl[i] = l;
+            dpr[j] = r;
+        }
+        for (int i = 0; i < dpl.length; i++) {
+            res = Math.min(res, dpl[i] + dpr[i]);
+        }
+        return res-1;
+    }
 
-        return 0;
+    public static class WithStack {
+        public int minimumDeletions(String s) {
+            int res = 0;
+            Stack<Character> stack = new Stack<>();
+            char[] chars = s.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                if (!stack.isEmpty() && stack.peek() == 'b' && chars[i] == 'a') {
+                    stack.pop();
+                    res++;
+                } else stack.push(chars[i]);
+            }
+            return res;
+        }
     }
 }
--- src/test/java/org/shvetsov/_1653Test.java
+++ src/test/java/org/shvetsov/_1653Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _1653Test {
 
     private static _1653 task;
+    private static _1653.WithStack taskWithStack;
 
     @BeforeEach
     public void setup() {
         task = new _1653();
+        taskWithStack = new _1653.WithStack();
     }
 
     @Test
@@ -22,5 +24,15 @@ class _1653Test {
     @Test
     void minimumDeletionsExample2() {
         assertThat(task.minimumDeletions("bbaaaaabb")).isEqualTo(2);
+    }
+
+    @Test
+    void minimumDeletionsWithStackExample1() {
+        assertThat(taskWithStack.minimumDeletions("aababbab")).isEqualTo(2);
+    }
+
+    @Test
+    void minimumDeletionsWithStackExample2() {
+        assertThat(taskWithStack.minimumDeletions("bbaaaaabb")).isEqualTo(2);
     }
 }
