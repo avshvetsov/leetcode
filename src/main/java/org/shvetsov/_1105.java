0e93ee57b16b92e0f28729c7fafe70733202bdd4
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-07-31 10:09:39 +0300

    setup 1105

---
+++ src/main/java/org/shvetsov/_1105.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1105,
+        name = "Filling Bookcase Shelves",
+        level = Level.MEDIUM,
+        tags = {"Array", "Dynamic Programming"}
+)
+public class _1105 {
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+
+        return 0;
+    }
+}
