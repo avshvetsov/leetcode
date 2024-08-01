58e5c77c19eb30c15fe7733901ef666b841edff8
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-01 13:19:28 +0300

    solve 2678

--- src/main/java/org/shvetsov/_2678.java
+++ src/main/java/org/shvetsov/_2678.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 2678,
         name = "Number of Senior Citizens",
@@ -11,7 +13,9 @@ import org.shvetsov.core.Level;
 )
 public class _2678 {
     public int countSeniors(String[] details) {
-
-        return 0;
+        return (int) Arrays.stream(details)
+                .mapToInt(s -> Integer.parseInt(s.substring(11, 13)))
+                .filter(value -> value > 60)
+                .count();
     }
 }
