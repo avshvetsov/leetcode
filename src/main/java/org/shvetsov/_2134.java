f1d404bc7b6397e831dfbd6f7ca3f56de23517cc
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-02 11:40:28 +0300

    solve 2134

--- src/main/java/org/shvetsov/_2134.java
+++ src/main/java/org/shvetsov/_2134.java
@@ -11,26 +11,27 @@ import org.shvetsov.core.Level;
 )
 public class _2134 {
     public int minSwaps(int[] nums) {
-        int countOne = 0;
-        for (int num : nums) {
-            if (num == 1) countOne++;
-        }
-        int left = 0, right = countOne;
         int n = nums.length;
+        int totalOne = 0;
+        for (int num : nums) {
+            totalOne += num;
+        }
+
+        int left = 0, right = totalOne;
         int maxOneInRange = 0;
         for (int i = left; i < right; i++) {
-            if (nums[i] == 1) maxOneInRange++;
+            maxOneInRange += nums[i];
         }
-        int countInRange = maxOneInRange;
+        int countOneInRange = maxOneInRange;
         while (left < n) {
             if (right >= n) {
                 right = 0;
             }
-            countInRange = countInRange - nums[left] + nums[right];
+            countOneInRange = countOneInRange - nums[left] + nums[right];
             left++;
             right++;
-            maxOneInRange = Math.max(maxOneInRange, countInRange);
+            maxOneInRange = Math.max(maxOneInRange, countOneInRange);
         }
-        return countOne - maxOneInRange;
+        return totalOne - maxOneInRange;
     }
 }
