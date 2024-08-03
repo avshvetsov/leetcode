cfd0f41f3cad7e2a784fff1cf936d641890f9ab4
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-03 10:16:39 +0300

    setup 1460

---
+++ src/main/java/org/shvetsov/_1460.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1460,
+        name = "Make Two Arrays Equal by Reversing Subarrays",
+        level = Level.EASY,
+        tags = {"Array", "Hash Table", "Sorting"}
+)
+public class _1460 {
+    public boolean canBeEqual(int[] target, int[] arr) {
+
+        return false;
+    }
+}
