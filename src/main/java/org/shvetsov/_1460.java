37d2940fc904c5a888c94cfd1e8045d6d4c53f1e
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-03 10:30:27 +0300

    solve 1460

--- src/main/java/org/shvetsov/_1460.java
+++ src/main/java/org/shvetsov/_1460.java
@@ -11,7 +11,19 @@ import org.shvetsov.core.Level;
 )
 public class _1460 {
     public boolean canBeEqual(int[] target, int[] arr) {
-
-        return false;
+        //reversing subarray we can achive sorting arr witch order you want
+        int[] freq = new int[1001];
+        for (int i : arr) {
+            freq[i]++;
+        }
+        for (int i : target) {
+            freq[i]--;
+        }
+        for (int i : freq) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
