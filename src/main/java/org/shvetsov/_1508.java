83ed77605a44ce40c137d79603d280a554f4246b
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-04 11:31:29 +0300

    solve 1508

--- src/main/java/org/shvetsov/_1508.java
+++ src/main/java/org/shvetsov/_1508.java
@@ -3,6 +3,10 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 @LeetCode(
         number = 1508,
         name = "Range Sum of Sorted Subarray Sums",
@@ -11,7 +15,41 @@ import org.shvetsov.core.Level;
 )
 public class _1508 {
     public int rangeSum(int[] nums, int n, int left, int right) {
+        int[] sums = new int[n * (n + 1) / 2];
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                sums[k] = i != j ? sums[k - 1] + nums[j] : nums[j];
+                k++;
+            }
+        }
+        Arrays.sort(sums);
+        int res = 0;
+        for (int i = left - 1; i < right; i++) {
+            res = (int) ((res + sums[i]) % (1e9 + 7));
+        }
+        return res;
+    }
 
-        return 0;
+    public static class WithPQ {
+        public int rangeSum(int[] nums, int n, int left, int right) {
+            int res = 0;
+            PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(value -> value[0]));
+            for (int i = 0; i < nums.length; i++) {
+                pq.offer(new int[]{nums[i], i});
+            }
+            int k = 0;
+            while (k < right && !pq.isEmpty()) {
+                int[] curr = pq.poll();
+                if (k >= left - 1) {
+                    res = (int) ((res + curr[0]) % (1e9 + 7));
+                }
+                if (curr[1] + 1 < n) {
+                    pq.offer(new int[]{curr[0] + nums[curr[1] + 1], curr[1] + 1});
+                }
+                k++;
+            }
+            return res;
+        }
     }
 }
--- src/test/java/org/shvetsov/_1508Test.java
+++ src/test/java/org/shvetsov/_1508Test.java
@@ -8,22 +8,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _1508Test {
 
     private static _1508 task;
+    private static _1508.WithPQ taskWithPQ;
 
     @BeforeEach
     public void setup() {
         task = new _1508();
+        taskWithPQ = new _1508.WithPQ();
     }
 
     @Test
     void rangeSumExample1() {
         assertThat(task.rangeSum(new int[]{1, 2, 3, 4}, 4, 1, 5)).isEqualTo(13);
     }
+
     @Test
     void rangeSumExample2() {
-        assertThat(task.rangeSum(new int[]{1, 2, 3, 4}, 4, 1, 10)).isEqualTo(50);
+        assertThat(task.rangeSum(new int[]{1, 2, 3, 4}, 4, 3, 4)).isEqualTo(6);
     }
+
     @Test
     void rangeSumExample3() {
-        assertThat(task.rangeSum(new int[]{1, 2, 3, 4}, 4, 3, 3)).isEqualTo(6);
+        assertThat(task.rangeSum(new int[]{1, 2, 3, 4}, 4, 1, 10)).isEqualTo(50);
+    }
+
+    @Test
+    void rangeSumWithPQExample1() {
+        assertThat(taskWithPQ.rangeSum(new int[]{1, 2, 3, 4}, 4, 1, 5)).isEqualTo(13);
+    }
+
+    @Test
+    void rangeSumWithPQExample2() {
+        assertThat(taskWithPQ.rangeSum(new int[]{1, 2, 3, 4}, 4, 3, 4)).isEqualTo(6);
+    }
+
+    @Test
+    void rangeSumWithPQExample3() {
+        assertThat(taskWithPQ.rangeSum(new int[]{1, 2, 3, 4}, 4, 1, 10)).isEqualTo(50);
     }
 }
