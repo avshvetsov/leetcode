96e8c53a86e840c4478a7e234b1c392a1093e9ae
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-05 10:14:03 +0300

    solve 2053

--- src/main/java/org/shvetsov/_2053.java
+++ src/main/java/org/shvetsov/_2053.java
@@ -3,6 +3,11 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @LeetCode(
         number = 2053,
         name = "Kth Distinct String in an Array",
@@ -11,7 +16,16 @@ import org.shvetsov.core.Level;
 )
 public class _2053 {
     public String kthDistinct(String[] arr, int k) {
-
+        Map<String, Long> map = Arrays.stream(arr)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        for (String s : arr) {
+            if (map.get(s) == 1) {
+                k--;
+                if (k == 0) {
+                    return s;
+                }
+            }
+        }
         return "";
     }
 }
