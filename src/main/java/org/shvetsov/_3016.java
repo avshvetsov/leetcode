94b876a5812b95cf5e40c664679b51529abd8a38
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-06 10:14:45 +0300

    solve 3016

--- src/main/java/org/shvetsov/_3016.java
+++ src/main/java/org/shvetsov/_3016.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+import java.util.SortedMap;
+
 @LeetCode(
         number = 3016,
         name = "Minimum Number of Pushes to Type Word II",
@@ -11,7 +14,21 @@ import org.shvetsov.core.Level;
 )
 public class _3016 {
     public int minimumPushes(String word) {
-
-        return 0;
+        int[] counts = new int[26];
+        for (char c : word.toCharArray()) {
+            counts[c - 'a']++;
+        }
+        Arrays.sort(counts);
+        int res = 0;
+        int touches = 0;
+        int i = 0;
+        while (i < 26 && counts[25 - i] > 0) {
+            if (i % 8 == 0) {
+                touches++;
+            }
+            res += counts[25 - i] * touches;
+            i++;
+        }
+        return res;
     }
 }
