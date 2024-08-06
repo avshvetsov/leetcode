84df35d246ce8ea2c4135832e65f9e70d997bf9d
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-06 09:48:54 +0300

    setup 3016

---
+++ src/main/java/org/shvetsov/_3016.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 3016,
+        name = "Minimum Number of Pushes to Type Word II",
+        level = Level.MEDIUM,
+        tags = {"Hash Table", "String", "Greedy", "Sorting", "Counting"}
+)
+public class _3016 {
+    public int minimumPushes(String word) {
+
+        return 0;
+    }
+}
