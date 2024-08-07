d82dbe1e550ed2b148ebe76b29658f8b21fe0db9
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-07 10:38:28 +0300

    solve 273

--- src/main/java/org/shvetsov/_273.java
+++ src/main/java/org/shvetsov/_273.java
@@ -10,8 +10,44 @@ import org.shvetsov.core.Level;
         tags = {"Math", "String", "Recursion"}
 )
 public class _273 {
-    public String numberToWords(int num) {
+    static final String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    static final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    static final String[] thousands = {"", "Thousand", "Million", "Billion"};
+    static final Integer[] powers = {1, 1000, 1000000, 1000000000};
 
-        return null;
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+        StringBuilder sb = new StringBuilder();
+        int power = 3;
+        while (power >= 0) {
+            int div = powers[power];
+            int curr = num / div;
+            String helper = helper(curr);
+            sb.append(" ").append(helper).append(" ");
+            if (!helper.isBlank()) {
+                sb.append(" ").append(thousands[power]).append(" ");
+            }
+            num %= div;
+            power--;
+        }
+        return sb.toString().trim().replaceAll(" +", " ");
+    }
+
+    private String helper(int curr) {
+        StringBuilder sb = new StringBuilder();
+        int h = curr / 100;
+        if (h > 0) {
+            sb.append(" ").append(ones[h]).append(" Hundred ");
+        }
+        int t = curr % 100 / 10;
+        int o = curr % 10;
+        if (t > 1) {
+            sb.append(" ").append(tens[t]).append(" ").append(ones[o]).append(" ");
+        } else if (t == 1) {
+            sb.append(" ").append(ones[10 + o]).append(" ");
+        } else {
+            sb.append(" ").append(ones[o]).append(" ");
+        }
+        return sb.toString();
     }
 }
