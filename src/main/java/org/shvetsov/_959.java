dbb5c29d8faada5220fae61cd1ec32959facdbeb
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-10 09:28:35 +0300

    setup 959

---
+++ src/main/java/org/shvetsov/_959.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 959,
+        name = "Regions Cut By Slashes",
+        level = Level.MEDIUM,
+        tags = {"Array", "Hash Table", "Depth-First Search", "Breadth-First Search", "Union Find", "Matrix"}
+)
+public class _959 {
+    public int regionsBySlashes(String[] grid) {
+
+        return 0;
+    }
+}
