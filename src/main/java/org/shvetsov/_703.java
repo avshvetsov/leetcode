6573a04d080ab9b6112a73835fb1011f4e114aff
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-12 20:00:38 +0300

    setup 703

---
+++ src/main/java/org/shvetsov/_703.java
@@ -0,0 +1,27 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 703,
+        name = "Kth Largest Element in a Stream",
+        level = Level.EASY,
+        tags = {"Tree", "Design", "Binary Search Tree", "Heap (Priority Queue)", "Binary Tree", "Data Stream"}
+)
+public class _703 {
+
+
+    static class KthLargest {
+
+        public KthLargest(int k, int[] nums) {
+
+        }
+
+        public int add(int val) {
+
+            return 0;
+        }
+    }
+
+}
