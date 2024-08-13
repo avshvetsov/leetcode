94c330c241fa97bece64c86cc2e46d64d7cb5087
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-13 12:47:39 +0300

    solve 40

--- src/main/java/org/shvetsov/_40.java
+++ src/main/java/org/shvetsov/_40.java
@@ -3,7 +3,11 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @LeetCode(
         number = 40,
@@ -13,7 +17,55 @@ import java.util.List;
 )
 public class _40 {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        int[] freq = new int[target + 1];
+        for (int i : candidates) {
+            if (i < target + 1) {
+                freq[i]++;
+            }
+        }
+        helper(freq, target, new ArrayList<>(), result);
+        return result.stream().distinct().collect(Collectors.toList());
+    }
 
-        return null;
+    private void helper(int[] freq, int target, List<Integer> current, List<List<Integer>> result) {
+        if (target < 0) return;
+        else if (target == 0) {
+            ArrayList<Integer> e = new ArrayList<>(current);
+            e.sort(Integer::compareTo);
+            result.add(e);
+            return;
+        }
+
+        for (int i = target; i >= 0; i--) {
+            if (freq[i] > 0) {
+                freq[i]--;
+                current.add(i);
+                helper(freq, target - i, current, result);
+                current.removeLast();
+                freq[i]++;
+            }
+        }
+    }
+
+    public static class NoFreq {
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            List<List<Integer>> result = new ArrayList<>();
+            Arrays.sort(candidates);
+            backtracking(candidates, 0, target, new ArrayList<>(), result);
+            return result;
+        }
+
+        private void backtracking(int[] candidates, int index, int target, List<Integer> current, List<List<Integer>> result) {
+            if (target == 0) {
+                result.add(new ArrayList<>(current));
+            }
+            for (int i = index; i < candidates.length && target >= candidates[i]; i++) {
+                if (i != index && candidates[i] == candidates[i - 1]) continue;
+                current.add(candidates[i]);
+                backtracking(candidates, i + 1, target - candidates[i], current, result);
+                current.removeLast();
+            }
+        }
     }
 }
--- src/test/java/org/shvetsov/_40Test.java
+++ src/test/java/org/shvetsov/_40Test.java
@@ -10,21 +10,60 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _40Test {
 
     private static _40 task;
+    private static _40.NoFreq taskNoFreq;
 
     @BeforeEach
     public void setup() {
         task = new _40();
+        taskNoFreq = new _40.NoFreq();
     }
 
     @Test
     void combinationSum2Example1() {
         assertThat(task.combinationSum2(new int[]{10, 1, 2, 7, 6, 1, 5}, 8))
-                .hasSameElementsAs(List.of(List.of(1, 1, 6), List.of(1, 2, 5), List.of(1, 7), List.of(2, 6)));
+                .containsExactlyInAnyOrder(List.of(1, 1, 6), List.of(1, 2, 5), List.of(1, 7), List.of(2, 6));
     }
 
     @Test
     void combinationSum2Example2() {
         assertThat(task.combinationSum2(new int[]{2, 5, 2, 1, 2}, 5))
-                .hasSameElementsAs(List.of(List.of(1, 2, 2), List.of(5)));
+                .containsExactlyInAnyOrder(List.of(1, 2, 2), List.of(5));
     }
+
+    @Test
+    void combinationSum2TC2() {
+        assertThat(task.combinationSum2(new int[]{2}, 1))
+                .containsExactlyInAnyOrder();
+    }
+
+    @Test
+    void combinationSum2TC172() {
+        assertThat(task.combinationSum2(new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, 30))
+                .containsExactlyInAnyOrder(List.of(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1));
+    }
+
+    @Test
+    void combinationSum2NoFreqExample1() {
+        assertThat(taskNoFreq.combinationSum2(new int[]{10, 1, 2, 7, 6, 1, 5}, 8))
+                .containsExactlyInAnyOrder(List.of(1, 1, 6), List.of(1, 2, 5), List.of(1, 7), List.of(2, 6));
+    }
+
+    @Test
+    void combinationSum2NoFreqExample2() {
+        assertThat(taskNoFreq.combinationSum2(new int[]{2, 5, 2, 1, 2}, 5))
+                .containsExactlyInAnyOrder(List.of(1, 2, 2), List.of(5));
+    }
+
+    @Test
+    void combinationSum2NoFreqTC2() {
+        assertThat(taskNoFreq.combinationSum2(new int[]{2}, 1))
+                .containsExactlyInAnyOrder();
+    }
+
+    @Test
+    void combinationSum2NoFreqTC172() {
+        assertThat(taskNoFreq.combinationSum2(new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, 30))
+                .containsExactlyInAnyOrder(List.of(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1));
+    }
+
 }
