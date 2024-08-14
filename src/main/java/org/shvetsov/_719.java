eabdab6d87dd89a27e115a59f33c550a7612aca2
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-14 17:18:20 +0300

    solve 719

--- src/main/java/org/shvetsov/_719.java
+++ src/main/java/org/shvetsov/_719.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 719,
         name = "Find K-th Smallest Pair Distance",
@@ -11,7 +13,54 @@ import org.shvetsov.core.Level;
 )
 public class _719 {
     public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int minDist = 0;
+        int maxDist = nums[nums.length - 1] - nums[0];
+        while (maxDist > minDist) {
+            int mid = (maxDist + minDist) / 2;
+            int cnt = countUntilDist(nums, mid);
+            if (cnt < k) {
+                minDist = mid + 1;
+            } else {
+                maxDist = mid;
+            }
+        }
+        return minDist;
+    }
 
-        return -1;
+    private int countUntilDist(int[] nums, int dist) {
+        int res = 0;
+        int l = 0;
+        for (int r = 0; r < nums.length; r++) {
+            while (nums[r] - nums[l] > dist) {
+                l++;
+            }
+            res += r - l;
+        }
+        return res;
+    }
+
+    public static class BruteForce {
+        public int smallestDistancePair(int[] nums, int k) {
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            for (int num : nums) {
+                min = Math.min(min, num);
+                max = Math.max(max, num);
+            }
+
+            int[] dist = new int[max - min + 1];
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    dist[Math.abs(nums[i] - nums[j])]++;
+                }
+            }
+            int currDist = 0;
+            while (k > 0) {
+                k -= dist[currDist];
+                currDist++;
+            }
+            return currDist - 1;
+        }
     }
 }
--- src/test/java/org/shvetsov/_719Test.java
+++ src/test/java/org/shvetsov/_719Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _719Test {
 
     private static _719 task;
+    private static _719.BruteForce taskBruteForce;
 
     @BeforeEach
     public void setup() {
         task = new _719();
+        taskBruteForce = new _719.BruteForce();
     }
 
     @Test
@@ -27,5 +29,20 @@ class _719Test {
     @Test
     void smallestDistancePairExample3() {
         assertThat(task.smallestDistancePair(new int[]{1, 6, 1}, 3)).isEqualTo(5);
+    }
+
+    @Test
+    void smallestDistancePairBruteForceExample1() {
+        assertThat(taskBruteForce.smallestDistancePair(new int[]{1, 3, 1}, 1)).isEqualTo(0);
+    }
+
+    @Test
+    void smallestDistancePairBruteForceExample2() {
+        assertThat(taskBruteForce.smallestDistancePair(new int[]{1, 1, 1}, 2)).isEqualTo(0);
+    }
+
+    @Test
+    void smallestDistancePairBruteForceExample3() {
+        assertThat(taskBruteForce.smallestDistancePair(new int[]{1, 6, 1}, 3)).isEqualTo(5);
     }
 }
