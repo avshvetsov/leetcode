5ffa2cc3d85756f7c685b2e43110b010d03690ae
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-15 10:12:22 +0300

    solve 860

--- src/main/java/org/shvetsov/_860.java
+++ src/main/java/org/shvetsov/_860.java
@@ -11,7 +11,31 @@ import org.shvetsov.core.Level;
 )
 public class _860 {
     public boolean lemonadeChange(int[] bills) {
-
-        return false;
+        int[] cash = new int[3];
+        for (int bill : bills) {
+            switch (bill) {
+                case 5 -> cash[0]++;
+                case 10 -> {
+                    if (cash[0] > 0) {
+                        cash[0]--;
+                        cash[1]++;
+                    } else return false;
+                }
+                case 20 -> {
+                    if (cash[1] > 0 && cash[0] > 0) {
+                        cash[0]--;
+                        cash[1]--;
+                        cash[2]++;
+                    } else if (cash[0] >= 3) {
+                        cash[0] -= 3;
+                        cash[2]++;
+                    } else return false;
+                }
+                default -> {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
