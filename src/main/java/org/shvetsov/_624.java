cf222fad08f38e58c34c9d8c397dcac93ca61a79
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-18 12:51:13 +0300

    setup 624

---
+++ src/main/java/org/shvetsov/_624.java
@@ -0,0 +1,21 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+import java.util.List;
+
+@LeetCode(
+        number = 624,
+        name = "Maximum Distance in Arrays",
+        level = Level.MEDIUM,
+        tags = {"Array", "Greedy"}
+)
+public class _624 {
+
+    public int maxDistance(List<List<Integer>> arrays) {
+
+        return -1;
+    }
+
+}
---
+++ src/test/java/org/shvetsov/_624Test.java
@@ -0,0 +1,28 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _624Test {
+
+    private static _624 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _624();
+    }
+
+    @Test
+    void maxDistanceExample1() {
+        assertThat(task.maxDistance(List.of(List.of(1, 2, 3), List.of(4, 5), List.of(1, 2, 3)))).isEqualTo(4);
+    }
+
+    @Test
+    void maxDistanceExample2() {
+        assertThat(task.maxDistance(List.of(List.of(1), List.of(1)))).isEqualTo(0);
+    }
+}
