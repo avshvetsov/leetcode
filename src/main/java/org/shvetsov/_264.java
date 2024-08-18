76fc395c3a36e323ca016f3dedb18f6a92a976a4
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-18 12:44:10 +0300

    solve 264

--- src/main/java/org/shvetsov/_264.java
+++ src/main/java/org/shvetsov/_264.java
@@ -3,7 +3,10 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 @LeetCode(
         number = 264,
@@ -13,6 +16,41 @@ import java.util.PriorityQueue;
 )
 public class _264 {
     public int nthUglyNumber(int n) {
-        return -1;
+        SortedSet<Long> pq = new TreeSet<>(Comparator.naturalOrder());
+        pq.add(1L);
+        Long curr = 1L;
+        for (int i = 0; i < n; i++) {
+            curr = pq.removeFirst();
+            pq.add(curr * 2);
+            pq.add(curr * 3);
+            pq.add(curr * 5);
+        }
+        return curr.intValue();
+    }
+
+    public static class LinearComplexity {
+        public int nthUglyNumber(int n) {
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            int i2 = 1, i3 = 1, i5 = 1;
+            int u2 = dp[i2] * 2, u3 = dp[i3] * 3, u5 = dp[i5] * 5;
+            for (int i = 2; i <= n; i++) {
+                int curr = Math.min(u2, Math.min(u3, u5));
+                dp[i] = curr;
+                if (curr == u2) {
+                    i2++;
+                    u2 = dp[i2] * 2;
+                }
+                if (curr == u3) {
+                    i3++;
+                    u3 = dp[i3] * 3;
+                }
+                if (curr == u5) {
+                    i5++;
+                    u5 = dp[i5] * 5;
+                }
+            }
+            return dp[n];
+        }
     }
 }
--- src/test/java/org/shvetsov/_264Test.java
+++ src/test/java/org/shvetsov/_264Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _264Test {
 
     private static _264 task;
+    private static _264.LinearComplexity taskLinearComplexity;
 
     @BeforeEach
     public void setup() {
         task = new _264();
+        taskLinearComplexity = new _264.LinearComplexity();
     }
 
     @Test
@@ -22,5 +24,25 @@ class _264Test {
     @Test
     void nthUglyNumberExample2() {
         assertThat(task.nthUglyNumber(1)).isEqualTo(1);
+    }
+
+    @Test
+    void nthUglyNumberTC6() {
+        assertThat(task.nthUglyNumber(11)).isEqualTo(15);
+    }
+
+    @Test
+    void nthUglyNumberLinearComplexityExample1() {
+        assertThat(taskLinearComplexity.nthUglyNumber(10)).isEqualTo(12);
+    }
+
+    @Test
+    void nthUglyNumberLinearComplexityExample2() {
+        assertThat(taskLinearComplexity.nthUglyNumber(1)).isEqualTo(1);
+    }
+
+    @Test
+    void nthUglyNumberLinearComplexityTC6() {
+        assertThat(taskLinearComplexity.nthUglyNumber(11)).isEqualTo(15);
     }
 }
