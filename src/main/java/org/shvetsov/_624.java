cbf1c58be2258b56883c60fdf561a157683b43ae
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-18 13:25:19 +0300

    solve 624

--- src/main/java/org/shvetsov/_624.java
+++ src/main/java/org/shvetsov/_624.java
@@ -12,10 +12,17 @@ import java.util.List;
         tags = {"Array", "Greedy"}
 )
 public class _624 {
-
     public int maxDistance(List<List<Integer>> arrays) {
-
-        return -1;
+        int min = arrays.getFirst().getFirst();
+        int max = arrays.getFirst().getLast();
+        int distance = 0;
+        for (int i = 1; i < arrays.size(); i++) {
+            List<Integer> array = arrays.get(i);
+            distance = Math.max(distance, Math.max(max - array.getFirst(), array.getLast() - min));
+            min = Math.min(min, array.getFirst());
+            max = Math.max(max, array.getLast());
+        }
+        return distance;
     }
 
 }
--- src/test/java/org/shvetsov/_624Test.java
+++ src/test/java/org/shvetsov/_624Test.java
@@ -20,9 +20,14 @@ class _624Test {
     void maxDistanceExample1() {
         assertThat(task.maxDistance(List.of(List.of(1, 2, 3), List.of(4, 5), List.of(1, 2, 3)))).isEqualTo(4);
     }
-
     @Test
     void maxDistanceExample2() {
         assertThat(task.maxDistance(List.of(List.of(1), List.of(1)))).isEqualTo(0);
+    }
+
+    @Test
+    void maxDistanceTC89() {
+        assertThat(task.maxDistance(List.of(List.of(-8,-7,-7,-5,1,1,3,4), List.of(-2), List.of(-10,-10,-7,0,1,3), List.of(2))))
+                .isEqualTo(14);
     }
 }
