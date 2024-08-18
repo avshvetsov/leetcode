a237ac004671a86afa11ccdfe4f55f609b456c9d
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-18 14:51:25 +0300

    solve 1937 (only bruteforce)

--- src/main/java/org/shvetsov/_1937.java
+++ src/main/java/org/shvetsov/_1937.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @LeetCode(
         number = 1937,
         name = "Maximum Number of Points with Cost",
@@ -11,7 +14,31 @@ import org.shvetsov.core.Level;
 )
 public class _1937 {
     public long maxPoints(int[][] points) {
+        return -1;
+    }
 
-        return 0;
+    public static class BruteForce {
+        public long maxPoints(int[][] points) {
+            long[] dp = new long[points[0].length];
+            for (int i = 0; i < points[0].length; i++) {
+                dp[i] = points[0][i];
+            }
+            for (int i = 1; i < points.length; i++) {
+                long[] dpNew = new long[points[0].length];
+                for (int j = 0; j < points[i].length; j++) {
+                    long currMax = points[i][j];
+                    for (int d = 0; d < dp.length; d++) {
+                        currMax = Math.max(currMax, points[i][j] + dp[d] - Math.abs(d - j));
+                    }
+                    dpNew[j] = currMax;
+                }
+                dp = dpNew;
+            }
+            long res = Long.MIN_VALUE;
+            for (long i : dp) {
+                res = Math.max(res, i);
+            }
+            return res;
+        }
     }
 }
--- src/test/java/org/shvetsov/_1937Test.java
+++ src/test/java/org/shvetsov/_1937Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _1937Test {
 
     private static _1937 task;
+    private static _1937.BruteForce taskBruteForce;
 
     @BeforeEach
     public void setup() {
         task = new _1937();
+        taskBruteForce = new _1937.BruteForce();
     }
 
     @Test
@@ -22,5 +24,15 @@ class _1937Test {
     @Test
     void maxPointsExample2() {
         assertThat(task.maxPoints(new int[][]{{1, 5}, {2, 3}, {4, 2}})).isEqualTo(11);
+    }
+
+    @Test
+    void maxPointsBruteForceExample1() {
+        assertThat(taskBruteForce.maxPoints(new int[][]{{1, 2, 3}, {1, 5, 1}, {3, 1, 1}})).isEqualTo(9);
+    }
+
+    @Test
+    void maxPointsBruteForceExample2() {
+        assertThat(taskBruteForce.maxPoints(new int[][]{{1, 5}, {2, 3}, {4, 2}})).isEqualTo(11);
     }
 }
