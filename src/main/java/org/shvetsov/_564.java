0c3d54858ed8b34a6067dd62b2a1d33449bbe9e4
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-24 12:23:13 +0300

    solve 564

--- src/main/java/org/shvetsov/_564.java
+++ src/main/java/org/shvetsov/_564.java
@@ -11,7 +11,80 @@ import org.shvetsov.core.Level;
 )
 public class _564 {
     public String nearestPalindromic(String n) {
+        if (n.length() == 1) {
+            return n.equals("0") ? "1" : String.valueOf(Integer.parseInt(n) - 1);
+        }
+        String result = String.valueOf(Long.MAX_VALUE);
+        long current = Long.parseLong(n);
+        long minAbs = Long.MAX_VALUE;
+        for (PalindromeOptions option : PalindromeOptions.values()) {
+            String palindromeString = option.getPalindrome(n);
+            if (!palindromeString.equals(n)) {
+                long palindrome = Long.parseLong(palindromeString);
+                long abs = Math.abs(current - palindrome);
+                if (abs < minAbs) {
+                    minAbs = abs;
+                    result = palindromeString;
+                } else if (abs == minAbs) {
+                    result = String.valueOf(Math.min(palindrome, Long.parseLong(result)));
+                }
+            }
+        }
+        return result;
+    }
 
-        return null;
+    private enum PalindromeOptions {
+        CURRENT{
+            @Override
+            public String getPalindrome(String n) {
+                String half = n.substring(0, n.length() / 2);
+                if (n.length() % 2 == 0) {
+                    return half + new StringBuilder(half).reverse();
+                } else {
+                    return half + n.charAt(n.length()/2) + new StringBuilder(half).reverse();
+                }
+            }
+        },
+        DECREMENTED{
+            @Override
+            public String getPalindrome(String n) {
+                String half = n.substring(0, n.length() / 2);
+                if (n.length() % 2 == 0) {
+                    String dec = String.valueOf(Long.parseLong(half) - 1);
+                    return dec + new StringBuilder(dec).reverse();
+                } else {
+                    if (n.charAt(n.length() / 2) == '0') {
+                        return n;
+                    }
+                    return half + (Character.getNumericValue(n.charAt(n.length()/2)) - 1) + new StringBuilder(half).reverse();
+                }
+            }
+        },
+        INCREMENTED{
+            @Override
+            public String getPalindrome(String n) {
+                String half = n.substring(0, n.length() / 2);
+                if (n.length() % 2 == 0) {
+                    String dec = String.valueOf(Long.parseLong(half) + 1);
+                    return dec + new StringBuilder(dec).reverse();
+                } else {
+                    return half + (Character.getNumericValue(n.charAt(n.length()/2)) + 1) + new StringBuilder(half).reverse();
+                }
+            }
+        },
+        LOWER_RANK{
+            @Override
+            public String getPalindrome(String n) {
+                return "9".repeat(Math.max(0, n.length() - 1));
+            }
+        },
+        UPPER_RANK{
+            @Override
+            public String getPalindrome(String n) {
+                return "1" + "0".repeat(Math.max(0, n.length() - 1)) + "1";
+            }
+        };
+
+        public abstract String getPalindrome(String n);
     }
 }
--- src/test/java/org/shvetsov/_564Test.java
+++ src/test/java/org/shvetsov/_564Test.java
@@ -2,6 +2,8 @@ package org.shvetsov;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,4 +25,30 @@ class _564Test {
     void nearestPalindromicExample2() {
         assertThat(task.nearestPalindromic("1")).isEqualTo("0");
     }
+
+    @Test
+    void nearestPalindromicTC202() {
+        assertThat(task.nearestPalindromic("1095500901")).isEqualTo("1095445901");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1234, 1221",
+            "12345, 12321",
+            "9999, 10001",
+            "99999, 100001",
+            "10000, 9999",
+            "1001, 999",
+            "4554, 4444",
+            "6556, 6446",
+            "68786, 68686",
+            "99800, 99799",
+            "12120, 12121",
+            "12932, 12921"
+    })
+    void nearestPalindromicMyTC(String input, String expected) {
+        assertThat(task.nearestPalindromic(input)).isEqualTo(expected);
+    }
+
+
 }
