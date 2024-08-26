65cac2d6d4fbdb814f649dfb5b9002b02516d701
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-26 10:41:22 +0300

    setup 590, add Node & NodeUtils

---
+++ src/main/java/org/shvetsov/_590.java
@@ -0,0 +1,21 @@
+package org.shvetsov;
+
+import org.shvetsov.classes.Node;
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+import java.util.Collections;
+import java.util.List;
+
+@LeetCode(
+        number = 590,
+        name = "N-ary Tree Postorder Traversal",
+        level = Level.EASY,
+        tags = {"Stack", "Tree", "Depth-First Search"}
+)
+public class _590 {
+    public List<Integer> postorder(Node root) {
+
+        return Collections.emptyList();
+    }
+}
