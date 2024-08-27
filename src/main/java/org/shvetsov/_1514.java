831e6b3ec30e712f9e1155c8a996ab0e7c44dcf3
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-27 12:13:22 +0300

    solve 1514

--- src/main/java/org/shvetsov/_1514.java
+++ src/main/java/org/shvetsov/_1514.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.*;
+
 @LeetCode(
         number = 1514,
         name = "Path with Maximum Probability",
@@ -11,7 +13,32 @@ import org.shvetsov.core.Level;
 )
 public class _1514 {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+        Map<Integer, List<GraphNode>> graph = new HashMap<>();
+        for (int i = 0; i < edges.length; i++) {
+            int[] edge = edges[i];
+            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(new GraphNode(edge[1], succProb[i]));
+            graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(new GraphNode(edge[0], succProb[i]));
+        }
+        return dijkstra(graph, start_node, end_node);
+    }
 
+    private double dijkstra(Map<Integer, List<GraphNode>> graph, int startNode, int endNode) {
+        Set<Integer> visited = new HashSet<>();
+        PriorityQueue<GraphNode> pq = new PriorityQueue<>(Comparator.comparing(node -> node.probability, Comparator.reverseOrder()));
+        pq.add(new GraphNode(startNode, 1));
+        while (!pq.isEmpty()) {
+            GraphNode current = pq.poll();
+            if (current.vertex == endNode) return current.probability;
+            List<GraphNode> nexts = graph.getOrDefault(current.vertex, Collections.emptyList());
+            for (GraphNode next : nexts) {
+                if (!visited.contains(next.vertex)) {
+                    pq.add(new GraphNode(next.vertex, current.probability * next.probability));
+                }
+            }
+            visited.add(current.vertex);
+        }
         return 0;
     }
+
+    private record GraphNode(int vertex, double probability) {}
 }
--- src/test/java/org/shvetsov/_1514Test.java
+++ src/test/java/org/shvetsov/_1514Test.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 
 class _1514Test {
 
@@ -17,18 +18,24 @@ class _1514Test {
     @Test
     void maxProbabilityExample1() {
         assertThat(task.maxProbability(3, new int[][]{{0, 1}, {1, 2}, {0, 2}}, new double[]{0.5, 0.5, 0.2}, 0, 2))
-                .isEqualTo(0.25);
+                .isCloseTo(0.25, within(0.00001));
     }
 
     @Test
     void maxProbabilityExample2() {
         assertThat(task.maxProbability(3, new int[][]{{0,1}}, new double[]{0.5}, 0, 2))
-                .isEqualTo(0);
+                .isCloseTo(0, within(0.00001));
     }
 
     @Test
     void maxProbabilityExample3() {
         assertThat(task.maxProbability(3, new int[][]{{0,1},{1,2},{0,2}}, new double[]{0.5,0.5,0.3}, 0, 2))
-                .isEqualTo(0.3);
+                .isCloseTo(0.3, within(0.00001));
+    }
+    
+    @Test
+    void maxProbabilityTC7() {
+        assertThat(task.maxProbability(5, new int[][]{{2,3},{1,2},{3,4},{1,3},{1,4},{0,1},{2,4},{0,4},{0,2}}, new double[]{0.06,0.26,0.49,0.25,0.2,0.64,0.23,0.21,0.77}, 0, 3))
+                .isCloseTo(0.16000, within(0.00001));
     }
 }
