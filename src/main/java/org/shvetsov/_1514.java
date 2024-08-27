4f718bf309ae5201e4c75d54ae148786ee4b20fb
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-27 10:25:43 +0300

    setup 1514

---
+++ src/main/java/org/shvetsov/_1514.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1514,
+        name = "Path with Maximum Probability",
+        level = Level.MEDIUM,
+        tags = {"Array", "Graph", "Heap (Priority Queue)", "Shortest Path"}
+)
+public class _1514 {
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+
+        return 0;
+    }
+}
