69609c321f3dd1a951d5de0dac5958c91a913fa9
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-28 09:54:28 +0300

    setup 1905

---
+++ src/test/java/org/shvetsov/_1905Test.java
@@ -0,0 +1,28 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _1905Test {
+
+    private static _1905 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _1905();
+    }
+
+    @Test
+    void countSubIslandsExample1() {
+        assertThat(task.countSubIslands(new int[][]{{1, 1, 1, 0, 0}, {0, 1, 1, 1, 1}, {0, 0, 0, 0, 0}, {1, 0, 0, 0, 0}, {1, 1, 0, 1, 1}},
+                new int[][]{{1, 1, 1, 0, 0}, {0, 0, 1, 1, 1}, {0, 1, 0, 0, 0}, {1, 0, 1, 1, 0}, {0, 1, 0, 1, 0}})).isEqualTo(3);
+    }
+
+    @Test
+    void countSubIslandsExample2() {
+        assertThat(task.countSubIslands(new int[][]{{1, 0, 1, 0, 1}, {1, 1, 1, 1, 1}, {0, 0, 0, 0, 0}, {1, 1, 1, 1, 1}, {1, 0, 1, 0, 1}},
+                new int[][]{{0, 0, 0, 0, 0}, {1, 1, 1, 1, 1}, {0, 1, 0, 1, 0}, {0, 1, 0, 1, 0}, {1, 0, 0, 0, 1}})).isEqualTo(2);
+    }
+}
