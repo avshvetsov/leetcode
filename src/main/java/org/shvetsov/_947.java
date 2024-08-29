1d235bad8e38eb0457c706acaeab339e07aecaf7
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-29 10:09:29 +0300

    setup 947

---
+++ src/main/java/org/shvetsov/_947.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 947,
+        name = "Most Stones Removed with Same Row or Column",
+        level = Level.MEDIUM,
+        tags = {"Hash Table", "Depth-First Search", "Union Find", "Graph"}
+)
+public class _947 {
+    public int removeStones(int[][] stones) {
+
+        return 0;
+    }
+}
