a67c6ceb395f0e529c4a487c96808d4f35f6ec5f
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-29 13:27:02 +0300

    solve 947 not optimal

--- src/main/java/org/shvetsov/_947.java
+++ src/main/java/org/shvetsov/_947.java
@@ -3,15 +3,55 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.*;
+
 @LeetCode(
         number = 947,
         name = "Most Stones Removed with Same Row or Column",
         level = Level.MEDIUM,
-        tags = {"Hash Table", "Depth-First Search", "Union Find", "Graph"}
+        tags = {"Hash Table", "Depth-First Search", "Union Find", "Graph"},
+        todo = {"Not optimal", "Union Find"}
 )
 public class _947 {
-    public int removeStones(int[][] stones) {
 
+    public int removeStones(int[][] stones) {
         return 0;
+    }
+
+    public static class MapSolution {
+        public int removeStones(int[][] stones) {
+            Map<Integer, List<Stone>> graphRow = new HashMap<>();
+            Map<Integer, List<Stone>> graphCol = new HashMap<>();
+            Set<Stone> stoneSet = new HashSet<>();
+            for (int[] stone : stones) {
+                Stone s = new Stone(stone[0], stone[1]);
+                stoneSet.add(s);
+                graphRow.computeIfAbsent(s.row, k -> new ArrayList<>()).add(s);
+                graphCol.computeIfAbsent(s.col, k -> new ArrayList<>()).add(s);
+            }
+            int groups = 0;
+            while (!stoneSet.isEmpty()) {
+                Stone stone = stoneSet.stream().findFirst().get();
+                visitStones(graphRow, graphCol, stoneSet, stone);
+                groups++;
+            }
+            return stones.length - groups;
+        }
+
+        private void visitStones(Map<Integer, List<Stone>> graphRow, Map<Integer, List<Stone>> graphCol, Set<Stone> stoneSet, Stone stone) {
+            stoneSet.remove(stone);
+            for (Stone curr : graphRow.get(stone.row)) {
+                if (stoneSet.contains(curr)) {
+                    visitStones(graphRow, graphCol, stoneSet, curr);
+                }
+            }
+            for (Stone curr : graphCol.get(stone.col)) {
+                if (stoneSet.contains(curr)) {
+                    visitStones(graphRow, graphCol, stoneSet, curr);
+                }
+            }
+        }
+
+        private record Stone(int row, int col) {}
     }
 }
--- src/test/java/org/shvetsov/_947Test.java
+++ src/test/java/org/shvetsov/_947Test.java
@@ -1,6 +1,7 @@
 package org.shvetsov;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,24 +9,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _947Test {
 
     private static _947 task;
+    private static _947.MapSolution taskMapSolution;
 
     @BeforeEach
     public void setup() {
         task = new _947();
+        taskMapSolution = new _947.MapSolution();
     }
 
     @Test
+    @Disabled
     void removeStonesExample1() {
         assertThat(task.removeStones(new int[][]{{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}})).isEqualTo(5);
     }
 
     @Test
+    @Disabled
     void removeStonesExample2() {
         assertThat(task.removeStones(new int[][]{{0, 0}, {0, 2}, {1, 1}, {2, 0}, {2, 2}})).isEqualTo(3);
     }
 
     @Test
+    @Disabled
     void removeStonesExample3() {
         assertThat(task.removeStones(new int[][]{{0, 0}})).isEqualTo(0);
+    }
+
+    @Test
+    void removeStonesMapSolutionExample1() {
+        assertThat(taskMapSolution.removeStones(new int[][]{{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}})).isEqualTo(5);
+    }
+
+    @Test
+    void removeStonesMapSolutionExample2() {
+        assertThat(taskMapSolution.removeStones(new int[][]{{0, 0}, {0, 2}, {1, 1}, {2, 0}, {2, 2}})).isEqualTo(3);
+    }
+
+    @Test
+    void removeStonesMapSolutionExample3() {
+        assertThat(taskMapSolution.removeStones(new int[][]{{0, 0}})).isEqualTo(0);
     }
 }
