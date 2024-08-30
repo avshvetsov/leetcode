2aedad3a3b71f5f240d70dba02fd7ce1bf7a697a
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-08-30 14:12:54 +0300

    solve 2699

--- src/main/java/org/shvetsov/_2699.java
+++ src/main/java/org/shvetsov/_2699.java
@@ -1,7 +1,10 @@
 package org.shvetsov;
 
+import org.apache.commons.math3.util.Pair;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
+
+import java.util.*;
 
 @LeetCode(
         number = 2699,
@@ -10,8 +13,99 @@ import org.shvetsov.core.Level;
         tags = {"Graph", "Heap (Priority Queue)", "Shortest Path"}
 )
 public class _2699 {
-    public int[][] modifiedGraphEdges(int n, int[][] edges, int source, int destination, int target) {
 
+    public static final int MAX_DISTANCE = 2000000000;
+    public static final int[][] IMPOSSIBLE_RESULT = new int[0][0];
+
+    public int[][] modifiedGraphEdges(int n, int[][] edges, int source, int destination, int target) {
+        GraphNode shortestPath = calculateShortestPath(edges, source, destination);
+        if (shortestPath == null || shortestPath.distance > target) {
+            return IMPOSSIBLE_RESULT;
+        } else {
+            int[][] results;
+            do {
+                results = replaceWildcards(shortestPath, edges, target);
+                //verify that -1 replaced correctly and shortestPath distance not changed after replacement
+                shortestPath = calculateShortestPath(results, source, destination);
+            } while (results.length > 0 && shortestPath.distance != target);
+            return results;
+        }
+    }
+
+    private GraphNode calculateShortestPath(int[][] edges, int start, int end) {
+        Map<Integer, List<int[]>> graph = buildGraph(edges);
+        return dijkstra(graph, start, end);
+    }
+
+    private GraphNode dijkstra(Map<Integer, List<int[]>> graph, int start, int end) {
+        Set<Integer> visited = new HashSet<>();
+        PriorityQueue<GraphNode> pq = new PriorityQueue<>(Comparator.comparingInt(value -> value.distance));
+        pq.add(new GraphNode(start, 0, new int[]{start}));
+        while (!pq.isEmpty()) {
+            GraphNode current = pq.poll();
+            List<int[]> neighbors = Objects.requireNonNullElse(graph.get(current.vertex), Collections.emptyList());
+            if (current.vertex == end) {
+                return current;
+            }
+            for (int[] neighbor : neighbors) {
+                if (!visited.contains(neighbor[0])) {
+                    //count distance -1 as minimal value (1) - Math.abs(neighbor[1])
+                    pq.add(new GraphNode(neighbor[0], current.distance + Math.abs(neighbor[1]), addValueToArray(current.path, neighbor[0])));
+                }
+            }
+            visited.add(current.vertex);
+        }
         return null;
     }
+
+    //replace -1 to MAX_DISTANCE, if it not in the shortestPath
+    //replace -1 to 1, if it in the shortestPath, but not the first
+    //replace -1 to 1 + diff, if it in the shortestPath and it's the first
+    //if diff(shortestPath diff to target) > 0 and shortestPath not contains -1, return IMPOSSIBLE_RESULT
+    private int[][] replaceWildcards(GraphNode end, int[][] edges, int target) {
+        int diff = target - end.distance;
+        Set<Pair<Integer, Integer>> path = new HashSet<>();
+        for (int i = 1; i < end.path.length; i++) {
+            path.add(new Pair<>(end.path[i - 1], end.path[i]));
+        }
+        int[][] edgesCopy = copy2DArray(edges);
+        for (int[] edge : edgesCopy) {
+            if (edge[2] == -1) {
+                if (path.contains(new Pair<>(edge[0], edge[1])) || path.contains(new Pair<>(edge[1], edge[0]))) {
+                    edge[2] = 1 + diff;
+                    diff = 0;
+                } else {
+                    edge[2] = MAX_DISTANCE;
+                }
+            }
+        }
+        if (diff == 0) {
+            return edgesCopy;
+        } else return IMPOSSIBLE_RESULT;
+    }
+
+    private int[] addValueToArray(int[] array, int last) {
+        int[] result = Arrays.copyOf(array, array.length + 1);
+        result[result.length - 1] = last;
+        return result;
+    }
+
+    private Map<Integer, List<int[]>> buildGraph(int[][] edges) {
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] edge : edges) {
+            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(new int[]{edge[1], edge[2]});
+            graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(new int[]{edge[0], edge[2]});
+        }
+        return graph;
+    }
+
+    private int[][] copy2DArray(int[][] edges) {
+        int[][] edgesCopy = new int[edges.length][edges[0].length];
+        for (int i = 0; i < edges.length; i++) {
+            System.arraycopy(edges[i], 0, edgesCopy[i], 0, edges[i].length);
+        }
+        return edgesCopy;
+    }
+
+    private record GraphNode(int vertex, int distance, int[] path) {}
 }
--- src/test/java/org/shvetsov/_2699Test.java
+++ src/test/java/org/shvetsov/_2699Test.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.shvetsov._2699.MAX_DISTANCE;
 
 class _2699Test {
 
@@ -17,7 +18,7 @@ class _2699Test {
     @Test
     void modifiedGraphEdgesExample1() {
         assertThat(task.modifiedGraphEdges(5, new int[][]{{4, 1, -1}, {2, 0, -1}, {0, 3, -1}, {4, 3, -1}}, 0, 1, 5))
-                .isDeepEqualTo(new int[][]{{4, 1, 1}, {2, 0, 1}, {0, 3, 3}, {4, 3, 1}});
+                .isDeepEqualTo(new int[][]{{4, 1, 3}, {2, 0, MAX_DISTANCE}, {0, 3, 1}, {4, 3, 1}});
     }
 
     @Test
@@ -30,5 +31,17 @@ class _2699Test {
     void modifiedGraphEdgesExample3() {
         assertThat(task.modifiedGraphEdges(4, new int[][]{{1, 0, 4}, {1, 2, 3}, {2, 3, 5}, {0, 3, -1}}, 0, 2, 6))
                 .isDeepEqualTo(new int[][]{{1, 0, 4}, {1, 2, 3}, {2, 3, 5}, {0, 3, 1}});
+    }
+    
+    @Test
+    void modifiedGraphEdgesTC604() {
+        assertThat(task.modifiedGraphEdges(4, new int[][]{{0, 1, -1}, {1, 2, -1}, {3, 1, -1}, {3, 0, 2}, {0, 2, 5}}, 2, 3, 8))
+                .isEmpty();
+    }
+
+    @Test
+    void modifiedGraphEdgesTC726() {
+        assertThat(task.modifiedGraphEdges(4, new int[][]{{3, 0, -1}, {1, 2, -1}, {2, 3, -1}, {1, 3, 9}, {2, 0, 5}}, 0, 1, 7))
+                .isDeepEqualTo(new int[][]{{3, 0, MAX_DISTANCE}, {1, 2, 2}, {2, 3, MAX_DISTANCE}, {1, 3, 9}, {2, 0, 5}});
     }
 }
