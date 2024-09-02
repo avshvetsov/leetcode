1800ae65734cf68f7006f5fcb9b268ea791ee4b2
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-02 15:05:46 +0300

    setup 1894

--- src/main/java/org/shvetsov/_1894.java
+++ src/main/java/org/shvetsov/_1894.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 1894,
         name = "Find the Student that Will Replace the Chalk",
@@ -11,7 +13,14 @@ import org.shvetsov.core.Level;
 )
 public class _1894 {
     public int chalkReplacer(int[] chalk, int k) {
-
-        return 0;
+        int n = chalk.length;
+        long sum = Arrays.stream(chalk).mapToLong(Long::valueOf).sum();
+        k = (int) (k % sum);
+        int i = 0;
+        while (k - chalk[i] >= 0) {
+            k -= chalk[i];
+            i = (i + 1) % n;
+        }
+        return i % n;
     }
 }
