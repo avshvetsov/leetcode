48635b2464d1ba3eddc2f9b08cdf6c9604fd8787
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-03 13:18:36 +0300

    setup 1945

---
+++ src/main/java/org/shvetsov/_1945.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1945,
+        name = "Sum of Digits of String After Convert",
+        level = Level.EASY,
+        tags = {"String", "Simulation"}
+)
+public class _1945 {
+    public int getLucky(String s, int k) {
+
+        return -1;
+    }
+}
