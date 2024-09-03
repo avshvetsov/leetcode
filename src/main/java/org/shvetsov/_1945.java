f7d73faf8564c8ccc466edb0c5583179fa087b6b
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-03 13:26:37 +0300

    solve 1945

--- src/main/java/org/shvetsov/_1945.java
+++ src/main/java/org/shvetsov/_1945.java
@@ -11,7 +11,22 @@ import org.shvetsov.core.Level;
 )
 public class _1945 {
     public int getLucky(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            sb.append(c - 'a' + 1);
+        }
+        String res = sb.toString();
+        for (int i = k; i > 0; i--) {
+            res = sumOfDigits(res);
+        }
+        return Integer.parseInt(res);
+    }
 
-        return -1;
+    private String sumOfDigits(String s) {
+        int res = 0;
+        for (char c : s.toCharArray()) {
+            res += Character.getNumericValue(c);
+        }
+        return String.valueOf(res);
     }
 }
