ce82dd8c8c9da19608a6a31850f7965e7fa01a40
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-05 10:44:26 +0300

    setup 2028

---
+++ src/main/java/org/shvetsov/_2028.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2028,
+        name = "Find Missing Observations",
+        level = Level.MEDIUM,
+        tags = {"Array", "Math", "Simulation"}
+)
+public class _2028 {
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+
+        return null;
+    }
+}
