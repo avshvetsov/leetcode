3dfee6af650581891f325466e8907284dcd34a2d
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-06 14:56:02 +0300

    solve 3217

--- src/main/java/org/shvetsov/_3217.java
+++ src/main/java/org/shvetsov/_3217.java
@@ -4,6 +4,9 @@ import org.shvetsov.classes.ListNode;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @LeetCode(
         number = 3217,
         name = "Delete Nodes From Linked List Present in Array",
@@ -12,7 +15,31 @@ import org.shvetsov.core.Level;
 )
 public class _3217 {
     public ListNode modifiedList(int[] nums, ListNode head) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        while (set.contains(head.val)) {
+            head = head.next;
+        }
 
-        return null;
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = head.next;
+        while (next != null) {
+            if (set.contains(curr.val)) {
+                prev.next = next;
+                curr = next;
+                next = next.next;
+            } else {
+                prev = curr;
+                curr = next;
+                next = next.next;
+            }
+        }
+        if (set.contains(curr.val)) {
+            prev.next = null;
+        }
+        return head;
     }
 }
