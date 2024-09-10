802482d72f15b1425c7ed015eed6d25a7177a494
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-10 10:08:27 +0300

    setup 2807

---
+++ src/main/java/org/shvetsov/_2807.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.classes.ListNode;
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2807,
+        name = "Insert Greatest Common Divisors in Linked List",
+        level = Level.MEDIUM,
+        tags = {"Linked List", "Math", "Number Theory"}
+)
+public class _2807 {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+
+        return head;
+    }
+}
---
+++ src/test/java/org/shvetsov/_2807Test.java
@@ -0,0 +1,29 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.shvetsov.utils.LinkedListUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _2807Test {
+
+    private static _2807 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _2807();
+    }
+
+    @Test
+    void insertGreatestCommonDivisorsExample1() {
+        assertThat(task.insertGreatestCommonDivisors(LinkedListUtils.constructListNode(18, 6, 10, 3)))
+                .isEqualTo(LinkedListUtils.constructListNode(18, 6, 6, 2, 10, 1, 3));
+    }
+
+    @Test
+    void insertGreatestCommonDivisorsExample2() {
+        assertThat(task.insertGreatestCommonDivisors(LinkedListUtils.constructListNode(7)))
+                .isEqualTo(LinkedListUtils.constructListNode(7));
+    }
+}
