fc30f61fc1f0d211ce60786f73d0a7681156d1df
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-11 09:53:23 +0300

    setup 2220

---
+++ src/main/java/org/shvetsov/_2220.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2220,
+        name = "Minimum Bit Flips to Convert Number",
+        level = Level.EASY,
+        tags = {"Bit Manipulation"}
+)
+public class _2220 {
+    public int minBitFlips(int start, int goal) {
+
+        return -1;
+    }
+}
