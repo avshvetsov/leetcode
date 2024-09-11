a44a7c0120e09f7ea3506e9b9b4f7b5fd78d0e14
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-11 10:52:53 +0300

    solve 2220

--- src/main/java/org/shvetsov/_2220.java
+++ src/main/java/org/shvetsov/_2220.java
@@ -11,7 +11,24 @@ import org.shvetsov.core.Level;
 )
 public class _2220 {
     public int minBitFlips(int start, int goal) {
+        int xor = start ^ goal;
+        int result = 0;
+        while (xor != 0) {
+            result += xor & 1;
+            xor = xor >> 1;
+        }
+        return result;
+    }
 
-        return -1;
+    public static class BrianKernighan {
+        public int minBitFlips(int start, int goal) {
+            int xor = start ^ goal;
+            int result = 0;
+            while (xor != 0) {
+                xor &= xor - 1;
+                result++;
+            }
+            return result;
+        }
     }
 }
--- src/test/java/org/shvetsov/_2220Test.java
+++ src/test/java/org/shvetsov/_2220Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _2220Test {
 
     private static _2220 task;
+    private static _2220.BrianKernighan taskBrianKernighan;
 
     @BeforeEach
     public void setup() {
         task = new _2220();
+        taskBrianKernighan = new _2220.BrianKernighan();
     }
 
     @Test
@@ -22,5 +24,15 @@ class _2220Test {
     @Test
     void minBitFlipsExample2() {
         assertThat(task.minBitFlips(3, 4)).isEqualTo(3);
+    }
+
+    @Test
+    void minBitFlipsBrianKernighanExample1() {
+        assertThat(taskBrianKernighan.minBitFlips(10, 7)).isEqualTo(3);
+    }
+
+    @Test
+    void minBitFlipsBrianKernighanExample2() {
+        assertThat(taskBrianKernighan.minBitFlips(3, 4)).isEqualTo(3);
     }
 }
