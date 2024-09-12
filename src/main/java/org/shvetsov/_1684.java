e985c3be7b505f35a9ba9c7a839a1d8adcf79b54
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-12 10:07:52 +0300

    solve 1684

--- src/main/java/org/shvetsov/_1684.java
+++ src/main/java/org/shvetsov/_1684.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @LeetCode(
         number = 1684,
         name = "Count the Number of Consistent Strings",
@@ -11,7 +14,21 @@ import org.shvetsov.core.Level;
 )
 public class _1684 {
     public int countConsistentStrings(String allowed, String[] words) {
-
-        return -1;
+        Set<Character> set = new HashSet<>();
+        for (char c : allowed.toCharArray()) {
+            set.add(c);
+        }
+        int res = 0;
+        for (String word : words) {
+            boolean onlyAllowed = true;
+            for (char c : word.toCharArray()) {
+                if (!set.contains(c)) {
+                    onlyAllowed = false;
+                    break;
+                }
+            }
+            if (onlyAllowed) res++;
+        }
+        return res;
     }
 }
