d4b82a5029c9cd3d84d1d2be22a2c3642f9ece04
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-13 10:20:25 +0300

    solve 1310

--- src/main/java/org/shvetsov/_1310.java
+++ src/main/java/org/shvetsov/_1310.java
@@ -10,8 +10,32 @@ import org.shvetsov.core.Level;
         tags = {"Array", "Bit Manipulation", "Prefix Sum"}
 )
 public class _1310 {
-    public int[] xorQueries(int[] arr, int[][] queries) {
 
-        return new int[0];
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] prefixXor = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            prefixXor[i + 1] = prefixXor[i] ^ arr[i];
+        }
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            result[i] = prefixXor[queries[i][0]] ^ prefixXor[queries[i][1] + 1];
+        }
+        return result;
+    }
+
+
+    public int[] xorQueriesMLE(int[] arr, int[][] queries) {
+        int[][] prefixXor = new int[arr.length][arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            prefixXor[i][i] = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                prefixXor[i][j] = prefixXor[i][j - 1] ^ arr[j];
+            }
+        }
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            result[i] = prefixXor[queries[i][0]][queries[i][1]];
+        }
+        return result;
     }
 }
