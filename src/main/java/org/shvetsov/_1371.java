0da5695ad6a8b15db4c259ae046a41a5fa890a20
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-15 14:18:25 +0300

    setup 1371

---
+++ src/main/java/org/shvetsov/_1371.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1371,
+        name = "Find the Longest Substring Containing Vowels in Even Counts",
+        level = Level.MEDIUM,
+        tags = {"Hash Table", "String", "Bit Manipulation", "Prefix Sum"}
+)
+public class _1371 {
+    public int findTheLongestSubstring(String s) {
+
+        return -1;
+    }
+}
