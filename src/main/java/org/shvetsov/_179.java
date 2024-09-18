e947ea46919f3523a0cca5753459677c13a88f3c
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-18 10:58:03 +0300

    setup 179

---
+++ src/main/java/org/shvetsov/_179.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 179,
+        name = "Largest Number",
+        level = Level.MEDIUM,
+        tags = {"Array", "String", "Greedy", "Sorting"}
+)
+public class _179 {
+    public String largestNumber(int[] nums) {
+
+        return "";
+    }
+}
---
+++ src/test/java/org/shvetsov/_179Test.java
@@ -0,0 +1,26 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _179Test {
+
+    private static _179 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _179();
+    }
+
+    @Test
+    void largestNumberExample1() {
+        assertThat(task.largestNumber(new int[]{10, 2})).isEqualTo(210);
+    }
+
+    @Test
+    void largestNumberExample2() {
+        assertThat(task.largestNumber(new int[]{3, 30, 34, 5, 9})).isEqualTo(9534330);
+    }
+}
