073b825ae551066d11b57a62a25ca2036c30686d
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-19 12:27:01 +0300

    solve 241

--- src/main/java/org/shvetsov/_241.java
+++ src/main/java/org/shvetsov/_241.java
@@ -3,7 +3,7 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
-import java.util.List;
+import java.util.*;
 
 @LeetCode(
         number = 241,
@@ -12,8 +12,30 @@ import java.util.List;
         tags = {"Math", "String", "Dynamic Programming", "Recursion", "Memoization"}
 )
 public class _241 {
-    public List<Integer> diffWaysToCompute(String expression) {
+    private final Map<String, List<Integer>> map = new HashMap<>();
 
-        return null;
+    public List<Integer> diffWaysToCompute(String expression) {
+        if (expression == null || expression.isEmpty()) return Collections.emptyList();
+        if (expression.length() <= 2) return List.of(Integer.parseInt(expression));
+
+        if (map.containsKey(expression)) return map.get(expression);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < expression.length(); i++) {
+            if (Character.isDigit(expression.charAt(i))) continue;
+            char operator = expression.charAt(i);
+            List<Integer> left = diffWaysToCompute(expression.substring(0, i));
+            List<Integer> right = diffWaysToCompute(expression.substring(i + 1));
+            for (Integer leftVal : left) {
+                for (Integer rightVal : right) {
+                    switch (operator) {
+                        case '+' -> result.add(leftVal + rightVal);
+                        case '-' -> result.add(leftVal - rightVal);
+                        case '*' -> result.add(leftVal * rightVal);
+                    }
+                }
+            }
+        }
+        map.put(expression, result);
+        return result;
     }
 }
