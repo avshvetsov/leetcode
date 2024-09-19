f93732126a3de8231ec43de7512ca3656622c7f4
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-19 11:43:38 +0300

    setup 241

---
+++ src/main/java/org/shvetsov/_241.java
@@ -0,0 +1,19 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+import java.util.List;
+
+@LeetCode(
+        number = 241,
+        name = "Different Ways to Add Parentheses",
+        level = Level.MEDIUM,
+        tags = {"Math", "String", "Dynamic Programming", "Recursion", "Memoization"}
+)
+public class _241 {
+    public List<Integer> diffWaysToCompute(String expression) {
+
+        return null;
+    }
+}
