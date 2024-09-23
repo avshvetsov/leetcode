94ab511f33a505e587d45ede47b7f3c40274ad0a
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-23 16:46:38 +0300

    setup 2707

---
+++ src/main/java/org/shvetsov/_2707.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2707,
+        name = "Extra Characters in a String",
+        level = Level.MEDIUM,
+        tags = {"Array", "Hash Table", "String", "Dynamic Programming", "Trie"}
+)
+public class _2707 {
+    public int minExtraChar(String s, String[] dictionary) {
+
+        return 0;
+    }
+}
