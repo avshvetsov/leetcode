a446c550da56af368daa3ad6dc19f8dfb7584594
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-23 17:40:45 +0300

    solve 2707

--- src/main/java/org/shvetsov/_2707.java
+++ src/main/java/org/shvetsov/_2707.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @LeetCode(
         number = 2707,
         name = "Extra Characters in a String",
@@ -10,8 +13,17 @@ import org.shvetsov.core.Level;
         tags = {"Array", "Hash Table", "String", "Dynamic Programming", "Trie"}
 )
 public class _2707 {
+    private final Map<String, Integer> dp = new HashMap<>();
     public int minExtraChar(String s, String[] dictionary) {
-
-        return 0;
+        if (s.isEmpty()) return 0;
+        if (dp.containsKey(s)) return dp.get(s);
+        int result = minExtraChar(s.substring(1), dictionary) + 1;
+        for (String entry : dictionary) {
+            if (s.startsWith(entry)) {
+                result = Math.min(result, minExtraChar(s.substring(entry.length()), dictionary));
+            }
+        }
+        dp.put(s, result);
+        return result;
     }
 }
