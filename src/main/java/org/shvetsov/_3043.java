68028bd1164dea3aef55493b453af87c907bfd07
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-24 12:28:28 +0300

    solve 3043

--- src/main/java/org/shvetsov/_3043.java
+++ src/main/java/org/shvetsov/_3043.java
@@ -11,7 +11,51 @@ import org.shvetsov.core.Level;
 )
 public class _3043 {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        TrieNode root = new TrieNode();
+        //setup 1 array as trie
+        for (int val : arr1) {
+            TrieNode current = root;
+            for (char c : String.valueOf(val).toCharArray()) {
+                current = current.setAndGetChildren(c - '0');
+            }
+        }
+        //find longest prefix
+        int result = 0;
+        for (int val : arr2) {
+            TrieNode current = root;
+            int prefSize = 0;
+            for (char c : String.valueOf(val).toCharArray()) {
+                int i = c - '0';
+                if (current.isExist(i)) {
+                    current = current.getChildren(i);
+                    prefSize++;
+                } else break;
+            }
+            result = Math.max(result, prefSize);
+        }
+        return result;
+    }
 
-        return 0;
+    public static class TrieNode {
+        private final TrieNode[] children;
+
+        public TrieNode() {
+            children = new TrieNode[10];
+        }
+
+        public TrieNode setAndGetChildren(int i) {
+            if (children[i] == null) {
+                children[i] = new TrieNode();
+            }
+            return children[i];
+        }
+
+        public TrieNode getChildren(int i) {
+            return children[i];
+        }
+
+        public boolean isExist(int i) {
+            return children[i] != null;
+        }
     }
 }
