2bb5b46bf11b8d4cba0a95fc4eb96ccfa08d8db2
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-26 11:14:37 +0300

    solve 729

--- src/main/java/org/shvetsov/_729.java
+++ src/main/java/org/shvetsov/_729.java
@@ -3,6 +3,10 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
 @LeetCode(
         number = 729,
         name = "My Calendar I",
@@ -13,12 +17,22 @@ public class _729 {
 
     static class MyCalendar {
 
-        public MyCalendar() {
+        private final NavigableMap<Integer, Boolean> isFreeMap = new TreeMap<>();
 
+        public MyCalendar() {
+            isFreeMap.put(0, true);
         }
 
         public boolean book(int start, int end) {
-
+            Map.Entry<Integer, Boolean> isStartFree = isFreeMap.floorEntry(start);
+            Map.Entry<Integer, Boolean> isEndFree = isFreeMap.lowerEntry(end);
+            if (isEndFree.getValue() && isStartFree.getValue() && isEndFree.getKey() <= start) {
+                if (!isFreeMap.containsKey(end)) {
+                    isFreeMap.put(end, true);
+                }
+                isFreeMap.put(start, false);
+                return true;
+            }
             return false;
         }
     }
--- src/test/java/org/shvetsov/_729Test.java
+++ src/test/java/org/shvetsov/_729Test.java
@@ -20,4 +20,14 @@ class _729Test {
         assertThat(calendar.book(15, 25)).isFalse();
         assertThat(calendar.book(20, 30)).isTrue();
     }
+
+    @Test
+    void bookMyCalendarTC94() {
+        int[][] input = new int[][]{{20,29},{13,22},{44,50},{1,7},{2,10},{14,20},{19,25},{36,42},{45,50},{47,50},{39,45},{44,50},{16,25},{45,50},{45,50},{12,20},{21,29},{11,20},{12,17},{34,40},{10,18},{38,44},{23,32},{38,44},{15,20},{27,33},{34,42},{44,50},{35,40},{24,31}};
+        boolean[] expected = new boolean[]{true,false,true,true,false,true,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+        for (int i = 0; i < input.length; i++) {
+            assertThat(calendar.book(input[i][0], input[i][1])).as("Start <%s>, end <%s>, expected <%s>", input[i][0], input[i][1], expected[i])
+                    .isEqualTo(expected[i]);
+        }
+    }
 }
