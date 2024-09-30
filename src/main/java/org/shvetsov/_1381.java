fbd2b5645241ae3267685d3bfd0b89f1a621a829
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-09-30 16:17:18 +0300

    solve 1381

--- src/main/java/org/shvetsov/_1381.java
+++ src/main/java/org/shvetsov/_1381.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.*;
+
 @LeetCode(
         number = 1381,
         name = "Design a Stack With Increment Operation",
@@ -13,20 +15,32 @@ public class _1381 {
 
     static class CustomStack {
 
-        public CustomStack(int maxSize) {
+        List<Integer> stack;
+        int maxSize;
 
+        public CustomStack(int maxSize) {
+            this.stack = new LinkedList<>();
+            this.maxSize = maxSize;
         }
 
         public void push(int x) {
-
+            if (stack.size() < maxSize) {
+                stack.addLast(x);
+            }
         }
 
         public int pop() {
-            return 0;
+            if (stack.isEmpty()) return -1;
+            else return stack.removeLast();
         }
 
         public void increment(int k, int val) {
-
+            ListIterator<Integer> iterator = stack.listIterator();
+            while (iterator.hasNext() && k > 0) {
+                int curr = iterator.next();
+                iterator.set(curr + val);
+                k--;
+            }
         }
     }
 
