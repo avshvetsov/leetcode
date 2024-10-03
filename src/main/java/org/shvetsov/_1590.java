ff9558ca7448cb4a2c5957f76e30a5029a2be815
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-10-03 12:28:01 +0300

    solve 1590

--- src/main/java/org/shvetsov/_1590.java
+++ src/main/java/org/shvetsov/_1590.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @LeetCode(
         number = 1590,
         name = "Make Sum Divisible by P",
@@ -11,7 +14,45 @@ import org.shvetsov.core.Level;
 )
 public class _1590 {
     public int minSubarray(int[] nums, int p) {
+        long[] prefixSum = new long[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        int targetSumForRemove = (int) (prefixSum[prefixSum.length - 1] % p);
+        if (targetSumForRemove == 0) return 0;
 
-        return -1;
+        int minLength = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 0);
+        for (int i = 1; i < prefixSum.length; i++) {
+            long neededSum = prefixSum[i] - targetSumForRemove + p;
+            if (map.containsKey((int) (neededSum % p))) {
+                minLength = Math.min(minLength, i - map.get((int) (neededSum % p)));
+            }
+            map.put((int) (prefixSum[i] % p), i);
+        }
+        return minLength == nums.length ? -1 : minLength;
+    }
+
+    public static class BruteForce {
+        public int minSubarray(int[] nums, int p) {
+            long[] prefixSum = new long[nums.length + 1];
+            for (int i = 0; i < nums.length; i++) {
+                prefixSum[i + 1] = prefixSum[i] + nums[i];
+            }
+            int reminder = (int) (prefixSum[prefixSum.length - 1] % p);
+            if (reminder == 0) return 0;
+
+            for (int i = 1; i < prefixSum.length - 1; i++) {
+                int l = 0;
+                int r = l + i;
+                while (r < prefixSum.length) {
+                    if ((prefixSum[r] - prefixSum[l]) % p == reminder) return i;
+                    l++;
+                    r++;
+                }
+            }
+            return -1;
+        }
     }
 }
--- src/test/java/org/shvetsov/_1590Test.java
+++ src/test/java/org/shvetsov/_1590Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _1590Test {
 
     private static _1590 task;
+    private static _1590.BruteForce taskBruteForce;
 
     @BeforeEach
     public void setup() {
         task = new _1590();
+        taskBruteForce = new _1590.BruteForce();
     }
 
     @Test
@@ -27,5 +29,49 @@ class _1590Test {
     @Test
     void minSubarrayExample3() {
         assertThat(task.minSubarray(new int[]{1, 2, 3}, 3)).isEqualTo(0);
+    }
+
+    @Test
+    void minSubarrayTC92() {
+        assertThat(task.minSubarray(new int[]{1000000000, 1000000000, 1000000000}, 3)).isEqualTo(0);
+    }
+
+    @Test
+    void minSubarrayTC95() {
+        assertThat(task.minSubarray(new int[]{3, 6, 8, 1}, 8)).isEqualTo(-1);
+    }
+
+    @Test
+    void minSubarrayTC141() {
+        assertThat(task.minSubarray(new int[]{1000000000, 1, 1, 1, 1, 1}, 1000000000)).isEqualTo(5);
+    }
+    @Test
+    void minSubarrayBruteForceExample1() {
+        assertThat(taskBruteForce.minSubarray(new int[]{3, 1, 4, 2}, 6)).isEqualTo(1);
+    }
+
+    @Test
+    void minSubarrayBruteForceExample2() {
+        assertThat(taskBruteForce.minSubarray(new int[]{6, 3, 5, 2}, 9)).isEqualTo(2);
+    }
+
+    @Test
+    void minSubarrayBruteForceExample3() {
+        assertThat(taskBruteForce.minSubarray(new int[]{1, 2, 3}, 3)).isEqualTo(0);
+    }
+
+    @Test
+    void minSubarrayBruteForceTC92() {
+        assertThat(taskBruteForce.minSubarray(new int[]{1000000000, 1000000000, 1000000000}, 3)).isEqualTo(0);
+    }
+
+    @Test
+    void minSubarrayBruteForceTC95() {
+        assertThat(taskBruteForce.minSubarray(new int[]{3, 6, 8, 1}, 8)).isEqualTo(-1);
+    }
+
+    @Test
+    void minSubarrayBruteForceTC141() {
+        assertThat(taskBruteForce.minSubarray(new int[]{1000000000, 1, 1, 1, 1, 1}, 1000000000)).isEqualTo(5);
     }
 }
