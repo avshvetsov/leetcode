636d44fbafb5012da7b19bcb45b532661bf84052
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-10-04 12:05:11 +0300

    solve 2491

--- src/main/java/org/shvetsov/_2491.java
+++ src/main/java/org/shvetsov/_2491.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 2491,
         name = "Divide Players Into Teams of Equal Skill",
@@ -11,7 +13,17 @@ import org.shvetsov.core.Level;
 )
 public class _2491 {
     public long dividePlayers(int[] skill) {
-
-        return 0;
+        Arrays.sort(skill);
+        long result = 0;
+        int l = 0;
+        int r = skill.length - 1;
+        int sum = skill[l] + skill[r];
+        while (r > l) {
+            if (sum != skill[l] + skill[r]) return -1;
+            result += (long) skill[l] * skill[r];
+            l++;
+            r--;
+        }
+        return result;
     }
 }
