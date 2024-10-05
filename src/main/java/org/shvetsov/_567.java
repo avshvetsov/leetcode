4cfcef5b39f60ddf8c11900e269f367d77b8c18e
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-10-05 11:12:44 +0300

    solve 567

--- src/main/java/org/shvetsov/_567.java
+++ src/main/java/org/shvetsov/_567.java
@@ -11,7 +11,35 @@ import org.shvetsov.core.Level;
 )
 public class _567 {
     public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
 
+        int[] count = new int[26];
+        for (char c : s1.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        int l = 0, r = 0;
+        while (r < s2.length()) {
+            int lc = s2.charAt(l) - 'a';
+            int rc = s2.charAt(r) - 'a';
+            if (r < s1.length()) {
+                count[rc]--;
+                r++;
+            } else {
+                count[lc]++;
+                count[rc]--;
+                l++;
+                r++;
+            }
+            if (containsOnlyZeros(count)) return true;
+        }
         return false;
+    }
+
+    private boolean containsOnlyZeros(int[] count) {
+        for (int i : count) {
+            if (i != 0) return false;
+        }
+        return true;
     }
 }
