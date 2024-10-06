0058d76cd9183b677ce5f9b41f11cb381f0e3ab2
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-10-06 11:07:51 +0300

    setup 1813

---
+++ src/test/java/org/shvetsov/_1813Test.java
@@ -0,0 +1,29 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _1813Test {
+
+    private static _1813 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _1813();
+    }
+
+    @Test
+    void areSentencesSimilarExample1() {
+        assertThat(task.areSentencesSimilar("My name is Haley", "My Haley")).isTrue();
+    }
+    @Test
+    void areSentencesSimilarExample2() {
+        assertThat(task.areSentencesSimilar("of", "A lot of words")).isFalse();
+    }
+    @Test
+    void areSentencesSimilarExample3() {
+        assertThat(task.areSentencesSimilar("Eating right now", "Eating")).isTrue();
+    }
+}
