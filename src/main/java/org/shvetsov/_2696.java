a77704a10b515c339adae5a0169824f833465328
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-10-07 09:53:21 +0300

    solve 2696

--- src/main/java/org/shvetsov/_2696.java
+++ src/main/java/org/shvetsov/_2696.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Stack;
+
 @LeetCode(
         number = 2696,
         name = "Minimum String Length After Removing Substrings",
@@ -11,7 +13,29 @@ import org.shvetsov.core.Level;
 )
 public class _2696 {
     public int minLength(String s) {
+        int l;
+        do {
+            l = s.length();
+            s = s.replace("AB", "");
+            s = s.replace("CD", "");
+        } while (l != s.length());
+        return s.length();
+    }
 
-        return -1;
+    public static class StackSolution {
+        public int minLength(String s) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.toCharArray().length; i++) {
+                char curr = s.charAt(i);
+                if (stack.isEmpty()) {
+                    stack.push(curr);
+                } else if ((curr == 'B' && stack.peek() == 'A') || (curr == 'D' && stack.peek() == 'C')) {
+                    stack.pop();
+                } else {
+                    stack.push(curr);
+                }
+            }
+            return stack.size();
+        }
     }
 }
