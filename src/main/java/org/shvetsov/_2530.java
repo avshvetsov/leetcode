bcbdf2b045f2f4f9c838d5f0aac93e3fa51f7612
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-10-14 10:05:37 +0300

    solve 2530

--- src/main/java/org/shvetsov/_2530.java
+++ src/main/java/org/shvetsov/_2530.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 @LeetCode(
         number = 2530,
         name = "Maximal Score After Applying K Operations",
@@ -11,7 +14,17 @@ import org.shvetsov.core.Level;
 )
 public class _2530 {
     public long maxKelements(int[] nums, int k) {
-
-        return -1L;
+        long res = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int num : nums) {
+            pq.add(num);
+        }
+        while (k > 0) {
+            Integer polled = pq.poll();
+            res += polled;
+            pq.add(Math.ceilDiv(polled, 3));
+            k--;
+        }
+        return res;
     }
 }
