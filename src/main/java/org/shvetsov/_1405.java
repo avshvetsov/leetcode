a31a74de12c610d09701ea5762ebb7b4ae2ef605
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-10-16 09:53:58 +0300

    setup 1405

---
+++ src/main/java/org/shvetsov/_1405.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1405,
+        name = "Longest Happy String",
+        level = Level.MEDIUM,
+        tags = {"String", "Greedy", "Heap (Priority Queue)"}
+)
+public class _1405 {
+    public String longestDiverseString(int a, int b, int c) {
+
+        return null;
+    }
+}
