1e689a5ba11bf61989f12534062f29a2fb35d5cd
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-10-17 11:44:43 +0300

    solve 1405

--- src/main/java/org/shvetsov/_1405.java
+++ src/main/java/org/shvetsov/_1405.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 @LeetCode(
         number = 1405,
         name = "Longest Happy String",
@@ -11,7 +14,37 @@ import org.shvetsov.core.Level;
 )
 public class _1405 {
     public String longestDiverseString(int a, int b, int c) {
-
-        return null;
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparing(ints -> ints[0], Comparator.reverseOrder()));
+        pq.offer(new int[]{a, 'a' - 'a'});
+        pq.offer(new int[]{b, 'b' - 'a'});
+        pq.offer(new int[]{c, 'c' - 'a'});
+        int[] prev = new int[]{0, 0};
+        int[] curr = pq.poll();
+        while (curr[0] > 0) {
+            if (curr[1] == prev[1]) {
+                if (prev[0] < 2) {
+                    prev[0]++;
+                    curr[0]--;
+                } else {
+                    int[] next = pq.poll();
+                    pq.offer(curr);
+                    curr = next;
+                    if (curr[0] > 0) {
+                        curr[0]--;
+                        prev[0] = 1;
+                        prev[1] = curr[1];
+                    } else break;
+                }
+            } else {
+                curr[0]--;
+                prev[0] = 1;
+                prev[1] = curr[1];
+            }
+            sb.append((char) ('a' + curr[1]));
+            pq.offer(curr);
+            curr = pq.poll();
+        }
+        return sb.toString();
     }
 }
