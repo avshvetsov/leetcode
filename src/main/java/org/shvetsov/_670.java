c41f19fd12584be4f3c5e5e506f02e3eda78faae
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-10-17 13:04:08 +0300

    solve 670

--- src/main/java/org/shvetsov/_670.java
+++ src/main/java/org/shvetsov/_670.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 670,
         name = "Maximum Swap",
@@ -11,7 +13,47 @@ import org.shvetsov.core.Level;
 )
 public class _670 {
     public int maximumSwap(int num) {
+        char[] origin = String.valueOf(num).toCharArray();
+        int max = origin[origin.length - 1];
+        int maxIndex = origin.length - 1;
+        int swapR = -1;
+        int swapL = -1;
+        for (int i = origin.length - 2; i >= 0; i--) {
+            if (origin[i] > max) {
+                max = origin[i];
+                maxIndex = i;
+            } else if (origin[i] < max) {
+                swapR = maxIndex;
+                swapL = i;
+            }
+        }
+        if (swapL != -1 && swapR != -1) {
+            char temp = origin[swapL];
+            origin[swapL] = origin[swapR];
+            origin[swapR] = temp;
+        }
+        return Integer.parseInt(String.valueOf(origin));
+    }
 
-        return -1;
+    public static class NotOptimal {
+        public int maximumSwap(int num) {
+            char[] origin = String.valueOf(num).toCharArray();
+            char[] sorted = String.valueOf(num).toCharArray();
+            Arrays.sort(sorted);
+            int i = 0;
+            while (i < origin.length && origin[i] == sorted[sorted.length - i - 1]) {
+                i++;
+            }
+            if (i == origin.length) return num;
+
+            int j = origin.length - 1;
+            while (origin[j] != sorted[sorted.length - i - 1]) {
+                j--;
+            }
+            char temp = origin[i];
+            origin[i] = origin[j];
+            origin[j] = temp;
+            return Integer.parseInt(String.valueOf(origin));
+        }
     }
 }
--- src/test/java/org/shvetsov/_670Test.java
+++ src/test/java/org/shvetsov/_670Test.java
@@ -7,10 +7,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _670Test {
 
     private static _670 task;
+    private static _670.NotOptimal taskNotOptimal;
 
     @BeforeEach
     public void setup() {
         task = new _670();
+        taskNotOptimal = new _670.NotOptimal();
     }
 
     @Test
@@ -21,5 +23,25 @@ class _670Test {
     @Test
     void maximumSwapExample2() {
         assertThat(task.maximumSwap(9973)).isEqualTo(9973);
+    }
+
+    @Test
+    void maximumSwapTC105() {
+        assertThat(task.maximumSwap(98368)).isEqualTo(98863);
+    }
+
+    @Test
+    void maximumSwapNotOptimalExample1() {
+        assertThat(taskNotOptimal.maximumSwap(2736)).isEqualTo(7236);
+    }
+
+    @Test
+    void maximumSwapNotOptimalExample2() {
+        assertThat(taskNotOptimal.maximumSwap(9973)).isEqualTo(9973);
+    }
+
+    @Test
+    void maximumSwapNotOptimalTC105() {
+        assertThat(taskNotOptimal.maximumSwap(98368)).isEqualTo(98863);
     }
 }
