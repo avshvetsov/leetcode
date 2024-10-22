a0114f4c7bab6711652a991dfb6fd69c9ec461a4
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-10-22 10:32:04 +0300

    solve 2583

--- src/main/java/org/shvetsov/_2583.java
+++ src/main/java/org/shvetsov/_2583.java
@@ -4,6 +4,10 @@ import org.shvetsov.classes.TreeNode;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 @LeetCode(
         number = 2583,
         name = "Kth Largest Sum in a Binary Tree",
@@ -12,7 +16,27 @@ import org.shvetsov.core.Level;
 )
 public class _2583 {
     public long kthLargestLevelSum(TreeNode root, int k) {
-
-        return -1;
+        List<Long> sums = new ArrayList<>();
+        List<TreeNode> current = new ArrayList<>();
+        current.add(root);
+        while (!current.isEmpty()) {
+            long sum = 0L;
+            List<TreeNode> next = new ArrayList<>();
+            for (TreeNode tn : current) {
+                sum += tn.val;
+                if (tn.left != null) {
+                    next.add(tn.left);
+                }
+                if (tn.right != null) {
+                    next.add(tn.right);
+                }
+            }
+            sums.add(sum);
+            current = next;
+        }
+        sums.sort(Comparator.reverseOrder());
+        if (sums.size() >= k) {
+            return sums.get(k - 1);
+        } else return -1;
     }
 }
