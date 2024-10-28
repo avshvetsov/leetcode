704e860f914ceba967e6e2b57545a386c53c342c
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-10-28 10:30:39 +0200

    solve 2501

--- src/main/java/org/shvetsov/_2501.java
+++ src/main/java/org/shvetsov/_2501.java
@@ -11,7 +11,24 @@ import org.shvetsov.core.Level;
 )
 public class _2501 {
     public int longestSquareStreak(int[] nums) {
-
-        return -1;
+        boolean[] numsTransformed = new boolean[100001];
+        for (int num : nums) {
+            numsTransformed[num] = true;
+        }
+        int MAX_POSSIBLE_SQUARE = (int) Math.ceil(Math.sqrt(100000));
+        int streak = -1;
+        for (int i = 0; i < MAX_POSSIBLE_SQUARE; i++) {
+            int currentStreak = 0;
+            int j = i;
+            while (numsTransformed[j]) {
+                numsTransformed[j] = false;
+                currentStreak++;
+                if (j < MAX_POSSIBLE_SQUARE) {
+                    j *= j;
+                } else break;
+            }
+            if (currentStreak > 1) streak = Math.max(streak, currentStreak);
+        }
+        return streak;
     }
 }
