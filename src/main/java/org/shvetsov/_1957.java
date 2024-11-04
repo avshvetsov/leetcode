a9578d7bd4b6810fc5038b2fe53137aacd04b267
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-11-04 13:43:59 +0200

    setup 1957

---
+++ src/main/java/org/shvetsov/_1957.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1957,
+        name = "Delete Characters to Make Fancy String",
+        level = Level.EASY,
+        tags = {"String"}
+)
+public class _1957 {
+    public String makeFancyString(String s) {
+
+        return null;
+    }
+}
