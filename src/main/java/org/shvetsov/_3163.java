e95a4886d9e2b033feb4a0aefde11034f43a316f
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-11-04 14:13:33 +0200

    solve 3163

--- src/main/java/org/shvetsov/_3163.java
+++ src/main/java/org/shvetsov/_3163.java
@@ -11,7 +11,27 @@ import org.shvetsov.core.Level;
 )
 public class _3163 {
     public String compressedString(String word) {
-
-        return null;
+        StringBuilder sb = new StringBuilder();
+        char prev = word.charAt(0);
+        int count = 0;
+        for (char c : word.toCharArray()) {
+            if (c == prev) {
+                if (count < 9) {
+                    count++;
+                } else {
+                    sb.append(count);
+                    sb.append(prev);
+                    count = 1;
+                }
+            } else {
+                sb.append(count);
+                sb.append(prev);
+                prev = c;
+                count = 1;
+            }
+        }
+        sb.append(count);
+        sb.append(prev);
+        return sb.toString();
     }
 }
