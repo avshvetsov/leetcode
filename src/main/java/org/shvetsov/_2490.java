b15a82760e179c6e98f80796b600fc5e945d05de
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-11-04 13:50:19 +0200

    setup 2490

---
+++ src/main/java/org/shvetsov/_2490.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2490,
+        name = "Circular Sentence",
+        level = Level.EASY,
+        tags = {"String"}
+)
+public class _2490 {
+    public boolean isCircularSentence(String sentence) {
+
+        return false;
+    }
+}
