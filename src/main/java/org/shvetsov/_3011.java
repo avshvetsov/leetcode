e3936ba2265b30e20e5ca502ed0f11e36f2851f2
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-11-06 10:14:17 +0200

    setup 3011

---
+++ src/main/java/org/shvetsov/_3011.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 3011,
+        name = "Find if Array Can Be Sorted",
+        level = Level.MEDIUM,
+        tags = {"Array", "Bit Manipulation", "Sorting"}
+)
+public class _3011 {
+    public boolean canSortArray(int[] nums) {
+
+        return false;
+    }
+}
