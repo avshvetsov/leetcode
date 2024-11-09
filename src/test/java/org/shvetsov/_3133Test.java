1b01ffc5e15de85cd6eca5de71bacee8ea441174
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-11-09 10:46:50 +0200

    setup 3133

---
+++ src/test/java/org/shvetsov/_3133Test.java
@@ -0,0 +1,26 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _3133Test {
+
+    private static _3133 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _3133();
+    }
+
+    @Test
+    void minEndExample1() {
+        assertThat(task.minEnd(3, 4)).isEqualTo(6);
+    }
+
+    @Test
+    void minEndExample2() {
+        assertThat(task.minEnd(2, 7)).isEqualTo(15);
+    }
+}
