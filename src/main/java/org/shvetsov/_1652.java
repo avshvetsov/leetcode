c543a80e5c8456d226a6ee94c53651cb3e62f614
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-11-18 18:19:12 +0200

    solve 1652

--- src/main/java/org/shvetsov/_1652.java
+++ src/main/java/org/shvetsov/_1652.java
@@ -11,7 +11,45 @@ import org.shvetsov.core.Level;
 )
 public class _1652 {
     public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] result = new int[n];
+        if (k == 0) return result;
+        for (int i = 0; i < code.length; i++) {
+            int sum = 0;
+            for (int j = Math.abs(k); j > 0; j--) {
+                int current = k > 0 ? code[(i + j + n) % n] : code[(i - j + n) % n];
+                sum += current;
+            }
+            result[i] = sum;
+        }
+        return result;
+    }
 
-        return null;
+
+    public static class SlidingWindow {
+        public int[] decrypt(int[] code, int k) {
+            int n = code.length;
+            int[] result = new int[n];
+            if (k == 0) return result;
+            int start, end, sum = 0;
+            if (k > 0) {
+                start = 1;
+                end = k;
+            }
+            else {
+                start = n - Math.abs(k);
+                end = n - 1;
+            }
+            for (int i = start; i <= end; i++) sum += code[i];
+
+            for (int i = 0; i < n; i++) {
+                result[i] = sum;
+                sum -= code[(start) % n];
+                end++;
+                sum += code[(end) % n];
+                start++;
+            }
+            return result;
+        }
     }
 }
--- src/test/java/org/shvetsov/_1652Test.java
+++ src/test/java/org/shvetsov/_1652Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _1652Test {
 
     private static _1652 task;
+    private static _1652.SlidingWindow taskSlidingWindow;
 
     @BeforeEach
     public void setup() {
         task = new _1652();
+        taskSlidingWindow = new _1652.SlidingWindow();
     }
 
     @Test
@@ -26,6 +28,21 @@ class _1652Test {
 
     @Test
     void decryptExample3() {
-        assertThat(task.decrypt(new int[]{2, 4, 9, 3}, -1)).containsExactly(12, 5, 6, 13);
+        assertThat(task.decrypt(new int[]{2, 4, 9, 3}, -2)).containsExactly(12, 5, 6, 13);
+    }
+
+    @Test
+    void decryptSlidingWindowExample1() {
+        assertThat(taskSlidingWindow.decrypt(new int[]{5, 7, 1, 4}, 3)).containsExactly(12, 10, 16, 13);
+    }
+
+    @Test
+    void decryptSlidingWindowExample2() {
+        assertThat(taskSlidingWindow.decrypt(new int[]{1, 2, 3, 4}, 0)).containsExactly(0, 0, 0, 0);
+    }
+
+    @Test
+    void decryptSlidingWindowExample3() {
+        assertThat(taskSlidingWindow.decrypt(new int[]{2, 4, 9, 3}, -2)).containsExactly(12, 5, 6, 13);
     }
 }
