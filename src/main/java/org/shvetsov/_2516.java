9fb5e69c3adec4510722046af051483956aad925
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-11-20 14:47:59 +0200

    solve 2516

--- src/main/java/org/shvetsov/_2516.java
+++ src/main/java/org/shvetsov/_2516.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 2516,
         name = "Take K of Each Character From Left and Right",
@@ -11,7 +13,97 @@ import org.shvetsov.core.Level;
 )
 public class _2516 {
     public int takeCharacters(String s, int k) {
-
-        return 1;
+        if (k == 0) return 0;
+        int n = s.length();
+        char[] chars = s.toCharArray();
+        int[] leftCount = new int[3];
+        int[] rightCount = new int[3];
+        int i = n - 1;
+        while (rightCount[0] < k || rightCount[1] < k || rightCount[2] < k) {
+            if (i < 0) return -1;
+            rightCount[chars[i] - 'a']++;
+            i--;
+        }
+        int left = 0;
+        int right = n - i - 1;
+        int result = Integer.MAX_VALUE;
+        while (left < n && right > 0) {
+            while (right > 0 && rightCount[chars[n - right] - 'a'] + leftCount[chars[n - right] - 'a'] > k) {
+                rightCount[chars[n - right] - 'a']--;
+                right--;
+            }
+            result = Math.min(result, left + right);
+            leftCount[chars[left] - 'a']++;
+            left++;
+        }
+        return result;
     }
+
+    public static class WithsingleWindow {
+        public int takeCharacters(String s, int k) {
+            if (k == 0) return 0;
+            int n = s.length();
+            char[] chars = s.toCharArray();
+            int[] window = new int[3];
+            int left = 0;
+            int right = n;
+            while (window[0] < k || window[1] < k || window[2] < k) {
+                right--;
+                if (right < 0) return -1;
+                window[chars[right] - 'a']++;
+            }
+            int result = Integer.MAX_VALUE;
+            while (left < n && right < n) {
+                while (right < n && window[chars[right] - 'a'] > k) {
+                    window[chars[right] - 'a']--;
+                    right++;
+                }
+                result = Math.min(result, left + (n - right));
+                window[chars[left] - 'a']++;
+                left++;
+            }
+            return result;
+        }
+    }
+
+    public static class NotEfficient {
+        public int takeCharacters(String s, int k) {
+            if (k == 0) return 0;
+            int n = s.length();
+            int[][] leftCount = new int[s.length() + 1][3];
+            int[][] rightCount = new int[s.length() + 1][3];
+            int[] leftCurrent = new int[]{0, 0, 0};
+            int[] rightCurrent = new int[]{0, 0, 0};
+            char[] chars = s.toCharArray();
+            for (int i = 0; i < n + 1; i++) {
+                leftCount[i] = Arrays.copyOf(leftCurrent, leftCurrent.length);
+                rightCount[i] = Arrays.copyOf(rightCurrent, leftCurrent.length);
+                if (i == n) break;
+                leftCurrent[chars[i] - 'a']++;
+                rightCurrent[chars[n - 1 - i] - 'a']++;
+            }
+
+            if (Arrays.stream(leftCount[n]).anyMatch(count -> count < k)) {
+                return -1;
+            }
+
+            int result = Integer.MAX_VALUE;
+            int right = -1;
+            for (int i = k; i < rightCount.length; i++) {
+                if (rightCount[i][0] >= k && rightCount[i][1] >= k && rightCount[i][2] >= k) {
+                    right = i;
+                    break;
+                }
+            }
+            for (int i = 0; i < leftCount.length; i++) {
+
+                while (right >= 0 && leftCount[i][0] + rightCount[right][0] >= k && leftCount[i][1] + rightCount[right][1] >= k && leftCount[i][2] + rightCount[right][2] >= k) {
+                    right--;
+                }
+                result = Math.min(result, i + right + 1);
+            }
+            return result;
+        }
+    }
+
 }
--- src/test/java/org/shvetsov/_2516Test.java
+++ src/test/java/org/shvetsov/_2516Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _2516Test {
 
     private static _2516 task;
+    private static _2516.NotEfficient taskNotEfficient;
 
     @BeforeEach
     public void setup() {
         task = new _2516();
+        taskNotEfficient = new _2516.NotEfficient();
     }
 
     @Test
@@ -22,5 +24,35 @@ class _2516Test {
     @Test
     void takeCharactersExample2() {
         assertThat(task.takeCharacters("a", 1)).isEqualTo(-1);
+    }
+
+    @Test
+    void takeCharactersTC95() {
+        assertThat(task.takeCharacters("abc", 1)).isEqualTo(3);
+    }
+
+    @Test
+    void takeCharactersTC139() {
+        assertThat(task.takeCharacters("aabbccca", 2)).isEqualTo(6);
+    }
+
+    @Test
+    void takeCharactersNotEfficientExample1() {
+        assertThat(taskNotEfficient.takeCharacters("aabaaaacaabc", 2)).isEqualTo(8);
+    }
+
+    @Test
+    void takeCharactersNotEfficientExample2() {
+        assertThat(taskNotEfficient.takeCharacters("a", 1)).isEqualTo(-1);
+    }
+
+    @Test
+    void takeCharactersNotEfficientTC95() {
+        assertThat(taskNotEfficient.takeCharacters("abc", 1)).isEqualTo(3);
+    }
+
+    @Test
+    void takeCharactersNotEfficientTC139() {
+        assertThat(taskNotEfficient.takeCharacters("aabbccca", 2)).isEqualTo(6);
     }
 }
