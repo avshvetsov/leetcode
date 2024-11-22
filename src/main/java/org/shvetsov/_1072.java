54c4a489429bbbbc552f085b8a76f88722dcb10c
avshvetsov <dev.anton.shvetsov@gmail.com>
2024-11-22 14:15:34 +0200

    solve 1072

--- src/main/java/org/shvetsov/_1072.java
+++ src/main/java/org/shvetsov/_1072.java
@@ -3,7 +3,6 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
-import java.math.BigDecimal;
 import java.util.*;
 
 @LeetCode(
@@ -13,6 +12,7 @@ import java.util.*;
         tags = {"Array", "Hash Table", "Matrix"}
 )
 public class _1072 {
+
     public int maxEqualRowsAfterFlips(int[][] matrix) {
         BitSet[] bsMatrix = new BitSet[matrix.length];
         BitSet zeros = new BitSet(matrix[0].length);
@@ -30,18 +30,40 @@ public class _1072 {
         int result = 0;
         Set<Integer> counted = new HashSet<>();
         for (int i = 0; i < bsMatrix.length; i++) {
-            int count = 1;
-            counted.add(i);
-            for (int j = i + 1; j < bsMatrix.length; j++) {
-                bsMatrix[i].xor(bsMatrix[j]);
-                if (bsMatrix[i].equals(zeros) || bsMatrix[i].equals(ones)) {
-                    counted.add(j);
-                    count++;
+            if (!counted.contains(i)) {
+                int count = 1;
+                for (int j = i + 1; j < bsMatrix.length; j++) {
+                    bsMatrix[i].xor(bsMatrix[j]);
+                    if (bsMatrix[i].equals(zeros) || bsMatrix[i].equals(ones)) {
+                        counted.add(j);
+                        count++;
+                    }
+                    bsMatrix[i].xor(bsMatrix[j]);
                 }
-                bsMatrix[i].xor(bsMatrix[j]);
+                result = Math.max(result, count);
             }
-            result = Math.max(result, count);
         }
         return result;
+    }
+
+    public static class LessEffisientButMoreBeautiful {
+        public int maxEqualRowsAfterFlips(int[][] matrix) {
+            Map<BitSet, Integer> map = new HashMap<>();
+            BitSet ones = new BitSet(matrix[0].length);
+            ones.set(0, matrix[0].length);
+            for (int i = 0; i < matrix.length; i++) {
+                int[] row = matrix[i];
+                BitSet bs = new BitSet(row.length);
+                for (int j = 0; j < row.length; j++) {
+                    if (matrix[i][j] == 1) {
+                        bs.set(j);
+                    }
+                }
+                map.put(bs, map.getOrDefault(bs, 0) + 1);
+                bs.xor(ones);
+                map.put(bs, map.getOrDefault(bs, 0) + 1);
+            }
+            return map.values().stream().max(Integer::compareTo).orElse(-1);
+        }
     }
 }
