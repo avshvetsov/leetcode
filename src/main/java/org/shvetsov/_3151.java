69168f4e7810f59c2f531137136943c735087b56
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-01 10:33:41 +0200

    solve 3151

--- src/main/java/org/shvetsov/_3151.java
+++ src/main/java/org/shvetsov/_3151.java
@@ -11,7 +11,9 @@ import org.shvetsov.core.Level;
 )
 public class _3151 {
     public boolean isArraySpecial(int[] nums) {
-
-        return false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] % 2 == nums[i] % 2) return false;
+        }
+        return true;
     }
 }
