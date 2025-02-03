8553903baaf7c8711c7437a4cb0e6a23a920b372
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-03 11:00:18 +0200

    setup 3105

---
+++ src/main/java/org/shvetsov/_3105.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 3105,
+        name = "Longest Strictly Increasing or Strictly Decreasing Subarray",
+        level = Level.EASY,
+        tags = {"Array"}
+)
+public class _3105 {
+    public int longestMonotonicSubarray(int[] nums) {
+
+        return 0;
+    }
+}
---
+++ src/test/java/org/shvetsov/_3105Test.java
@@ -0,0 +1,32 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _3105Test {
+
+    private static _3105 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _3105();
+    }
+
+    @Test
+    void longestMonotonicSubarrayExample1() {
+        assertThat(task.longestMonotonicSubarray(new int[]{1, 4, 3, 3, 2})).isEqualTo(2);
+    }
+
+    @Test
+    void longestMonotonicSubarrayExample2() {
+        assertThat(task.longestMonotonicSubarray(new int[]{3, 3, 3, 3})).isEqualTo(1);
+    }
+
+    @Test
+    void longestMonotonicSubarrayExample3() {
+        assertThat(task.longestMonotonicSubarray(new int[]{3, 2, 1})).isEqualTo(3);
+    }
+
+}
