463132bf0e0179edc36ff5f3693e473012a149b0
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-04 12:18:44 +0200

    setup 1800

---
+++ src/main/java/org/shvetsov/_1800.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1800,
+        name = "Maximum Ascending Subarray Sum",
+        level = Level.EASY,
+        tags = {"Array"}
+)
+public class _1800 {
+    public int maxAscendingSum(int[] nums) {
+
+        return -1;
+    }
+}
