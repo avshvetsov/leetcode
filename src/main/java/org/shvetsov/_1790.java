77fd26ea2070372ddd8166b5a36e3ee124c68cc0
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-05 14:14:57 +0200

    setup 1790

---
+++ src/main/java/org/shvetsov/_1790.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1790,
+        name = "Check if One String Swap Can Make Strings Equal",
+        level = Level.EASY,
+        tags = {"Hash Table", "String", "Counting"}
+)
+public class _1790 {
+    public boolean areAlmostEqual(String s1, String s2) {
+
+        return false;
+    }
+}
