50c7821e038ce56edeb40d81693351d7b2806ae1
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-06 12:19:37 +0200

    solve 1726

--- src/main/java/org/shvetsov/_1726.java
+++ src/main/java/org/shvetsov/_1726.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @LeetCode(
         number = 1726,
         name = "Tuple with Same Product",
@@ -11,7 +14,15 @@ import org.shvetsov.core.Level;
 )
 public class _1726 {
     public int tupleSameProduct(int[] nums) {
-
-        return -1;
+        Map<Integer, Integer> counting = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int key = nums[i] * nums[j];
+                counting.put(key, counting.getOrDefault(key, 0) + 1);
+            }
+        }
+        return counting.values().stream()
+                .filter(count -> count > 1)
+                .reduce(0, (res, element) -> res + (element * (element - 1)) * 4);
     }
 }
