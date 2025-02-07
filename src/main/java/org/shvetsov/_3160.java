b635212f07e2f78f641b25ae9a79f9d6668563b3
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-07 12:00:17 +0200

    solve 3160

--- src/main/java/org/shvetsov/_3160.java
+++ src/main/java/org/shvetsov/_3160.java
@@ -40,4 +40,33 @@ public class _3160 {
         }
         return result;
     }
+
+
+    public int[] queryResultsOptimized(int limit, int[][] queries) {
+        Map<Integer, Integer> ballColor = new HashMap<>();
+        Map<Integer, Integer> colorBalls = new HashMap<>();
+        int[] result = new int[queries.length];
+        int colorsCount = 0;
+        for (int i = 0; i < queries.length; i++) {
+            int[] query = queries[i];
+            int ball = query[0];
+            int color = query[1];
+            Integer prevColor = ballColor.getOrDefault(ball, 0);
+            Integer ballsWithPrevColor = colorBalls.getOrDefault(prevColor, 0);
+            if (ballsWithPrevColor > 0) {
+                colorBalls.put(prevColor, ballsWithPrevColor - 1);
+                if (ballsWithPrevColor == 1) {
+                    colorsCount--;
+                }
+            }
+            Integer ballsWithCurrColor = colorBalls.getOrDefault(color, 0);
+            if (ballsWithCurrColor == 0) {
+                colorsCount++;
+            }
+            colorBalls.put(color, ballsWithCurrColor + 1);
+            ballColor.put(ball, color);
+            result[i] = colorsCount;
+        }
+        return result;
+    }
 }
--- src/test/java/org/shvetsov/_3160Test.java
+++ src/test/java/org/shvetsov/_3160Test.java
@@ -24,4 +24,14 @@ class _3160Test {
         assertThat(task.queryResults(4, new int[][]{{0, 1}, {1, 2}, {2, 2}, {3, 4}, {4, 5}})).isEqualTo(new int[]{1, 2, 2, 3, 4});
     }
 
+    @Test
+    void queryResultsOptimizedExample1() {
+        assertThat(task.queryResults(4, new int[][]{{1, 4}, {2, 5}, {1, 3}, {3, 4}})).isEqualTo(new int[]{1, 2, 2, 3});
+    }
+
+    @Test
+    void queryResultsOptimizedExample2() {
+        assertThat(task.queryResults(4, new int[][]{{0, 1}, {1, 2}, {2, 2}, {3, 4}, {4, 5}})).isEqualTo(new int[]{1, 2, 2, 3, 4});
+    }
+
 }
