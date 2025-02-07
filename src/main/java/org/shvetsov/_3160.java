e486bb5c8dbfad5658be5a14bb99bb49bfa0d4b7
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-07 10:35:41 +0200

    setup 3160

---
+++ src/main/java/org/shvetsov/_3160.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 3160,
+        name = "Find the Number of Distinct Colors Among the Balls",
+        level = Level.MEDIUM,
+        tags = {"Array", "Hash Table", "Simulation"}
+)
+public class _3160 {
+    public int[] queryResults(int limit, int[][] queries) {
+
+        return null;
+    }
+}
