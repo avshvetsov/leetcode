f8d8cf7ed02c7b1c403ee1ef50b4e16be0a088d4
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-08 11:09:29 +0200

    setup 2349

---
+++ src/main/java/org/shvetsov/_2349.java
@@ -0,0 +1,29 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2349,
+        name = "Design a Number Container System",
+        level = Level.MEDIUM,
+        tags = {"Hash Table", "Design", "Heap (Priority Queue)", "Ordered Set"}
+)
+public class _2349 {
+
+    static class NumberContainers {
+
+        public NumberContainers() {
+
+        }
+
+        public void change(int index, int number) {
+
+        }
+
+        public int find(int number) {
+
+            return 0;
+        }
+    }
+}
