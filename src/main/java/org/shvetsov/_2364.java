7e0bc2556e6da9c094a5193d5e695dda155dca6b
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-09 11:01:40 +0200

    solve 2364

--- src/main/java/org/shvetsov/_2364.java
+++ src/main/java/org/shvetsov/_2364.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @LeetCode(
         number = 2364,
         name = "Count Number of Bad Pairs",
@@ -11,7 +14,17 @@ import org.shvetsov.core.Level;
 )
 public class _2364 {
     public long countBadPairs(int[] nums) {
-
-        return 0;
+        Map<Integer, Integer> subtractCount = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int subtract = nums[i] - i;
+            subtractCount.merge(subtract, 1, Integer::sum);
+        }
+        long result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int subtract = nums[i] - i;
+            result += nums.length - i - 1 - subtractCount.get(subtract) + 1;
+            subtractCount.merge(subtract, 1, (old, neww) -> old - neww);
+        }
+        return result;
     }
 }
