f1120c663284097c64187d1fd8ba81cecec448b3
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-12 14:20:32 +0200

    setup 2342

---
+++ src/main/java/org/shvetsov/_2342.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2342,
+        name = "Max Sum of a Pair With Equal Sum of Digits",
+        level = Level.MEDIUM,
+        tags = {"Array", "Hash Table", "Sorting", "Heap (Priority Queue)"}
+)
+public class _2342 {
+    public int maximumSum(int[] nums) {
+
+        return -1;
+    }
+}
