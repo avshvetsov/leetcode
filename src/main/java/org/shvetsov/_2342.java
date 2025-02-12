a65e4ff1ff83095397a939e4112219f3d9059b02
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-12 14:38:52 +0200

    solve 2342

--- src/main/java/org/shvetsov/_2342.java
+++ src/main/java/org/shvetsov/_2342.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.*;
+
 @LeetCode(
         number = 2342,
         name = "Max Sum of a Pair With Equal Sum of Digits",
@@ -11,7 +13,18 @@ import org.shvetsov.core.Level;
 )
 public class _2342 {
     public int maximumSum(int[] nums) {
-
-        return -1;
+        Map<Integer, PriorityQueue<Integer>> digitSumMap = new HashMap<>();
+        for (int num : nums) {
+            int sum = 0;
+            for (char c : String.valueOf(num).toCharArray()) {
+                sum += c - '0';
+            }
+            digitSumMap.computeIfAbsent(sum, k -> new PriorityQueue<>(Comparator.reverseOrder())).add(num);
+        }
+        return digitSumMap.values().stream()
+                .filter(set -> set.size() > 1)
+                .mapToInt(set -> set.poll() + set.poll())
+                .max()
+                .orElse(-1);
     }
 }
