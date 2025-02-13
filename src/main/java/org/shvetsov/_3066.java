6709fdf65d0fadb7f6c139fe9e7db6c7e375ce80
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-13 10:03:02 +0200

    setup 3066

---
+++ src/main/java/org/shvetsov/_3066.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 3066,
+        name = "Minimum Operations to Exceed Threshold Value II",
+        level = Level.MEDIUM,
+        tags = {"Array", "Heap (Priority Queue)", "Simulation"}
+)
+public class _3066 {
+    public int minOperations(int[] nums, int k) {
+
+        return -1;
+    }
+}
