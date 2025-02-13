3080c704daa409e4f19aa97f02d61e4365fe46cd
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-13 10:20:48 +0200

    solve 3066

--- src/main/java/org/shvetsov/_3066.java
+++ src/main/java/org/shvetsov/_3066.java
@@ -3,6 +3,10 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.PriorityQueue;
+
 @LeetCode(
         number = 3066,
         name = "Minimum Operations to Exceed Threshold Value II",
@@ -11,7 +15,18 @@ import org.shvetsov.core.Level;
 )
 public class _3066 {
     public int minOperations(int[] nums, int k) {
-
-        return -1;
+        PriorityQueue<Long> pq = new PriorityQueue<>(
+                Arrays.stream(nums)
+                        .mapToObj(Long::valueOf)
+                        .toList()
+        );
+        int count = 0;
+        while (pq.size() >= 2 && pq.peek() < k) {
+            Long x = pq.poll();
+            Long y = pq.poll();
+            pq.add(x * 2 + y);
+            count++;
+        }
+        return count;
     }
 }
