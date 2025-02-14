fc8604fee42b9f05172f1510a1911e6a77306240
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-14 13:42:25 +0200

    solve 1352

--- src/main/java/org/shvetsov/_1352.java
+++ src/main/java/org/shvetsov/_1352.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @LeetCode(
         number = 1352,
         name = "Product of the Last K Numbers",
@@ -13,17 +16,68 @@ public class _1352 {
 
     static class ProductOfNumbers {
 
-        public ProductOfNumbers() {
+        List<Integer> prefixProduct;
+        int index;
 
+        public ProductOfNumbers() {
+            initPrefixProduct();
+        }
+
+        private void initPrefixProduct() {
+            prefixProduct = new ArrayList<>();
+            prefixProduct.add(1);
+            index = 0;
         }
 
         public void add(int num) {
-
+            if (num == 0) {
+                initPrefixProduct();
+            } else {
+                index++;
+                prefixProduct.add(prefixProduct.get(index - 1) * num);
+            }
         }
 
         public int getProduct(int k) {
+            if (index >= k) {
+                return prefixProduct.get(index) / prefixProduct.get(index - k);
+            } else {
+                return 0;
+            }
+        }
+    }
 
-            return -1;
+
+    static class ProductOfNumbersArray {
+
+        int[] prefixProduct;
+        int index;
+
+        public ProductOfNumbersArray() {
+            initPrefixProduct();
+        }
+
+        private void initPrefixProduct() {
+            prefixProduct = new int[40001];
+            prefixProduct[0] = 1;
+            index = 0;
+        }
+
+        public void add(int num) {
+            if (num == 0) {
+                initPrefixProduct();
+            } else {
+                index++;
+                prefixProduct[index] = prefixProduct[index - 1] * num;
+            }
+        }
+
+        public int getProduct(int k) {
+            if (index >= k) {
+                return prefixProduct[index] / prefixProduct[index - k];
+            } else {
+                return 0;
+            }
         }
     }
 
--- src/test/java/org/shvetsov/_1352Test.java
+++ src/test/java/org/shvetsov/_1352Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _1352Test {
 
     private static _1352.ProductOfNumbers task;
+    private static _1352.ProductOfNumbersArray taskArray;
 
     @BeforeEach
     public void setup() {
         task = new _1352.ProductOfNumbers();
+        taskArray = new _1352.ProductOfNumbersArray();
     }
 
     @Test
@@ -24,8 +26,22 @@ class _1352Test {
         assertThat(task.getProduct(2)).isEqualTo(20);
         assertThat(task.getProduct(3)).isEqualTo(40);
         assertThat(task.getProduct(4)).isEqualTo(0);
-        task.add(4);
+        task.add(8);
         assertThat(task.getProduct(2)).isEqualTo(32);
+    }
+
+    @Test
+    void productOfNumbersArrayExample1() {
+        taskArray.add(3);
+        taskArray.add(0);
+        taskArray.add(2);
+        taskArray.add(5);
+        taskArray.add(4);
+        assertThat(taskArray.getProduct(2)).isEqualTo(20);
+        assertThat(taskArray.getProduct(3)).isEqualTo(40);
+        assertThat(taskArray.getProduct(4)).isEqualTo(0);
+        taskArray.add(8);
+        assertThat(taskArray.getProduct(2)).isEqualTo(32);
     }
 
 }
