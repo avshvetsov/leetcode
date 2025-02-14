cef368df6fd9b80939de0f58c3a8c30af5dec1a2
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-14 10:15:54 +0200

    setup 1352

---
+++ src/main/java/org/shvetsov/_1352.java
@@ -0,0 +1,30 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1352,
+        name = "Product of the Last K Numbers",
+        level = Level.MEDIUM,
+        tags = {"Array", "Math", "Design", "Data Stream", "Prefix Sum"}
+)
+public class _1352 {
+
+    static class ProductOfNumbers {
+
+        public ProductOfNumbers() {
+
+        }
+
+        public void add(int num) {
+
+        }
+
+        public int getProduct(int k) {
+
+            return -1;
+        }
+    }
+
+}
---
+++ src/test/java/org/shvetsov/_1352Test.java
@@ -0,0 +1,31 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _1352Test {
+
+    private static _1352.ProductOfNumbers task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _1352.ProductOfNumbers();
+    }
+
+    @Test
+    void productOfNumbersExample1() {
+        task.add(3);
+        task.add(0);
+        task.add(2);
+        task.add(5);
+        task.add(4);
+        assertThat(task.getProduct(2)).isEqualTo(20);
+        assertThat(task.getProduct(3)).isEqualTo(40);
+        assertThat(task.getProduct(4)).isEqualTo(0);
+        task.add(4);
+        assertThat(task.getProduct(2)).isEqualTo(32);
+    }
+
+}
