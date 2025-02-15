f2acde7877556aab87c72dc3f1ef6f6381648836
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-15 11:19:02 +0200

    solve 2698

--- src/main/java/org/shvetsov/_2698.java
+++ src/main/java/org/shvetsov/_2698.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @LeetCode(
         number = 2698,
         name = "Find the Punishment Number of an Integer",
@@ -10,8 +13,41 @@ import org.shvetsov.core.Level;
         tags = {"Math", "Backtracking"}
 )
 public class _2698 {
-    public int punishmentNumber(int n) {
 
-        return -1;
+    static Map<Integer, Boolean> cache = new HashMap<>();
+
+    public int punishmentNumber(int n) {
+        int res = 0;
+        while (n > 0) {
+            int square = n * n;
+            if (cache.containsKey(n)) {
+                if (cache.get(n)) {
+                    res += square;
+                }
+            } else {
+                boolean punishment = isPunishment(String.valueOf(square), 0, n);
+                cache.put(n, punishment);
+                if (punishment) {
+                    res += square;
+                }
+            }
+            n--;
+        }
+        return res;
+    }
+
+    private boolean isPunishment(String s, int acc, int target) {
+        if (s.isEmpty() && acc == target) {
+            return true;
+        }
+        if (acc > target) {
+            return false;
+        }
+        for (int i = 1; i <= s.length(); i++) {
+            if (isPunishment(s.substring(i), acc + Integer.parseInt(s.substring(0, i)), target)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
