0621560c1cf2ebe42e762e9ab6d3371298b4693e
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-16 13:02:31 +0200

    solve 1718

---
+++ src/main/java/org/shvetsov/_1718.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1718,
+        name = "Construct the Lexicographically Largest Valid Sequence",
+        level = Level.MEDIUM,
+        tags = {"Array", "Backtracking"}
+)
+public class _1718 {
+    public int[] constructDistancedSequence(int n) {
+
+        return null;
+    }
+}
