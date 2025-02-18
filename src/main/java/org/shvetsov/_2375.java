3dd9f8b47bd7f5b3b8ef472dd7a4c4478bf1cc78
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-18 12:36:18 +0200

    solve 2375

--- src/main/java/org/shvetsov/_2375.java
+++ src/main/java/org/shvetsov/_2375.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 @LeetCode(
         number = 2375,
         name = "Construct Smallest Number From DI String",
@@ -11,7 +14,93 @@ import org.shvetsov.core.Level;
 )
 public class _2375 {
     public String smallestNumber(String pattern) {
+        boolean[] used = new boolean[11];
+        Stack<Integer> stack = null;
+        for (int i = 1; i < 10; i++) {
+            stack = new Stack<>();
+            used[i] = true;
+            stack.push(i);
+            if (getSmallestNumber(pattern, 0, used, stack)) break;
+            stack.pop();
+            used[i] = false;
+        }
+        return stack.stream().map(String::valueOf).collect(Collectors.joining());
+    }
 
-        return null;
+    private boolean getSmallestNumber(String pattern, int index, boolean[] used, Stack<Integer> stack) {
+        if (index == pattern.length()) return true;
+        char c = pattern.charAt(index);
+        for (int i = c == 'I' ? stack.peek() + 1 : 1; i < (c == 'I' ? 10 : stack.peek()); i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            stack.push(i);
+            if (getSmallestNumber(pattern, index + 1, used, stack)) {
+                return true;
+            }
+            stack.pop();
+            used[i] = false;
+        }
+        return false;
+    }
+
+
+    static class StringSolution {
+        public String smallestNumber(String pattern) {
+            boolean[] used = new boolean[11];
+            for (int i = 1; i < 10; i++) {
+                used[i] = true;
+                String smallestNumber = getSmallestNumber(pattern, 0, used, i);
+                if (smallestNumber != null) return i + smallestNumber;
+                used[i] = false;
+            }
+            throw new IllegalArgumentException();
+        }
+
+        private String getSmallestNumber(String pattern, int index, boolean[] used, int prev) {
+            if (index == pattern.length()) return "";
+            char c = pattern.charAt(index);
+            for (int i = c == 'I' ? prev + 1 : 1; i < (c == 'I' ? 10 : prev); i++) {
+                if (used[i]) continue;
+                used[i] = true;
+                String smallestNumber = getSmallestNumber(pattern, index + 1, used, i);
+                if (smallestNumber != null) return i + smallestNumber;
+                used[i] = false;
+            }
+            return null;
+        }
+    }
+
+    static class StackSolutionBottomUp {
+        public String smallestNumber(String pattern) {
+            boolean[] used = new boolean[11];
+            for (int i = 1; i < 10; i++) {
+                used[i] = true;
+                Stack<Integer> smallestNumber = getSmallestNumber(pattern, 0, used, i);
+                if (smallestNumber != null) {
+                    smallestNumber.push(i);
+                    return new StringBuilder(smallestNumber.stream()
+                            .map(String::valueOf)
+                            .collect(Collectors.joining())).reverse().toString();
+                }
+                used[i] = false;
+            }
+            throw new IllegalArgumentException();
+        }
+
+        private Stack<Integer> getSmallestNumber(String pattern, int index, boolean[] used, int prev) {
+            if (index == pattern.length()) return new Stack<>();
+            char c = pattern.charAt(index);
+            for (int i = c == 'I' ? prev + 1 : 1; i < (c == 'I' ? 10 : prev); i++) {
+                if (used[i]) continue;
+                used[i] = true;
+                Stack<Integer> smallestNumber = getSmallestNumber(pattern, index + 1, used, i);
+                if (smallestNumber != null) {
+                    smallestNumber.push(i);
+                    return smallestNumber;
+                }
+                used[i] = false;
+            }
+            return null;
+        }
     }
 }
--- src/test/java/org/shvetsov/_2375Test.java
+++ src/test/java/org/shvetsov/_2375Test.java
@@ -8,10 +8,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _2375Test {
 
     private static _2375 task;
+    private static _2375.StackSolutionBottomUp taskStackSolutionBottomUp;
+    private static _2375.StringSolution taskStringSolution;
 
     @BeforeEach
     public void setup() {
         task = new _2375();
+        taskStackSolutionBottomUp = new _2375.StackSolutionBottomUp();
+        taskStringSolution = new _2375.StringSolution();
     }
 
     @Test
@@ -22,6 +26,24 @@ class _2375Test {
     @Test
     void smallestNumberExample2() {
         assertThat(task.smallestNumber("DDD")).isEqualTo("4321");
+    }
+    @Test
+    void smallestNumberStackSolutionBottomUpExample1() {
+        assertThat(taskStackSolutionBottomUp.smallestNumber("IIIDIDDD")).isEqualTo("123549876");
+    }
+
+    @Test
+    void smallestNumberStackSolutionBottomUpExample2() {
+        assertThat(taskStackSolutionBottomUp.smallestNumber("DDD")).isEqualTo("4321");
+    }
+    @Test
+    void smallestNumberStringSolutionExample1() {
+        assertThat(taskStringSolution.smallestNumber("IIIDIDDD")).isEqualTo("123549876");
+    }
+
+    @Test
+    void smallestNumberStringSolutionExample2() {
+        assertThat(taskStringSolution.smallestNumber("DDD")).isEqualTo("4321");
     }
 
 }
