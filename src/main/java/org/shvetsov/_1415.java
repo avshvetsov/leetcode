136e6b9d4a555c7f76c6033506056e235d67ac70
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-19 10:01:14 +0200

    setup 1415

---
+++ src/main/java/org/shvetsov/_1415.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1415,
+        name = "The k-th Lexicographical String of All Happy Strings of Length n",
+        level = Level.MEDIUM,
+        tags = {"String", "Backtracking"}
+)
+public class _1415 {
+    public String getHappyString(int n, int k) {
+
+        return null;
+    }
+}
