7f310ec658c01275c9f1aff375ff16028b57ac75
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-20 11:46:30 +0200

    setup 1980

---
+++ src/main/java/org/shvetsov/_1980.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1980,
+        name = "Find Unique Binary String",
+        level = Level.MEDIUM,
+        tags = {"Array", "Hash Table", "String", "Backtracking"}
+)
+public class _1980 {
+    public String findDifferentBinaryString(String[] nums) {
+
+        return null;
+    }
+}
