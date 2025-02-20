341cb780b8a1bf44e222c4691e5d4672e7b29095
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-20 12:21:26 +0200

    solve 1980

--- src/main/java/org/shvetsov/_1980.java
+++ src/main/java/org/shvetsov/_1980.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @LeetCode(
         number = 1980,
         name = "Find Unique Binary String",
@@ -11,7 +14,26 @@ import org.shvetsov.core.Level;
 )
 public class _1980 {
     public String findDifferentBinaryString(String[] nums) {
+        Set<Integer> ints = new HashSet<>();
+        for (String num : nums) {
+            ints.add(Integer.parseInt(num, 2));
+        }
+        for (int i = 0; i < 17; i++) {
+            if (!ints.contains(i)) {
+                String res = "0000000000000000" + Integer.toBinaryString(i);
+                return res.substring(res.length() - nums.length);
+            }
+        }
+        throw new IllegalArgumentException();
+    }
 
-        return null;
+    static class CantorsDiagonalArgument {
+        public String findDifferentBinaryString(String[] nums) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < nums.length; i++) {
+                sb.append(nums[i].charAt(i) == '1' ? '0' : '1');
+            }
+            return sb.toString();
+        }
     }
 }
--- src/test/java/org/shvetsov/_1980Test.java
+++ src/test/java/org/shvetsov/_1980Test.java
@@ -8,10 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class _1980Test {
 
     private static _1980 task;
+    private static _1980.CantorsDiagonalArgument taskCantorsDiagonalArgument;
 
     @BeforeEach
     public void setup() {
         task = new _1980();
+        taskCantorsDiagonalArgument = new _1980.CantorsDiagonalArgument();
     }
 
     @Test
@@ -25,6 +27,23 @@ class _1980Test {
     @Test
     void findDifferentBinaryStringExample3() {
         assertThat(task.findDifferentBinaryString(new String[]{"111","011","001"})).isIn("101", "000", "010", "100", "110");
+    }
+    @Test
+    void findDifferentBinaryStringTC88() {
+        assertThat(task.findDifferentBinaryString(new String[]{"11010011101","10110010101","01011001111","01100011001","00110110110","10110011011","11110000010","01110000000","00110011100","11111011100","11111110110"})).isIn("00000000000");
+    }
+
+    @Test
+    void findDifferentBinaryStringCantorsDiagonalArgumentExample1() {
+        assertThat(taskCantorsDiagonalArgument.findDifferentBinaryString(new String[]{"01","10"})).isIn("11", "00");
+    }
+    @Test
+    void findDifferentBinaryStringCantorsDiagonalArgumentExample2() {
+        assertThat(taskCantorsDiagonalArgument.findDifferentBinaryString(new String[]{"00","01"})).isIn("11", "10");
+    }
+    @Test
+    void findDifferentBinaryStringCantorsDiagonalArgumentExample3() {
+        assertThat(taskCantorsDiagonalArgument.findDifferentBinaryString(new String[]{"111","011","001"})).isIn("101", "000", "010", "100", "110");
     }
 
 }
