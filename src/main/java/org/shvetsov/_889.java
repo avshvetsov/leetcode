224960b1a41280ffe05dfbd492cf520de3650e43
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-23 13:56:31 +0200

    solve 889

--- src/main/java/org/shvetsov/_889.java
+++ src/main/java/org/shvetsov/_889.java
@@ -4,6 +4,8 @@ import org.shvetsov.classes.TreeNode;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 889,
         name = "Construct Binary Tree from Preorder and Postorder Traversal",
@@ -12,7 +14,28 @@ import org.shvetsov.core.Level;
 )
 public class _889 {
     public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        TreeNode root = new TreeNode();
+        if (preorder[0] == postorder[postorder.length - 1]) {
+            root.val = preorder[0];
+        } else throw new IllegalArgumentException("Invalid preorder/postorder");
 
-        return null;
+        if (preorder.length > 1 && postorder.length > 1){
+            if (preorder[1] != postorder[postorder.length - 2]) {
+                int i = getIndexOf(preorder, postorder[postorder.length - 2]);
+                int j = getIndexOf(postorder, preorder[1]);
+                root.left = constructFromPrePost(Arrays.copyOfRange(preorder, 1, i), Arrays.copyOfRange(postorder, 0, j + 1));
+                root.right = constructFromPrePost(Arrays.copyOfRange(preorder, i, preorder.length), Arrays.copyOfRange(postorder, j + 1, postorder.length - 1));
+            } else {
+                root.left = constructFromPrePost(Arrays.copyOfRange(preorder, 1, preorder.length), Arrays.copyOfRange(postorder, 0, postorder.length - 1));
+            }
+        }
+        return root;
+    }
+
+    private int getIndexOf(int[] array, int val) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == val) return i;
+        }
+        throw new IllegalArgumentException("Invalid not found");
     }
 }
--- src/test/java/org/shvetsov/_889Test.java
+++ src/test/java/org/shvetsov/_889Test.java
@@ -25,4 +25,19 @@ class _889Test {
         assertThat(task.constructFromPrePost(new int[]{1}, new int[]{1})).isEqualTo(TreeUtils.constructTreeNode(1));
     }
 
+    @Test
+    void constructFromPrePostTC2() {
+        assertThat(task.constructFromPrePost(new int[]{2, 1}, new int[]{1, 2})).isEqualTo(TreeUtils.constructTreeNode(2, 1, null));
+    }
+
+    @Test
+    void constructFromPrePostTC4() {
+        assertThat(task.constructFromPrePost(new int[]{2, 1, 3}, new int[]{3, 1, 2})).isEqualTo(TreeUtils.constructTreeNode(2, 1, null, 3, null));
+    }
+
+    @Test
+    void constructFromPrePostTC9() {
+        assertThat(task.constructFromPrePost(new int[]{4, 2, 1, 3}, new int[]{3, 1, 2, 4})).isEqualTo(TreeUtils.constructTreeNode(4, 2, null, 1, null, 3, null));
+    }
+
 }
