90f9b557fda232bc6f21eaf726fa3dcf6ae8dc0a
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-23 12:43:03 +0200

    setup 889

---
+++ src/main/java/org/shvetsov/_889.java
@@ -0,0 +1,18 @@
+package org.shvetsov;
+
+import org.shvetsov.classes.TreeNode;
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 889,
+        name = "Construct Binary Tree from Preorder and Postorder Traversal",
+        level = Level.MEDIUM,
+        tags = {"Array", "Hash Table", "Divide and Conquer", "Tree", "Binary Tree"}
+)
+public class _889 {
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+
+        return null;
+    }
+}
