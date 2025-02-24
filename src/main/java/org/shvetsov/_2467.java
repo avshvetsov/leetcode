d70a8824f89309ca33532dd32b864815e608e8e1
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-24 09:57:48 +0200

    setup 2467

---
+++ src/main/java/org/shvetsov/_2467.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2467,
+        name = "Most Profitable Path in a Tree",
+        level = Level.MEDIUM,
+        tags = {"Array", "Tree", "Depth-First Search", "Breadth-First Search", "Graph"}
+)
+public class _2467 {
+    public int mostProfitablePath(int[][] edges, int bob, int[] amount) {
+
+        return -1;
+    }
+}
