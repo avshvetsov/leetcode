2368906d18cf136488d1406ee6d5adb3f02513c6
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-26 16:07:05 +0200

    solve 1749

--- src/main/java/org/shvetsov/_1749.java
+++ src/main/java/org/shvetsov/_1749.java
@@ -7,11 +7,28 @@ import org.shvetsov.core.Level;
         number = 1749,
         name = "Maximum Absolute Sum of Any Subarray",
         level = Level.MEDIUM,
-        tags = {"Array", "Dynamic Programming"}
+        tags = {"Array", "Dynamic Programming", "Kadane’s Algorithm"}
 )
 public class _1749 {
     public int maxAbsoluteSum(int[] nums) {
+        //Kadane’s Algorithm
+        int max = nums[0];
+        int maxEndingPrev = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            int maxEnding = Math.max(maxEndingPrev + num, num);
+            max = Math.max(max, maxEnding);
+            maxEndingPrev = maxEnding;
+        }
 
-        return 0;
+        int min = nums[0];
+        int minEndingPrev = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            int minEnding = Math.min(minEndingPrev + num, num);
+            min = Math.min(min, minEnding);
+            minEndingPrev = minEnding;
+        }
+        return Math.max(Math.abs(min), Math.abs(max));
     }
 }
