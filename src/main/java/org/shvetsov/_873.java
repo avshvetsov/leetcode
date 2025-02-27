deb1808e0dbdf64423ae767385aeb52618b59bdf
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-27 11:12:49 +0200

    solve 873

--- src/main/java/org/shvetsov/_873.java
+++ src/main/java/org/shvetsov/_873.java
@@ -3,6 +3,12 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @LeetCode(
         number = 873,
         name = "Length of Longest Fibonacci Subsequence",
@@ -11,7 +17,23 @@ import org.shvetsov.core.Level;
 )
 public class _873 {
     public int lenLongestFibSubseq(int[] arr) {
-
-        return 0;
+        int result = 0;
+        Set<Integer> set = Arrays.stream(arr).boxed().collect(Collectors.toSet());
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int prev = arr[i];
+                int curr = arr[j];
+                int next = curr + prev;
+                int length = 2;
+                while (set.contains(next)) {
+                    prev = curr;
+                    curr = next;
+                    next = curr + prev;
+                    length++;
+                }
+                result = Math.max(result, length);
+            }
+        }
+        return result > 2 ? result : 0;
     }
 }
