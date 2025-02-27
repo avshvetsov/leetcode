f1a8fd8af74b1d581d053e7020cc6c324db5b675
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-27 10:50:39 +0200

    setup 873

---
+++ src/main/java/org/shvetsov/_873.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 873,
+        name = "Length of Longest Fibonacci Subsequence",
+        level = Level.MEDIUM,
+        tags = {"Array", "Hash Table", "Dynamic Programming"}
+)
+public class _873 {
+    public int lenLongestFibSubseq(int[] arr) {
+
+        return 0;
+    }
+}
