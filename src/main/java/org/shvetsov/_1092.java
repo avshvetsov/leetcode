14ea101b557b90abd0981bf9b880d9c6efdb9877
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-02-28 13:32:32 +0200

    setup 1092

---
+++ src/main/java/org/shvetsov/_1092.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1092,
+        name = "Shortest Common Supersequence ",
+        level = Level.HARD,
+        tags = {"String", "Dynamic Programming"}
+)
+public class _1092 {
+    public String shortestCommonSupersequence(String str1, String str2) {
+
+        return "";
+    }
+}
