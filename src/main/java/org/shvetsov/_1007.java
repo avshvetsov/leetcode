37d9fd7275c8514616617bc31af5fffb9e5c36a6
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-05-03 12:03:02 +0300

    solve 1007

--- src/main/java/org/shvetsov/_1007.java
+++ src/main/java/org/shvetsov/_1007.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @LeetCode(
         number = 1007,
         name = "Minimum Domino Rotations For Equal Row",
@@ -11,7 +14,20 @@ import org.shvetsov.core.Level;
 )
 public class _1007 {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-
-        return -2;
+        Map<Integer, int[]> options = new HashMap<>();
+        for (int i = 0; i < tops.length; i++) {
+            if (tops[i] == bottoms[i]) {
+                options.computeIfAbsent(tops[i], integer -> new int[3])[2]++;
+            } else {
+                options.computeIfAbsent(tops[i], integer -> new int[3])[0]++;
+                options.computeIfAbsent(bottoms[i], integer -> new int[3])[1]++;
+            }
+        }
+        for (Map.Entry<Integer, int[]> entry : options.entrySet()) {
+            if (entry.getValue()[0] + entry.getValue()[1] + entry.getValue()[2] >= tops.length) {
+                return Math.min(entry.getValue()[0], entry.getValue()[1]);
+            }
+        }
+        return -1;
     }
 }
