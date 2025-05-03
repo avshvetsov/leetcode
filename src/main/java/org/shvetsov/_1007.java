747c328170b6c2f658f8d35cd11b748a4fe75074
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-05-03 11:46:52 +0300

    setup 1007

---
+++ src/main/java/org/shvetsov/_1007.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1007,
+        name = "Minimum Domino Rotations For Equal Row",
+        level = Level.MEDIUM,
+        tags = {"Array", "Greedy"}
+)
+public class _1007 {
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+
+        return -2;
+    }
+}
