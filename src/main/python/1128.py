7fe3f60327e6c05777d421fd6c102b2aaa3e705c
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-05-04 14:14:02 +0300

    solve 1128

--- src/main/python/1128.py
+++ src/main/python/1128.py
@@ -1,22 +1,39 @@
 import unittest
 from parameterized import parameterized
 
+
 class Solution(object):
     def numEquivDominoPairs(self, dominoes):
         """
         :type dominoes: List[List[int]]
         :rtype: int
         """
-        print(dominoes)
-        return 0
+        res = 0
+        map = {}
+        for a, b in dominoes:
+            if b < a:
+                t = a
+                a = b
+                b = t
+            if a in map:
+                if b in map[a]:
+                    res = res + map[a][b]
+                    map[a][b] = map[a][b] + 1
+                    continue
+                else:
+                    map[a][b] = 0
+            else:
+                map[a] = {b : 0}
 
+            map[a][b] = map[a][b] + 1
+        return res
 
 class SolutionTest(unittest.TestCase):
-    @parameterized.expand([
-        [[[1,2],[2,1],[3,4],[5,6]], 1],
-        [[[1,2],[1,2],[1,1],[1,2],[2,2]], 3],
-    ])
-    def test(self, n, exp):
-        s = Solution()
-        act_n = s.numEquivDominoPairs(n)
-        self.assertEqual(exp, act_n)
+        @parameterized.expand([
+            [[[1, 2], [2, 1], [3, 4], [5, 6]], 1],
+            [[[1, 2], [1, 2], [1, 1], [1, 2], [2, 2]], 3],
+        ])
+        def test(self, n, exp):
+            s = Solution()
+            act_n = s.numEquivDominoPairs(n)
+            self.assertEqual(exp, act_n)
