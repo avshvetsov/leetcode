6dd40787ac0536d5d8852732fffe28d5470e2a56
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-05-11 13:00:30 +0300

    solve 1550

--- src/main/python/1550.py
+++ src/main/python/1550.py
@@ -1,5 +1,7 @@
 import unittest
+
 from parameterized import parameterized
+
 
 class Solution(object):
     def threeConsecutiveOdds(self, arr):
@@ -7,13 +9,18 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-
+        countrer = 0
+        for e in arr:
+            countrer = countrer + 1 if e % 2 == 1 else 0
+            if countrer == 3:
+                return True
+        return False
 
 
 class SolutionTest(unittest.TestCase):
     @parameterized.expand([
-        [[2,6,4,1], False],
-        [[1,2,34,3,4,5,7,23,12], True],
+        [[2, 6, 4, 1], False],
+        [[1, 2, 34, 3, 4, 5, 7, 23, 12], True],
     ])
     def test(self, input, exp):
         s = Solution()
