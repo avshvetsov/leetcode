a5c60855fb3dbe42add8a90a410df5e35bab2001
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-06-09 15:48:21 +0300

    setup 3403

---
+++ src/test/java/org/shvetsov/_3403Test.java
@@ -0,0 +1,26 @@
+package org.shvetsov;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _3403Test {
+
+    private static _3403 task;
+
+    @BeforeEach
+    public void setup() {
+        task = new _3403();
+    }
+
+    @Test
+    void answerStringExample1() {
+        assertThat(task.answerString("dbca", 2)).isEqualTo("dbc");
+    }
+
+    @Test
+    void answerStringExample2() {
+        assertThat(task.answerString("gggg", 4)).isEqualTo("g");
+    }
+}
