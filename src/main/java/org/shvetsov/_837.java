e96f00ea82decd4800f71506b120b2072300904d
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-08-18 11:31:05 +0300

    solve 837

--- src/main/java/org/shvetsov/_837.java
+++ src/main/java/org/shvetsov/_837.java
@@ -11,7 +11,41 @@ import org.shvetsov.core.Level;
 )
 public class _837 {
     public double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n - maxPts >= k) return 1;
+        double[] probability = new double[n + 1];
+        probability[0] = 1;
+        double windowSum = 1;
+        for (int i = 1; i <= n; i++) {
+            probability[i] = windowSum / maxPts;
+            if (i < k) {
+                windowSum += probability[i];
+            }
+            if (i - maxPts >= 0) {
+                windowSum -= probability[i - maxPts];
+            }
+        }
+        double result = 0;
+        for (int i = k; i <= n; i++) {
+            result += probability[i];
+        }
+        return result;
+    }
 
-        return 0.0;
+
+    public double new21GameSimple(int n, int k, int maxPts) {
+        if (n == 0 && n == k) return 1;
+        double[] probability = new double[20001];
+        probability[0] = 1;
+        for (int i = 0; i < k; i++) {
+            double nextP = probability[i] / maxPts;
+            for (int j = i + 1; j <= i + maxPts; j++) {
+                probability[j] += nextP;
+            }
+        }
+        double result = 0;
+        for (int i = k; i <= n; i++) {
+            result += probability[i];
+        }
+        return result;
     }
 }
--- src/test/java/org/shvetsov/_837Test.java
+++ src/test/java/org/shvetsov/_837Test.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.offset;
 
 class _837Test {
 
@@ -16,14 +17,30 @@ class _837Test {
 
     @Test
     void new21GameExample1() {
-        assertThat(task.new21Game(10, 1, 10)).isEqualTo(1.0);
+        assertThat(task.new21Game(10, 1, 10)).isCloseTo(1.0, offset(0.00001));
     }
     @Test
     void new21GameExample2() {
-        assertThat(task.new21Game(6, 1, 10)).isEqualTo(0.6);
+        assertThat(task.new21Game(6, 1, 10)).isCloseTo(0.6, offset(0.00001));
     }
     @Test
     void new21GameExample3() {
-        assertThat(task.new21Game(21, 17, 10)).isEqualTo(0.73278);
+        assertThat(task.new21Game(21, 17, 10)).isCloseTo(0.73278, offset(0.00001));
+    }
+    @Test
+    void new21GameTC150() {
+        assertThat(task.new21Game(0, 0, 1)).isCloseTo(1, offset(0.00001));
+    }
+    @Test
+    void new21GameTC127() {
+        assertThat(task.new21Game(1, 0, 1)).isCloseTo(1, offset(0.00001));
+    }
+    @Test
+    void new21GameTC136() {
+        assertThat(task.new21Game(1, 0, 2)).isCloseTo(1, offset(0.00001));
+    }
+    @Test
+    void new21GameTC149() {
+        assertThat(task.new21Game(12, 1, 10)).isCloseTo(1, offset(0.00001));
     }
 }
