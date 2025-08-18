a5264c1be157e3cf6f7660e25ba032f37e59b7e0
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-08-18 10:31:15 +0300

    setup 837

---
+++ src/main/java/org/shvetsov/_837.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 837,
+        name = "New 21 Game",
+        level = Level.MEDIUM,
+        tags = {"Math", "Dynamic Programming", "Sliding Window", "Probability and Statistics", "Weekly Contest 85"}
+)
+public class _837 {
+    public double new21Game(int n, int k, int maxPts) {
+
+        return 0.0;
+    }
+}
