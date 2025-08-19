abad1ee66fbc685fe98cae772ad76d05153c4134
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-08-19 12:42:09 +0300

    setup 2348

---
+++ src/main/java/org/shvetsov/_2348.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2348,
+        name = "Number of Zero-Filled Subarrays",
+        level = Level.MEDIUM,
+        tags = {"Array", "Math", "Biweekly Contest 83"}
+)
+public class _2348 {
+    public long zeroFilledSubarray(int[] nums) {
+
+        return -1;
+    }
+}
