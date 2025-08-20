88d6c960caf3d09d7fc46eb35d69e299a10078f5
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-08-20 10:39:46 +0300

    solve 1277

--- src/main/java/org/shvetsov/_1277.java
+++ src/main/java/org/shvetsov/_1277.java
@@ -11,7 +11,34 @@ import org.shvetsov.core.Level;
 )
 public class _1277 {
     public int countSquares(int[][] matrix) {
+        int result = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1) {
+                    result += countSquaresFrom(matrix, i, j);
+                }
+            }
+        }
+        return result;
+    }
 
-        return 0;
+    private int countSquaresFrom(int[][] matrix, int i, int j) {
+        int dist = 1;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        while (true) {
+            boolean isSquare = true;
+            for (int k = i; k <= i + dist; k++) {
+                int l = j + dist;
+                if (k >= m || l >= n || matrix[k][l] != 1) isSquare = false;
+            }
+            for (int l = j; l <= j + dist; l++) {
+                int k = i + dist;
+                if (k >= m || l >= n || matrix[k][l] != 1) isSquare = false;
+            }
+            if (!isSquare) break;
+            dist++;
+        }
+        return dist;
     }
 }
