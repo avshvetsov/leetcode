3e8e6045f45ff653ba2a524ebc2f2286edad1d82
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-08-27 16:00:37 +0300

    solve 498

--- src/main/java/org/shvetsov/_498.java
+++ src/main/java/org/shvetsov/_498.java
@@ -11,7 +11,41 @@ import org.shvetsov.core.Level;
 )
 public class _498 {
     public int[] findDiagonalOrder(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        if (n == 1) {
+            return mat[0];
+        } else if (m == 1) {
+            int[] res = new int[n * m];
+            for (int i = 0; i < res.length; i++) {
+                res[i] = mat[i][0];
+            }
+            return res;
+        }
+        int[] res = new int[n * m];
+        int i = 0, r = 0, c = 0;
+        boolean direction = false;
+        while (i < res.length) {
+            if (r == 0 && c == m - 1) {
+                res[i++] = mat[r++][c];
+                direction = !direction;
+            } else if (c == 0 && r == n - 1) {
+                res[i++] = mat[r][c++];
+                direction = !direction;
+            } else if (r == 0 || r == n - 1) {
+                res[i++] = mat[r][c++];
+                direction = !direction;
+            } else if (c == 0 || c == m - 1) {
+                res[i++] = mat[r++][c];
+                direction = !direction;
+            }
 
-        return null;
+            if (direction) {
+                res[i++] = mat[r++][c--];
+            } else {
+                res[i++] = mat[r--][c++];
+            }
+        }
+        return res;
     }
 }
