78b7f258acc4d95f41ffc75dff02984326633135
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-08-28 10:24:18 +0300

    solve 3446

--- src/main/java/org/shvetsov/_3446.java
+++ src/main/java/org/shvetsov/_3446.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.*;
+
 @LeetCode(
         number = 3446,
         name = "Sort Matrix by Diagonals",
@@ -11,7 +13,30 @@ import org.shvetsov.core.Level;
 )
 public class _3446 {
     public int[][] sortMatrix(int[][] grid) {
-
-        return null;
+        int n = grid.length;
+        int m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            List<Integer> diagonal = new LinkedList<>();
+            int r = i, c = 0;
+            while (r < n) {
+                diagonal.add(grid[r++][c++]);
+            }
+            diagonal.sort(Integer::compareTo);
+            while (c > 0) {
+                grid[--r][--c] = diagonal.removeFirst();
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            List<Integer> diagonal = new LinkedList<>();
+            int r = 0, c = i;
+            while (c < m) {
+                diagonal.add(grid[r++][c++]);
+            }
+            diagonal.sort(Comparator.reverseOrder());
+            while (r > 0) {
+                grid[--r][--c] = diagonal.removeFirst();
+            }
+        }
+        return grid;
     }
 }
