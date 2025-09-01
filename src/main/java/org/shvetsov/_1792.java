e88c468b29539d70e9395fbea993fb48c63370b1
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-01 12:12:54 +0300

    solve 1792

--- src/main/java/org/shvetsov/_1792.java
+++ src/main/java/org/shvetsov/_1792.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 @LeetCode(
         number = 1792,
         name = "Maximum Average Pass Ratio",
@@ -11,7 +14,20 @@ import org.shvetsov.core.Level;
 )
 public class _1792 {
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-
-        return 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                Comparator.comparingDouble((int[] value) -> ((double) (value[0] + 1) / (value[1] + 1)) - ((double) value[0] / value[1]))
+                .reversed());
+        for (int[] cl : classes) {
+            pq.offer(cl);
+        }
+        while (extraStudents > 0) {
+            int[] polled = pq.poll();
+            pq.add(new int[]{polled[0] + 1, polled[1] + 1});
+            extraStudents--;
+        }
+        return pq.stream()
+                .mapToDouble(value -> ((double) value[0] / value[1]))
+                .average()
+                .orElse(0.0);
     }
 }
