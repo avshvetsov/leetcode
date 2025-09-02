2f5c9ee0cc32ce73bec2747189492c3e64b9d3ec
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-02 13:14:07 +0300

    solve 3025

--- src/main/java/org/shvetsov/_3025.java
+++ src/main/java/org/shvetsov/_3025.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 3025,
         name = "Find the Number of Ways to Place People I",
@@ -11,7 +13,22 @@ import org.shvetsov.core.Level;
 )
 public class _3025 {
     public int numberOfPairs(int[][] points) {
-
-        return -1;
+        int res = 0;
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                if (i == j) continue;
+                int[] pointA = points[i];
+                int[] pointB = points[j];
+                if (pointA[0] <=  pointB[0] && pointA[1] >= pointB[1]) {
+                    if (Arrays.stream(points)
+                            .filter(point -> point[0] >= pointA[0] && point[0] <= pointB[0])
+                            .filter(point -> point[1] <= pointA[1] && point[1] >= pointB[1])
+                            .count() == 2) {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
