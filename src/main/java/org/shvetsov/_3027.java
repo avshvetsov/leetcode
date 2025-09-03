9319e317948898fcdd1244695aa6ceef8b608a27
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-03 10:13:05 +0300

    setup 3027

---
+++ src/main/java/org/shvetsov/_3027.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 3027,
+        name = "Find the Number of Ways to Place People II",
+        level = Level.HARD,
+        tags = {"Array", "Math", "Geometry", "Sorting", "Enumeration", "Biweekly Contest 123"}
+)
+public class _3027 {
+    public int numberOfPairs(int[][] points) {
+
+        return -1;
+    }
+}
