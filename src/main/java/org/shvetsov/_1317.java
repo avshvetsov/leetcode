4ca4577540dcd126fff787c633fbd5baf7c47e13
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-08 17:40:36 +0300

    solve 1317

--- src/main/java/org/shvetsov/_1317.java
+++ src/main/java/org/shvetsov/_1317.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Arrays;
+
 @LeetCode(
         number = 1317,
         name = "Convert Integer to the Sum of Two No-Zero Integers",
@@ -11,7 +13,11 @@ import org.shvetsov.core.Level;
 )
 public class _1317 {
     public int[] getNoZeroIntegers(int n) {
-
-        return null;
+        int[] result = new int[]{1, n - 1};
+        while (Arrays.toString(result).contains("0")) {
+            result[0]++;
+            result[1]--;
+        }
+        return result;
     }
 }
