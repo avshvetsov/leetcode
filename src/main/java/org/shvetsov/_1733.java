fef28fe4986e08619897f845529bf5201f4aff5a
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-11 11:17:58 +0300

    solve 1733

--- src/main/java/org/shvetsov/_1733.java
+++ src/main/java/org/shvetsov/_1733.java
@@ -3,6 +3,8 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.*;
+
 @LeetCode(
         number = 1733,
         name = "Minimum Number of People to Teach",
@@ -11,7 +13,55 @@ import org.shvetsov.core.Level;
 )
 public class _1733 {
     public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
+        Map<Integer, Set<Integer>> langs = new HashMap<>();
+        for (int i = 0; i < languages.length; i++) {
+            langs.put(i + 1, new HashSet<>(Arrays.stream(languages[i]).boxed().toList()));
+        }
+        Set<Integer> shouldBeTeach = new HashSet<>();
+        for (int[] friends : friendships) {
+            Set<Integer> p1 = langs.get(friends[0]);
+            Set<Integer> p2 = langs.get(friends[1]);
+            if (Collections.disjoint(p1, p2)) {
+                shouldBeTeach.add(friends[0]);
+                shouldBeTeach.add(friends[1]);
+            }
+        }
+        int res = Integer.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {
+            int needTeach = 0;
+            for (Integer user : shouldBeTeach) {
+                if (!langs.get(user).contains(i)) {
+                    needTeach++;
+                }
+            }
+            res = Math.min(res, needTeach);
+        }
+        return res;
+    }
 
-        return -1;
+
+    public int minimumTeachingsBF(int n, int[][] languages, int[][] friendships) {
+        int res = Integer.MAX_VALUE;
+        Map<Integer, Set<Integer>> langs = new HashMap<>();
+        for (int i = 0; i < languages.length; i++) {
+            langs.put(i + 1, new HashSet<>(Arrays.stream(languages[i]).boxed().toList()));
+        }
+        for (int i = 1; i <= n; i++) {
+            Set<Integer> persons = new HashSet<>();
+            for (int[] friends : friendships) {
+                Set<Integer> p1 = langs.get(friends[0]);
+                Set<Integer> p2 = langs.get(friends[1]);
+                if (Collections.disjoint(p1, p2)) {
+                    if (!p1.contains(i)) {
+                        persons.add(friends[0]);
+                    }
+                    if (!p2.contains(i)) {
+                        persons.add(friends[1]);
+                    }
+                }
+            }
+            res = Math.min(res, persons.size());
+        }
+        return res;
     }
 }
