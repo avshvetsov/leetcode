d66d9b2205f0cc8cb3ba8bf461372648aa5733b1
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-11 10:02:46 +0300

    setup 1733

---
+++ src/main/java/org/shvetsov/_1733.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1733,
+        name = "Minimum Number of People to Teach",
+        level = Level.MEDIUM,
+        tags = {"Array", "Hash Table", "Greedy", "Biweekly Contest 44"}
+)
+public class _1733 {
+    public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
+
+        return -1;
+    }
+}
