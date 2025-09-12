9dfecf1828f0c2597fd2f55c958d99dc11e37097
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-12 10:31:54 +0300

    setup 2785

---
+++ src/main/java/org/shvetsov/_2785.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2785,
+        name = "Sort Vowels in a String",
+        level = Level.MEDIUM,
+        tags = {"String", "Sorting", "Biweekly Contest 109"}
+)
+public class _2785 {
+    public String sortVowels(String s) {
+
+        return null;
+    }
+}
