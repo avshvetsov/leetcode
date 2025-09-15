2e3f77399a96ffa54555492477d3832a12e02ae6
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-15 12:50:50 +0300

    solve 1935

--- src/main/java/org/shvetsov/_1935.java
+++ src/main/java/org/shvetsov/_1935.java
@@ -3,6 +3,9 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @LeetCode(
         number = 1935,
         name = "Maximum Number of Words You Can Type",
@@ -11,7 +14,14 @@ import org.shvetsov.core.Level;
 )
 public class _1935 {
     public int canBeTypedWords(String text, String brokenLetters) {
-
-        return -1;
+        int count = 0;
+        Set<Integer> broken = brokenLetters.chars().boxed().collect(Collectors.toSet());
+        for (String word : text.split(" ")) {
+            if (word.chars()
+                    .noneMatch(broken::contains)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
