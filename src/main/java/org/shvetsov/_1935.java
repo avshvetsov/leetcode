41aae1f75314c3a298602a9af40df99c9448d0b6
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-15 12:36:06 +0300

    setup 1935

---
+++ src/main/java/org/shvetsov/_1935.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 1935,
+        name = "Maximum Number of Words You Can Type",
+        level = Level.EASY,
+        tags = {"Hash Table", "String", "Weekly Contest 250"}
+)
+public class _1935 {
+    public int canBeTypedWords(String text, String brokenLetters) {
+
+        return -1;
+    }
+}
