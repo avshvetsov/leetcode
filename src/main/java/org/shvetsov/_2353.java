c0ed47705209fe71056a74ce619aaca4335cff94
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-17 10:12:03 +0300

    setup 2353

---
+++ src/main/java/org/shvetsov/_2353.java
@@ -0,0 +1,30 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 2353,
+        name = "Design a Food Rating System",
+        level = Level.MEDIUM,
+        tags = {"Array", "Hash Table", "String", "Design", "Heap (Priority Queue)", "Ordered Set", "Weekly Contest 303"}
+)
+public class _2353 {
+
+    static class FoodRatings {
+
+        public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+
+        }
+
+        public void changeRating(String food, int newRating) {
+
+        }
+
+        public String highestRated(String cuisine) {
+
+            return null;
+        }
+    }
+
+}
