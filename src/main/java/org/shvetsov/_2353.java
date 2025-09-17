7ea2f14f1423a623f3aee26580047110a3574477
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-17 18:30:36 +0300

    not solve 2353 (TLE)

--- src/main/java/org/shvetsov/_2353.java
+++ src/main/java/org/shvetsov/_2353.java
@@ -3,27 +3,67 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @LeetCode(
         number = 2353,
         name = "Design a Food Rating System",
         level = Level.MEDIUM,
-        tags = {"Array", "Hash Table", "String", "Design", "Heap (Priority Queue)", "Ordered Set", "Weekly Contest 303"}
+        tags = {"Array", "Hash Table", "String", "Design", "Heap (Priority Queue)", "Ordered Set", "Weekly Contest 303", "TLE"}
 )
 public class _2353 {
 
     static class FoodRatings {
 
-        public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+        private Map<String, Food> foodMap = new HashMap<>();
+        private Map<String, Food> cuisineHighestRatedFood = new HashMap<>();
 
+        public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+            for (int i = 0; i < foods.length; i++) {
+                String cuisine = cuisines[i];
+                int rating = ratings[i];
+                Food food = new Food(foods[i], cuisine, rating);
+                this.foodMap.put(foods[i], food);
+                this.cuisineHighestRatedFood.compute(cuisine, (k, v) -> v == null || food.compareTo(v) > 0 ? food : v);
+            }
+        }
+
+        record Food(String food, String cuisine, int rating) implements Comparable<Food> {
+            public Food withRating(int newRating) {
+                return new Food(food, cuisine, newRating);
+            }
+
+            @Override
+            public int compareTo(Food f) {
+                if (f.rating != this.rating) {
+                    return this.rating - f.rating;
+                } else {
+                    return f.food.compareTo(this.food);
+                }
+            }
         }
 
         public void changeRating(String food, int newRating) {
-
+            Food foodR = this.foodMap.get(food);
+            if (foodR != null) {
+                Food newFood = foodR.withRating(newRating);
+                this.foodMap.put(food, newFood);
+                if (!this.cuisineHighestRatedFood.get(newFood.cuisine).food.equals(food)) {
+                    this.cuisineHighestRatedFood.compute(foodR.cuisine, (k, v) -> v == null || newFood.compareTo(v) > 0 ? newFood : v);
+                } else {
+                    List<Food> foodList = foodMap.values().stream()
+                            .filter(f -> f.cuisine.equals(newFood.cuisine))
+                            .sorted((o1, o2) -> o1.rating != o2.rating ? Integer.compare(o1.rating, o2.rating) : o2.food.compareTo(o1.food))
+                            .toList();
+                    this.cuisineHighestRatedFood.put(newFood.cuisine, foodList.getLast());
+                }
+            }
         }
 
         public String highestRated(String cuisine) {
-
-            return null;
+            return this.cuisineHighestRatedFood.get(cuisine).food;
         }
     }
 
--- src/test/java/org/shvetsov/_2353Test.java
+++ src/test/java/org/shvetsov/_2353Test.java
@@ -1,6 +1,5 @@
 package org.shvetsov;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,5 +18,33 @@ class _2353Test {
         assertThat(foodRatings.highestRated("japanese")).isEqualTo("sushi");
         foodRatings.changeRating("ramen", 16);
         assertThat(foodRatings.highestRated("japanese")).isEqualTo("ramen");
+    }
+
+    @Test
+    void FoodRatingsTC37() {
+        _2353.FoodRatings foodRatings = new _2353.FoodRatings(new String[]{"emgqdbo", "jmvfxjohq", "qnvseohnoe", "yhptazyko", "ocqmvmwjq"},
+                new String[]{"snaxol", "snaxol", "snaxol", "fajbervsj", "fajbervsj"},
+                new int[]{2, 6, 18, 6, 5});
+
+        foodRatings.changeRating("qnvseohnoe", 11);
+        assertThat(foodRatings.highestRated("fajbervsj")).isEqualTo("yhptazyko");
+        foodRatings.changeRating("emgqdbo", 3);
+        foodRatings.changeRating("jmvfxjohq", 9);
+        foodRatings.changeRating("emgqdbo", 14);
+        assertThat(foodRatings.highestRated("fajbervsj")).isEqualTo("yhptazyko");
+        assertThat(foodRatings.highestRated("snaxol")).isEqualTo("emgqdbo");
+    }
+
+    @Test
+    void FoodRatingsTC38() {
+        _2353.FoodRatings foodRatings = new _2353.FoodRatings(new String[]{"czopaaeyl","lxoozsbh","kbaxapl"},
+                new String[]{"dmnuqeatj","dmnuqeatj","dmnuqeatj"},
+                new int[]{11,2,15});
+
+        foodRatings.changeRating("czopaaeyl", 12);
+        assertThat(foodRatings.highestRated("dmnuqeatj")).isEqualTo("kbaxapl");
+        foodRatings.changeRating("kbaxapl", 8);
+        foodRatings.changeRating("lxoozsbh", 5);
+        assertThat(foodRatings.highestRated("dmnuqeatj")).isEqualTo("czopaaeyl");
     }
 }
