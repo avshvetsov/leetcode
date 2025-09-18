7cf6c09b504c72cfaea29dcd5406e37315790280
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-18 18:49:24 +0300

    solve 3408

--- src/main/java/org/shvetsov/_3408.java
+++ src/main/java/org/shvetsov/_3408.java
@@ -3,7 +3,7 @@ package org.shvetsov;
 import org.shvetsov.core.LeetCode;
 import org.shvetsov.core.Level;
 
-import java.util.List;
+import java.util.*;
 
 @LeetCode(
         number = 3408,
@@ -14,25 +14,49 @@ import java.util.List;
 public class _3408 {
     static class TaskManager {
 
-        public TaskManager(List<List<Integer>> tasks) {
+        private final Map<Integer, Task> taskMap =  new HashMap<>();
+        private final TreeSet<Task> pq = new TreeSet<>(
+                (o1, o2) -> o1.priority() != o2.priority() ? Integer.compare(o2.priority(), o1.priority()) : Integer.compare(o2.taskId(), o1.taskId())
+        );
 
+        public TaskManager(List<List<Integer>> tasks) {
+            for (List<Integer> task : tasks) {
+                add(task.get(0),  task.get(1), task.get(2));
+            }
         }
 
         public void add(int userId, int taskId, int priority) {
-
+            Task task = new Task(userId, taskId, priority);
+            taskMap.put(taskId, task);
+            pq.add(task);
         }
 
         public void edit(int taskId, int newPriority) {
-
+            Task old = taskMap.remove(taskId);
+            Task newTask = old.withPriority(newPriority);
+            taskMap.put(taskId, newTask);
+            pq.remove(old);
+            pq.add(newTask);
         }
 
         public void rmv(int taskId) {
-
+            Task old = taskMap.remove(taskId);
+            pq.remove(old);
         }
 
         public int execTop() {
+            if (!pq.isEmpty()) {
+                Task executed = pq.removeFirst();
+                taskMap.remove(executed.taskId());
+                return executed.userId();
+            } else
+                return -1;
+        }
 
-            return 0;
+        record Task(int userId, int taskId, int priority) {
+            public Task withPriority(int newPriority) {
+                return new Task(userId, taskId, newPriority);
+            }
         }
     }
 }
