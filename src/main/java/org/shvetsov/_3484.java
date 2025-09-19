bb3834d2e8e76e874c25da4ff0f85e2153b577ed
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-19 12:45:58 +0300

    solve 3484

--- src/main/java/org/shvetsov/_3484.java
+++ src/main/java/org/shvetsov/_3484.java
@@ -12,21 +12,36 @@ import org.shvetsov.core.Level;
 public class _3484 {
     static class Spreadsheet {
 
-        public Spreadsheet(int rows) {
+        int[][] sheet;
 
+
+        public Spreadsheet(int rows) {
+            sheet = new int[26][rows];
         }
 
         public void setCell(String cell, int value) {
-
+            sheet[cell.charAt(0) - 'A'][Integer.parseInt(cell.substring(1)) - 1] = value;
         }
 
         public void resetCell(String cell) {
-
+            sheet[cell.charAt(0) - 'A'][Integer.parseInt(cell.substring(1)) - 1] = 0;
         }
 
         public int getValue(String formula) {
-
-            return -1;
+            String xS = formula.substring(1, formula.indexOf("+"));
+            String yS = formula.substring(formula.indexOf("+") + 1);
+            int x, y;
+            if (Character.isDigit(xS.charAt(0))) {
+                x = Integer.parseInt(xS);
+            } else {
+                x = sheet[xS.charAt(0) - 'A'][Integer.parseInt(xS.substring(1)) - 1];
+            }
+            if (Character.isDigit(yS.charAt(0))) {
+                y = Integer.parseInt(yS);
+            } else {
+                y = sheet[yS.charAt(0) - 'A'][Integer.parseInt(yS.substring(1)) - 1];
+            }
+            return x + y;
         }
     }
 }
