3a216feaa8ec91a25c9ce8e9a61c6e3badfdced4
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-09-26 10:31:28 +0300

    setup 611

---
+++ src/main/java/org/shvetsov/_611.java
@@ -0,0 +1,17 @@
+package org.shvetsov;
+
+import org.shvetsov.core.LeetCode;
+import org.shvetsov.core.Level;
+
+@LeetCode(
+        number = 611,
+        name = "Valid Triangle Number",
+        level = Level.MEDIUM,
+        tags = {"Array", "Two Pointers", "Binary Search", "Greedy", "Sorting"}
+)
+public class _611 {
+    public int triangleNumber(int[] nums) {
+
+        return -1;
+    }
+}
