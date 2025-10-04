5067815ffbfb9547eee74c58742e1f63fb00e63e
avshvetsov <dev.anton.shvetsov@gmail.com>
2025-10-04 13:11:18 +0300

    solve 3100

--- src/main/java/org/shvetsov/_3100.java
+++ src/main/java/org/shvetsov/_3100.java
@@ -11,7 +11,31 @@ import org.shvetsov.core.Level;
 )
 public class _3100 {
     public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int result = 0;
+        int numEmptyBottles = 0;
+        while (numBottles > 0 || numEmptyBottles >= numExchange) {
+            if (numBottles > 0) {
+                result += numBottles;
+                numEmptyBottles += numBottles;
+                numBottles = 0;
+            } else {
+                while (numEmptyBottles >= numExchange) {
+                    numEmptyBottles -= numExchange;
+                    numExchange++;
+                    numBottles++;
+                }
+            }
+        }
+        return result;
+    }
 
-        return -1;
+
+    public int maxBottlesDrunkAlternative(int numBottles, int numExchange) {
+        int ans = numBottles;
+        for (int empty = numBottles; empty >= numExchange; numExchange++) {
+            ans++;
+            empty -= numExchange - 1;
+        }
+        return ans;
     }
 }
